f67ee5a98f47a4bdb0724667e40ed1376805a574
Jessica Yuen <jyuen@ualberta.ca>
2015-02-20 16:55:14 -0700

    Re-route login to index

--- socialdistribution/socialdistribution/urls.py
+++ socialdistribution/socialdistribution/urls.py
@@ -7,5 +7,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^author/', include('author.urls')),
+    url(r'^', include('author.urls')),
 )
