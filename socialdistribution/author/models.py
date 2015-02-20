1efb1d79c722efd1d6cf48ed54af2c017cc02b44
Jessica Yuen <jyuen@ualberta.ca>
2015-02-20 13:55:34 -0700

    Initial Author model

--- socialdistribution/author/models.py
+++ socialdistribution/author/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+
+class Author(models.Model):
+    """Represents a author, which is a primary user in socialdistrubtion.
+
+    Some of the things an author can do include creating and sharing posts,
+    adding friends, managing their profile, and more.
+
+    An author has a one to one relationship with Django's User, which will
+    be used for login and authentication.
+    """
+    user = models.OneToOneField(User, primary_key=True)
