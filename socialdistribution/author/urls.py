83763f1ddf26bce06698b5dd5696af83256dde51
Jessica Yuen <jyuen@ualberta.ca>
2015-03-01 15:21:19 -0700

    Added logout, page permission checking, showing nav icons only when logged in

--- socialdistribution/author/urls.py
+++ socialdistribution/author/urls.py
@@ -9,4 +9,5 @@ urlpatterns = patterns(
     #url(r'^logout/$', 'logout', name='logout'),
     #url(r'^register/$', 'register', name='register'),
     url(r'^authors/(?P<author>\d+)/$', views.home),
+    url(r'^authors/(?P<author>\d+)/logout/$', views.logout),
 )
--- socialdistribution/author/views.py
+++ socialdistribution/author/views.py
@@ -1,4 +1,5 @@
-from django.contrib.auth import authenticate, login as auth_login, logout
+from django.contrib.auth import (authenticate,
+                                 login as auth_login, logout as auth_logout)
 from django.contrib.auth.decorators import login_required
 from django.core.context_processors import csrf
 from django.http import HttpResponseRedirect
@@ -14,39 +15,51 @@ def login(request):
     If successful, redirect the user to the home page, otherwise, return an
     error in the response.
     """
-    context = RequestContext(request)
+    if not request.user.is_authenticated():
+        context = RequestContext(request)
 
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
+        if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
 
-        # Use Django's built in authentication to verify if the username and
-        # password combination is valid
-        user = authenticate(username=username, password=password)
-        author = Author.objects.filter(user=user)
-        if len(author) > 0:
-            auth_login(request, user)
-            return redirect('/authors/' + str(user.id))
-        else:
-            # An error occurred
-            context['error'] = 'The username and/or password is incorrect.'
+            # Use Django's built in authentication to verify if the username
+            # and password combination is valid
+            user = authenticate(username=username, password=password)
+            author = Author.objects.filter(user=user)
+            if len(author) > 0:
+                auth_login(request, user)
+                return redirect('/authors/' + str(user.id))
+            else:
+                # An error occurred
+                context['error'] = 'The username and/or password is incorrect.'
 
-    return render_to_response('login.html', context)
+        return render_to_response('login.html', context)
+    else:
+        return redirect('/authors/' + str(request.user.id))
 
 
 @login_required
+def logout(request, author):
+    context = RequestContext(request)
+    auth_logout(request)
+    return redirect('/')
+
+
 def home(request, author):
     """Display the author's home page."""
     context = RequestContext(request)
 
     if request.method == 'GET':
-        if request.user.is_authenticated:
+        if request.user.is_authenticated():
             try:
                 author = Author.objects.get(user=request.user)
                 return render_to_response('home.html', context)
             except Author.DoesNotExist:
                 context['error'] = 'Please log in.'
                 return render_to_response('login.html', context)
+        else:
+            context['error'] = 'Please log in.'
+            return render_to_response('login.html', context)
     else:
         context['error'] = 'Invalid request.'
         return_to_response('login.html', context)
