f4920503cf622e42754bd212f7901c4a47cece26
nhu bui <nbui@ualberta.ca>
2015-03-06 01:12:09 -0700

    added searchbar

--- socialdistribution/socialdistribution/settings.py
+++ socialdistribution/socialdistribution/settings.py
@@ -86,3 +86,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_PATH = os.path.abspath(os.path.join(BASE_DIR, 'static'))
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
+
