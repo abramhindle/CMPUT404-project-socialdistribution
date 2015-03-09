f9ff4c010ba64d7777ec854d226ae3817fbc30b2
Jessica Yuen <jyuen@ualberta.ca>
2015-03-09 17:34:06 -0600

    Docstrings for web api, framework changes

--- socialdistribution/node/urls.py
+++ socialdistribution/node/urls.py
@@ -4,11 +4,10 @@ from node import views
 
 urlpatterns = patterns(
     '',
-    url(r'^posts/$', views.all_visible_posts),
-    url(r'^posts/(?P<post_id>)\d+/$', views.post),
-    url(r'^author/(?P<author>\d+)/$', views.profile),
+    url(r'^posts/$', views.public_posts),
+    url(r'^posts/(?P<post_id>)\d+/$', views.public_posts),
+    url(r'^author/posts/$', views.posts),
     url(r'^author/(?P<author>\d+)/posts/$', views.posts),
-    url(r'^author/posts/$', views.author_posts),
     url(r'^friends/(?P<user_id>\d+)/$', views.friends),
     url(r'^friends/(?P<user_id1>\d+)/(?P<user_id2>\d+)/$', views.is_friend),
     url(r'^friendrequest/$', views.friend_request),
--- socialdistribution/node/views.py
+++ socialdistribution/node/views.py
@@ -1,34 +1,153 @@
 from django.shortcuts import render
 
 
-def all_visible_posts(request):
+def public_posts(request, post_id=None):
+    """Return all posts marked as public on the server.
+
+    If a post_id is specified, only return a single post with the provided id.
+
+    This responds with the same JSON as the posts API below.
+    """
     raise NotImplementedError
 
 
-def post(request, post_id):
-    raise NotImplementedError
+def posts(request, author_id=None):
+    """Return the posts that are visible to the current authenticated user.
 
+    If author id is specified, only posts for the specified author will be
+    returned.
 
-def posts(request):
-    """Return the posts that are visible to the current authenticated user."""
-    raise NotImplementedError
+    This responds with the following JSON:
 
-
-def author_posts(request):
-    raise NotImplementedError
-
-
-def profile(request, author):
+    {
+        "posts":[
+            {
+                "title":"A post title about a post about web dev",
+                "source":"http://lastplaceigotthisfrom.com/post/yyyyy",
+                "origin":"http://whereitcamefrom.com/post/zzzzz",
+                "description":"This post discusses stuff -- brief",
+                # The content type of the post
+                # assume either text/html, text/x-markdown, text/plain
+                # for HTML you will want to strip tags before displaying
+                "content-type":"text/html",
+                "content":"your content here",
+                "author":{
+                    # unique id to each author, either a sha1 or a uuid
+                    "id":"9de17f29c12e8f97bcbbd34cc908f1baba40658e",
+                    # the home host of the author
+                    "host":"http://127.0.0.1:5454/",
+                    # the display name of the author
+                    "displayname":"Lara",
+                    # url to the authors information
+                    "url":"http://127.0.0.1:5454/author/
+                           9de17f29c12e8f97bcbbd34cc908f1baba40658e"
+                },
+                "categories":["web","tutorial"],
+                "comments":[
+                    {
+                        "author":{
+                            "id":"8d919f29c12e8f97bcbbd34cc908f19ab9496989",
+                            "host":"http://127.0.0.1:5454/",
+                            "displayname":"Greg"
+                        },
+                        "comment":"Sick Olde English"
+                        "pubDate":"Fri Jan  3 15:50:40 MST 2014",
+                        "guid":"5471fe89-7697-4625-a06e-b3ad18577b72"
+                    }
+                ]
+                "pubDate":"Fri Jan  1 12:12:12 MST 2014",
+                # ID of the post (uuid or sha1)
+                "guid":"108ded43-8520-4035-a262-547454d32022"
+                # visibility ["PUBLIC","FOAF","FRIENDS","PRIVATE","SERVERONLY"]
+                "visibility":"PUBLIC"
+            }
+        ]
+    }
+    """
     raise NotImplementedError
 
 
 def friends(request, user_id):
+    """Return whether anyone in the list is a friend
+
+    This expects a POST request with the following content:
+
+    {
+        "query": "friends",
+        "author": "9de17f29c12e8f97bcbbd34cc908f1baba40658e",
+        "authors": [
+            "7deee0684811f22b384ccb5991b2ca7e78abacde",
+            "31cc28a8fbc05787d0470cdbd62ea0474764b0ae",
+            "1af17e947f387a2d8c09a807271bd094e8eff077",
+            "77cb4f546b280ea905a6fdd99977cd090613994a",
+            "11c3783f15f7ade03430303573098f0d4d20797b",
+            "bd9ef9619c7241112d2a2b79505f736fc8d7f43e",
+            "0169a8ebf3cb3bd7f092603564873e12cce9d4c5",
+            "2130905fd0de94c3379e04839cd9f6889ba2b52c",
+            "b32c9e0b5fcf85f46b9ce2ba89b2068b57d4641b",
+            "fe45075b93d06c833bb25d5a6dfe669cfde3f99d",
+            "e28e59a9612c369717f66f53f3e014b341857601",
+            "b36e52d6aaee9285220f94fc321407a44e4dc622",
+            "584a9739ea459ce4aae5a88827d970196fb27769",
+            "96b3b5a70cd9591c73760bd8669aa5bd7cc689c5",
+            "6465678d0a409b96829fd64d0894132966e97eee",
+            "695c780ea2815bc94c54782f5046dfa4e325f875",
+            "8743f7511a1a569e4e9dacbb25e27395629ba5c0",
+            "539b65f2d76d0327dc45bf6354cda535d6f8ed02",
+            "c55670261253c5ce25e22b47a34629dd15e819d4"
+        ]
+    }
+
+    This responds with the following JSON:
+    {
+        "query": "friends",
+        "author": "9de17f29c12e8f97bcbbd34cc908f1baba40658e",
+        "friends":[
+            "7deee0684811f22b384ccb5991b2ca7e78abacde",
+            "11c3783f15f7ade03430303573098f0d4d20797b",
+        ]
+    }
+    """
     raise NotImplementedError
 
 
 def is_friend(request, user_id1, user_id2):
+    """Return whether the provided two users are friends.
+
+    This expects a POST request with the following content:
+
+    {
+        "query": "friends",
+        "authors": [
+            "9de17f29c12e8f97bcbbd34cc908f1baba40658e",
+            "8d919f29c12e8f97bcbbd34cc908f19ab9496989"
+        ],
+        # or NO
+        "friends":"YES"
+    }
+    """
     raise NotImplementedError
 
 
 def friend_request(request):
+    """Makes a friend request.
+
+    This expects a POST request with the following JSON as the content:
+
+    {
+        "query": "friendrequest",
+        "author": {
+            "id": "8d919f29c12e8f97bcbbd34cc908f19ab9496989",
+            "host": "http://127.0.0.1:5454/",
+            "displayname": "Greg"
+        },
+        "friend": {
+            "id":"9de17f29c12e8f97bcbbd34cc908f1baba40658e",
+            "host":"http://127.0.0.1:5454/",
+            "displayname":"Lara",
+            "url":"http://127.0.0.1:5454/author/
+                   9de17f29c12e8f97bcbbd34cc908f1baba40658e"
+        }
+    }
+    """
     raise NotImplementedError
