a204077a5a76b472dbe3cf422c75a51cadebbc36
Jessica Yuen <jyuen@ualberta.ca>
2015-03-09 16:10:31 -0600

    Add REST api url framework

--- socialdistribution/node/urls.py
+++ socialdistribution/node/urls.py
@@ -1,6 +1,15 @@
 from django.conf.urls import patterns, url
 
+from node import views
+
 urlpatterns = patterns(
     '',
-    #url(r'^/', 'temp'),
+    url(r'^posts/$', views.all_visible_posts),
+    url(r'^posts/(?P<post_id>)\d+/$', views.post),
+    url(r'^author/(?P<author>\d+)/$', views.profile),
+    url(r'^author/(?P<author>\d+)/posts/$', views.posts),
+    url(r'^author/posts/$', views.author_posts),
+    url(r'^friends/(?P<user_id>\d+)/$', views.friends),
+    url(r'^friends/(?P<user_id1>\d+)/(?P<user_id2>\d+)/$', views.is_friend),
+    url(r'^friendrequest/$', views.friend_request),
 )
--- socialdistribution/node/views.py
+++ socialdistribution/node/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def all_visible_posts(request):
+    raise NotImplementedError
+
+
+def post(request, post_id):
+    raise NotImplementedError
+
+
+def posts(request):
+    """Return the posts that are visible to the current authenticated user."""
+    raise NotImplementedError
+
+
+def author_posts(request):
+    raise NotImplementedError
+
+
+def profile(request, author):
+    raise NotImplementedError
+
+
+def friends(request, user_id):
+    raise NotImplementedError
+
+
+def is_friend(request, user_id1, user_id2):
+    raise NotImplementedError
+
+
+def friend_request(request):
+    raise NotImplementedError
