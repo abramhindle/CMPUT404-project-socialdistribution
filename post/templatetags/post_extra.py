5360a59d1ac3799f824d3886487e81dc56e6679d
hwen3 <hwen3@ualberta.ca>
2015-03-24 20:52:26 -0600

    added missing file

---
+++ post/templatetags/post_extra.py
@@ -0,0 +1,24 @@
+from datetime import datetime, timedelta
+from time import strptime, mktime
+from dateutil import parser
+from django import template
+from django.utils.timesince import timesince
+
+import pytz
+
+register = template.Library()
+
+# http://stackoverflow.com/questions/6494921/how-to-display-x-days-ago-type-time-using-humanize-in-django-template
+
+@register.filter
+def datesince(value):
+    now = datetime.now(pytz.utc)
+    try:
+        date = parser.parse(value)
+        difference = now - date
+    except:
+        return value
+
+    if difference <= timedelta(minutes=1):
+        return 'just now'
+    return '%(time)s ago' % {'time': timesince(date).split(', ')[0]}
