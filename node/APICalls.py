acdcfea80c7ebdaf61d9e6a75d0c0fe10a65087c
nhu bui <nbui@ualberta.ca>
2015-03-25 19:22:15 -0600

    return json for api call

--- node/APICalls.py
+++ node/APICalls.py
@@ -9,9 +9,9 @@ def api_getPublicPost():
 	#TODO if they implement authentication
 	#fix this if other teams change their APIs calls
 	url = 'http://' + host+ '/main/getposts/'
-	getJsonFromURL(url)
+	return getJsonFromURL(url, "posts")
 	
-def getJsonFromURL(url):
+def getJsonFromURL(url, type):
 	try:
 		# download the json string
 		json_string = urlopen(url).read()
@@ -19,33 +19,9 @@ def getJsonFromURL(url):
 		# de-serialize the string so that we can work with it
 		the_data = json.loads(json_string)
 
-
-		parse_setPost(the_data)
+		if (the_data.get(type)):
+			return the_data
 	except Exception as e:
 		return HttpResponse(e.message,
                                 content_type='text/plain',
                                 status=500)
-
-def parse_setPost(jsonReply):
-	hostauthor =""
-	if (jsonReply.get("posts")):
-		for item in jsonReply.get("posts"):
-			#print(item.get('content-type') + " content-type")
-			
-			#maybe do some checking so the same post does not get recreated
-			post = Post.objects.create(
-				title=item.get('title'),
-				content=item.get('content'),
-				content_type=item.get('content-type'),
-				#be careful of spelling
-				visibility=item.get('visability'),
-				guid=item.get('guid'),
-				description=item.get('description'),
-				origin=item.get('origin'),
-				source=item.get('source'),
-				pubdate=item.get('pubdate'),
-				author=hostauthor
-				#if (item.get('author')):
-				#	author = item.get('author').get('displayname')
-				#comment stuff
-			)
