465249ade00191ba18f86c918409155ffe8497ee
hwen3 <hwen3@ualberta.ca>
2015-03-26 22:03:39 -0600

    no idea what was in this folder, but it looked like alot of libraries we arent using...

--- cmput404-project-socialdistribution/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/cshome/vsawyer1/CMPUT404-project-socialdistribution/cmput404-project-socialdistribution/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
