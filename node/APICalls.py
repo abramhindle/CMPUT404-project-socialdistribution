2e293a87690b36d864eec525ba97b764e0efdd8c
nhu bui <nbui@ualberta.ca>
2015-03-26 20:55:45 -0600

    added post by id to other server

--- node/APICalls.py
+++ node/APICalls.py
@@ -6,11 +6,34 @@ import urllib2, base64
 from base64 import b64encode
 import requests
 
-def api_getPublicPost():
-	url = 'http://thought-bubble.herokuapp.com/main/getposts/'
-	return getJsonFromURL(url, "posts")
+'''
+thought-bubble.herokuapp
+curl -u admin:host:admin --request GET 'thought-bubble.herokuapp/main/getapost/?postid=ad3e28a6d41211e48a1c6edce164578e'
+curl -u admin:host:admin --request GET 'thought-bubble.herokuapp/main/getauthorposts/?authorid=3cf3d542f96a42c3a8edd5da02826740/'
+curl -u admin:host:admin --request GET 'thought-bubble.herokuapp/main/author/posts2/'
+curl -u admin:host:admin --request GET 'thought-bubble.herokuapp.com/main/getposts/'
+'''
+POSTS ='posts'
+
+def api_getPostByID(postID):
+	'''get a post by its ID'''
+	url = 'thought-bubble.herokuapp/main/getapost/?postid=%s' % postID
 	
-def getJsonFromURL(url, type):
+	data = getJsonFromURL(url)
+	if (data.get(POSTS)):
+		return data.get(POSTS)
+	return ""
+
+def api_getPublicPost():
+	'''get all public posts'''
+	url = 'http://thought-bubble.herokuapp.com/main/getposts/'
+
+	data = getJsonFromURL(url)
+	if (data.get(POSTS)):
+		return data.get(POSTS)
+	return ""
+	
+def getJsonFromURL(url):
 	username = "admin"
 	host="host"
 	password="admin"
@@ -22,9 +45,7 @@ def getJsonFromURL(url, type):
 
 		# de-serialize the string so that we can work with it
 		the_data = json.loads(request.content)
-
-		if (the_data.get(type)):
-			return the_data.get(type)
+		return the_data
 	except Exception as e:
 		return HttpResponse(e.message,
                                 content_type='text/plain',
