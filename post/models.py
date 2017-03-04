5b52e43171b82078f4c0f172448fc6a3a4b3fb8d
ookmm <gaylordmbuyi@gmail.com>
2017-03-04 00:58:22 -0700

    Completed basic add and delete post.

--- post/models.py
+++ post/models.py
@@ -1,13 +1,20 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.utils import timezone
+from django.core.urlresolvers import reverse
 
 
 class Post(models.Model):
     post_story = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
+
+    def get_absolute_url(self):
+        '''
+        Add new post to database
+        :return:
+        '''
+        return reverse('post:detail', kwargs={'pk': self.pk})
 
     # Print the string representation of Post
     def __str__(self):
--- post/views.py
+++ post/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.http import Http404
+'''
+from django.shortcuts import render, get_object_or_404
 from . models import Post
+
 
 def index(request):
     all_posts = Post.objects.all()
@@ -11,10 +12,39 @@ def index(request):
     return render(request, 'post/index.html', context)
 
 def detail(request, post_id):
-    try:
-        post = Post.objects.get(id=post_id)
-    except Post.DoesNotExist:
-        raise Http404("Post does not exist")
+    post = get_object_or_404(Post, id=post_id)
 
     return render(request, 'post/detail.html', {'post': post})
+'''
 
+
+from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
+from . models import Post
+
+
+class IndexView(generic.ListView):
+    template_name = 'post/index.html'
+    context_object_name = 'all_posts'
+
+    def get_queryset(self):
+        return Post.objects.all()
+
+class DetailView(generic.DetailView):
+    model = Post
+    template_name = 'post/detail.html'
+
+
+class PostCreate(CreateView):
+    model = Post
+    fields = ['post_story']
+    success_url = reverse_lazy('post:index')
+
+class PostUpdate(UpdateView):
+    model = Post
+    fields = ['post_story']
+
+class PostDelete(DeleteView):
+    model = Post
+    success_url = reverse_lazy('post:index')
