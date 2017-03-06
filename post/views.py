6411d52c961ef814d6b139892a85256c9af30595
ookmm <gaylordmbuyi@gmail.com>
2017-03-05 19:34:51 -0700

    Added Bootstrap and minor styling.

--- post/views.py
+++ post/views.py
@@ -1,23 +1,3 @@
-'''
-from django.shortcuts import render, get_object_or_404
-from . models import Post
-
-
-def index(request):
-    all_posts = Post.objects.all()
-    context = {
-        'all_posts': all_posts,
-    }
-
-    return render(request, 'post/index.html', context)
-
-def detail(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
-
-    return render(request, 'post/detail.html', {'post': post})
-'''
-
-
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
@@ -29,12 +9,11 @@ class IndexView(generic.ListView):
     context_object_name = 'all_posts'
 
     def get_queryset(self):
-        return Post.objects.all()
+        return Post.objects.all().order_by('-pub_date')
 
 class DetailView(generic.DetailView):
     model = Post
     template_name = 'post/detail.html'
-
 
 class PostCreate(CreateView):
     model = Post
