d9ffc259e0be114937e2bc1967d028137e13a64a
David Yee <dvyee@ualberta.ca>
2017-03-08 21:18:01 -0700

    Remove generic redirection to login page

--- common/middleware/auth/middleware.py
+++ common/middleware/auth/middleware.py
@@ -19,18 +19,9 @@ class AuthRequiredMiddleware(object):
 
     def process_request(self, request):
         path = request.path_info.lstrip('/')
-        if not request.user.is_authenticated():
-            # Login check by conner.xyz (http://stackoverflow.com/users/2836259/conner-xyz)
-            # http://stackoverflow.com/a/40873794/2557554
-            # CC-BY-SA 3.0 (https://creativecommons.org/licenses/by-sa/3.0/deed.en)
-            if not any(path == eu for eu in ["", "login/",
-                                             "accounts/register/",
-                                             "accounts/password/reset/",
-                                             "admin/"]):
-                return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))  # or http response
-        else:
+        if request.user.is_authenticated():
             if any(path == eu for eu in ["accounts/register/"]):
-                return redirect('/')  # or http response
+                return redirect(reverse('index'))
 
             user_profile = Author.objects.get(user_id=request.user.id)
 
