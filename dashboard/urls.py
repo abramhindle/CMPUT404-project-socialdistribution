3ceb1171844d5c79a4e8a1669293ed9d48355bca
Adam Ford <adamjford@gmail.com>
2017-03-11 12:39:56 -0700

    Add links to template

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,5 +6,5 @@ from dashboard import views
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='dashboard/index.html'), name='index'),
     url(r'^authors/$', views.AuthorListView.as_view(), name='author-list'),
-    url(r'^authors/(?P<pk>[0-9]+)$', views.AuthorDetailView.as_view(), name='author-detail'),
+    url(r'^authors/(?P<author_id>[0-9,a-z,\\-]+)$', views.AuthorDetailView.as_view(), name='author-detail'),
 ]
