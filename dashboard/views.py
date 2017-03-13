47a05d9292a2d4c09f0592b9e0480be3c469db2e
Alanna McLafferty <mclaffer@ualberta.ca>
2017-03-12 18:26:47 -0600

    Some post visibility cases handled for my stream

--- dashboard/views.py
+++ dashboard/views.py
@@ -34,25 +34,37 @@ def index(request):
 def indexHome(request):
     # Note: Slight modification to allow for latest posts to be displayed on landing page
     if request.user.is_authenticated():
-
+        print("REQUEST:",request)
         user = request.user
         author = Author.objects.get(user=request.user.id)
         print (author)
         print (author.id)
         context = dict()
-        # Return posts that are NOT by current user (=author) and
-        # that ARE in being followed by current user (=author)
+        context1 = dict()
+        context2 = dict()
+
+        # Return posts that are NOT by current user (=author) and:
 
         # case 1: post.visibility=public and following               --> can view
-        context['all_posts'] = Post.objects\
+        # case 1': post.visibility=public  and not following          --> can't view
+        # case 2': post.visibility=friends and not friends            --> can't view
+        context1['all_posts'] = Post.objects\
             .filter(~Q(author__id=user.profile.id))\
             .filter(author__id__in=author.followed_authors.all())\
             .filter(visibility="PUBLIC").order_by('-pub_date')
+        print("CONTEXT ORIGINAL", context1)
+
+        # case 2: post.visibility=friends and friends                 --> can view
+        context2['all_posts'] = Post.objects \
+            .filter(~Q(author__id=user.profile.id)) \
+            .filter(author__id__in=author.friends.all()) \
+            .filter(Q(visibility="FRIENDS")|Q(visibility="PUBLIC")).order_by('-pub_date')
+        print("CONTEXT 2", context2)
+
+        context["all_posts"] = context1["all_posts"] | context2["all_posts"]
+        print("CONTEXT UPDATE", context)
 
         # TODO: need to be able to filter posts by current user's relationship to post author
-        # case 1': post.visibility=public  and not following          --> can't view
-        # case 2: post.visibility=friends and friends                 --> can view
-        # case 2': post.visibility=friends and not friends            --> can't view
         # case 3: post.visibility=foaf and friend/foaf                --> can view
         # case 3': post.visibility=foaf and not either friend/foaf    --> can view
         # case 4: post.visibility=private                             --> can't see
