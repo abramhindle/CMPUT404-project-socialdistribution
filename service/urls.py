00a90339180a5dea54863ffb9ed0e073138782c2
Adam Ford <adamjford@gmail.com>
2017-03-20 19:57:37 -0600

    A little bit of reorganization in the service app

--- service/urls.py
+++ service/urls.py
@@ -1,17 +1,18 @@
 from django.conf.urls import url, include
 from rest_framework import routers
+from service.views import authors, friendrequest, users
 
 # Routers provide an easy way of automatically determining the URL conf.
 from service import views
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'authors', views.AuthorViewSet)
+router.register(r'users', users.UserViewSet)
+router.register(r'authors', authors.AuthorViewSet)
 
 # Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
+# Additionally, we include login URLs for the browseable API.
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^friendrequest/', views.send_friend_request, name='friend-request'),
+    url(r'^friendrequest/', friendrequest.friendrequest, name='friend-request'),
 ]
