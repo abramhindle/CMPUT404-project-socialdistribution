381f1fe982697018973b2b6270bd5bd5be56d989
hiufungk <hiufungk@ualberta.ca>
2019-02-08 13:32:10 -0500

    added basic registration api

--- backend/api/endpoints.py
+++ backend/api/endpoints.py
@@ -2,10 +2,12 @@ from django.conf.urls import include, url
 from rest_framework import routers
 
 from .api import DummyPostViewSet
+from .view.RegistrationView import RegistrationView
 
 router = routers.DefaultRouter()
 router.register('dummy_post', DummyPostViewSet)
 
 urlpatterns = [
     url("^", include(router.urls)),
+    url("^auth/register/$", RegistrationView.as_view()),
 ]
--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,9 +1,29 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 from .models import DummyPost
+
+
+class CreateUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(validated_data['username'],
+                                        None,
+                                        validated_data['password'])
+        return user
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username')
 
 
 class DummyPostSerializer(serializers.ModelSerializer):
     class Meta:
         model = DummyPost
-        fields = ('id', 'text', )
+        fields = ('id', 'text',)
