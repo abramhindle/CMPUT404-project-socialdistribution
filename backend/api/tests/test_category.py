f5099502376ce01adaef0bed6ff3b3baf26699dc
hiufungk <hiufungk@ualberta.ca>
2019-02-13 17:47:53 -0500

    added permission tests for getting categories

--- backend/api/tests/test_category.py
+++ backend/api/tests/test_category.py
@@ -14,12 +14,14 @@ class AuthorProfileCase(TestCase):
                                     data={"username": self.username, "password": self.password}
                                     )
         self.assertEqual(response.status_code, 200)
-        self.client.login(username=self.username, password=self.password)
-
-    def test_get_categories(self):
         Category.objects.create(name="test_category_1")
         Category.objects.create(name="test_category_2")
 
+    def test_get_categories(self):
+        response = self.client.get("/api/categories/")
+        self.assertEqual(response.status_code, 403)
+
+        self.client.login(username=self.username, password=self.password)
         response = self.client.get("/api/categories/")
         expected_output = ["test_category_1", "test_category_2"]
         self.assertEqual(response.status_code, 200)
