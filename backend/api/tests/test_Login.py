0d870cad37b1784bfde0a20aa790d8620e8fc363
hiufungk <hiufungk@ualberta.ca>
2019-02-13 11:22:16 -0700

    Update test_Login.py
    changed single quotes to double quotes

--- backend/api/tests/test_Login.py
+++ backend/api/tests/test_Login.py
@@ -9,19 +9,19 @@ class LoginTestCase(TestCase):
 
     def test_login(self):
         #try to login as a user that does not exist
-        response = self.client.post('/api/auth/login/',
+        response = self.client.post("/api/auth/login/",
                                     data={"username": self.username, "password": self.password}
                                     )
         self.assertNotEqual(response.status_code, 200)
 
         #create user
-        response = self.client.post('http://localhost:8000/api/auth/register/',
+        response = self.client.post("http://localhost:8000/api/auth/register/",
                                     data={"username": self.username, "password": self.password}
                                     )
         self.assertEqual(response.status_code, 200)
 
         #try to login again after user is registered
-        response = self.client.post('/api/auth/login/',
+        response = self.client.post("/api/auth/login/",
                                     data={"username": self.username, "password": self.password}
                                     )
         self.assertEqual(response.status_code, 200)
