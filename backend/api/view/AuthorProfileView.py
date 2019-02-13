2abdd5194c764895bd90084af6b687ee1eb1b4a0
hiufungk <hiufungk@ualberta.ca>
2019-02-12 22:12:57 -0500

    fixed AuthorProfileView.py incorrect indentation

--- backend/api/view/AuthorProfileView.py
+++ backend/api/view/AuthorProfileView.py
@@ -5,15 +5,16 @@ from rest_framework.response import Response
 
 
 class AuthorProfileView(generics.GenericAPIView):
-   serializer_class = AuthorProfileSerializer
-   permission_classes = (permissions.IsAuthenticated,)
-   # permission_classes = (permissions.AllowAny,)
-   def post(self, request, *args, **kwargs):
-      serializer = AuthorProfileSerializer(data=request.data, context={'request': self.request})
-      
-      if(serializer.is_valid(raise_exception=True)):
-         httpStatus = status.HTTP_200_OK
-         return Response(serializer.data, httpStatus)
-      else:
-         httpStatus = status.HTTP_400_BAD_REQUEST
-         return Response(serializer.errors, httpStatus)
+    serializer_class = AuthorProfileSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    # permission_classes = (permissions.AllowAny,)
+    def post(self, request, *args, **kwargs):
+        serializer = AuthorProfileSerializer(data=request.data, context={'request': self.request})
+
+        if (serializer.is_valid(raise_exception=True)):
+            httpStatus = status.HTTP_200_OK
+            return Response(serializer.data, httpStatus)
+        else:
+            httpStatus = status.HTTP_400_BAD_REQUEST
+            return Response(serializer.errors, httpStatus)
