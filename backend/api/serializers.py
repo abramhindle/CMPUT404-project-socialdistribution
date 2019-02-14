d5b63cfda915fd6a082f0bdcf4814204e6d1219a
hiufungk <hiufungk@ualberta.ca>
2019-02-13 20:48:24 -0500

    added get request for categories

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,8 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
-
-from .models import Post, AuthorProfile
+from .models import Post, AuthorProfile, Category
 
 
 class CreateUserSerializer(serializers.ModelSerializer):
@@ -37,19 +36,20 @@ class LoginUserSerializer(serializers.Serializer):
 
 class AuthorProfileSerializer(serializers.ModelSerializer):
     author = UserSerializer(read_only=True)
-    
-    class Meta:     
+
+    class Meta:
         model = AuthorProfile
-        fields= (
+        fields = (
             'github',
             'author'
-            )            
+        )
+
 
 class PostSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
         read_only=True,
         slug_field='username'
-     )
+    )
 
     class Meta:
         model = Post
@@ -58,3 +58,11 @@ class PostSerializer(serializers.ModelSerializer):
             'text',
             'author'
         )
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = (
+            'name',
+        )
+
