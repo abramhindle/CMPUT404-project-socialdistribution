7c4c8f01388e26347f95d9c2714b4b597cab00a5
htruong1 <htruong1@ualberta.ca>
2019-02-19 13:56:41 -0700

    WIP for put method for posting content

--- backend/api/tests/test_Post.py
+++ backend/api/tests/test_Post.py
@@ -33,84 +33,127 @@ class AuthorProfileCase(TestCase):
                                      user=self.user)
 
 
-    def test_invalid_auth(self):
-        # test if the endpoint is protected by auth
-        response = self.client.post("/api/posts/", data=self.input_params)
-        self.assertEqual(response.status_code, 403)
+    # def test_invalid_auth(self):
+    #     # test if the endpoint is protected by auth
+    #     response = self.client.post("/api/posts/", data=self.input_params)
+    #     self.assertEqual(response.status_code, 403)
 
 
-    def test_create_post_fail(self):
+    # def test_create_post_fail(self):
+    #     self.client.login(username=self.username, password=self.password)
+
+    #     for key in self.input_params.keys():
+    #         if (key == "visibleTo" or key == "unlisted"):
+    #             continue
+    #         invalid_input = self.input_params.copy()
+    #         # test fields is empty
+    #         invalid_input[key] = ""
+    #         if (key == "categories"):
+    #             invalid_input[key] = []
+    #         response = self.client.post("/api/posts/", data=invalid_input)
+    #         self.assertEqual(response.status_code, 400)
+
+    #         # test field is missing
+    #         invalid_input.pop(key)
+    #         response = self.client.post("/api/posts/", data=invalid_input)
+    #         self.assertEqual(response.status_code, 400)
+
+    # # helper function for validating if the post is created since some attributes not possible to assert equal
+    # def assert_create_post(self, output, expected_post):
+    #     for key in expected_post.keys():
+    #         if key != "id" and key != "author" and key != "published":
+    #             self.assertEqual(output[key], expected_post[key])
+    #     # assert author part
+    #     for key in ["host", "displayName", "github"]:
+    #         self.assertEqual(output["author"][key], expected_post["author"][key])
+
+    #     expected_id = "{}author/{}".format(self.authorProfile.host, self.authorProfile.id)
+    #     self.assertEqual(output["author"]["id"], expected_id)
+    #     self.assertEqual(output["author"]["url"], expected_id)
+
+    # def test_create_post_success(self):
+    #     self.client.login(username=self.username, password=self.password)
+
+    #     expected_post = {"title": "A post title about a post about web dev",
+    #                      "source": "http://lastplaceigotthisfrom.com/posts/yyyyy",
+    #                      "origin": "http://whereitcamefrom.com/posts/zzzzz",
+    #                      "description": "This post discusses stuff -- brief",
+    #                      "contentType": "text/plain",
+    #                      "content": "Some content",
+    #                      "author": {
+    #                          "id": "http://127.0.0.1:5454/author/9de17f29c12e8f97bcbbd34cc908f1baba40658e",
+    #                          "host": "http://127.0.0.1:5454/",
+    #                          "displayName": "Lara Croft",
+    #                          "url": "http://127.0.0.1:5454/author/9de17f29c12e8f97bcbbd34cc908f1baba40658e",
+    #                          "github": "http://github.com/laracroft"
+    #                      },
+    #                      "categories": ["test_category_1", "test_category_2"],
+    #                      "published": "2015-03-09T13:07:04+00:00",
+    #                      "id": "de305d54-75b4-431b-adb2-eb6b9e546013",
+    #                      "visibility": "PUBLIC",
+    #                      "visibleTo": [],
+    #                      "unlisted": False
+    #                      }
+
+    #     # test valid input with existing categories
+    #     response = self.client.post("/api/posts/", data=self.input_params)
+
+    #     created_post = Post.objects.all()[0]
+    #     created_post = PostSerializer(created_post).data
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assert_create_post(created_post, expected_post)
+    #     self.assertEqual(json.loads(response.content), "Create Post Success")
+
+    #     # test valid input with non-existing categories
+    #     expected_post["categories"] = ["non_existing_category"]
+    #     self.input_params["categories"] = ["non_existing_category"]
+    #     response = self.client.post("/api/posts/", data=self.input_params)
+    #     self.assertEqual(response.status_code, 200)
+    #     created_post = Post.objects.all()[1]
+    #     created_post = PostSerializer(created_post).data
+    #     self.assert_create_post(created_post, expected_post)
+    #     self.assertEqual(json.loads(response.content), "Create Post Success")
+    #     self.client.logout()
+
+    # def create_mock_post(self, dict_input, author_profile):
+    #     post = Post.objects.create(title=dict_input["title"],
+    #                                source=dict_input["source"],
+    #                                origin=dict_input["origin"],
+    #                                description=dict_input["description"],
+    #                                contentType=dict_input["contentType"],
+    #                                content=dict_input["content"],
+    #                                author=author_profile,
+    #                                visibility=dict_input["visibility"],
+    #                                unlisted=dict_input["unlisted"])
+    #     post.categories.set(dict_input["categories"])
+    #     post.visibleTo.set(dict_input["visibleTo"
+    #                        ])
+
+    def test_put_post(self):
+        input_params = {"title": "A post title about a post about web dev",
+                    "source": "http://lastplaceigotthisfrom.com/posts/yyyyy",
+                    "origin": "http://whereitcamefrom.com/posts/zzzzz",
+                    "description": "This post discusses stuff -- brief",
+                    "contentType": "text/plain",
+                    "content": "Some content",
+                    "categories": ["test_category_1", "test_category_2"],
+                    "visibility": "PUBLIC",
+                    "visibleTo": [],
+                    "unlisted": False,
+                    }
         self.client.login(username=self.username, password=self.password)
+        Post.objects.all().delete()
 
-        for key in self.input_params.keys():
-            if (key == "visibleTo" or key == "unlisted"):
-                continue
-            invalid_input = self.input_params.copy()
-            # test fields is empty
-            invalid_input[key] = ""
-            if (key == "categories"):
-                invalid_input[key] = []
-            response = self.client.post("/api/posts/", data=invalid_input)
-            self.assertEqual(response.status_code, 400)
-
-            # test field is missing
-            invalid_input.pop(key)
-            response = self.client.post("/api/posts/", data=invalid_input)
-            self.assertEqual(response.status_code, 400)
-
-    # helper function for validating if the post is created since some attributes not possible to assert equal
-    def assert_create_post(self, output, expected_post):
-        for key in expected_post.keys():
-            if key != "id" and key != "author" and key != "published":
-                self.assertEqual(output[key], expected_post[key])
-        # assert author part
-        for key in ["host", "displayName", "github"]:
-            self.assertEqual(output["author"][key], expected_post["author"][key])
-
-        expected_id = "{}author/{}".format(self.authorProfile.host, self.authorProfile.id)
-        self.assertEqual(output["author"]["id"], expected_id)
-        self.assertEqual(output["author"]["url"], expected_id)
-
-    def test_create_post_success(self):
-        self.client.login(username=self.username, password=self.password)
-
-        expected_post = {"title": "A post title about a post about web dev",
-                         "source": "http://lastplaceigotthisfrom.com/posts/yyyyy",
-                         "origin": "http://whereitcamefrom.com/posts/zzzzz",
-                         "description": "This post discusses stuff -- brief",
-                         "contentType": "text/plain",
-                         "content": "Some content",
-                         "author": {
-                             "id": "http://127.0.0.1:5454/author/9de17f29c12e8f97bcbbd34cc908f1baba40658e",
-                             "host": "http://127.0.0.1:5454/",
-                             "displayName": "Lara Croft",
-                             "url": "http://127.0.0.1:5454/author/9de17f29c12e8f97bcbbd34cc908f1baba40658e",
-                             "github": "http://github.com/laracroft"
-                         },
-                         "categories": ["test_category_1", "test_category_2"],
-                         "published": "2015-03-09T13:07:04+00:00",
-                         "id": "de305d54-75b4-431b-adb2-eb6b9e546013",
-                         "visibility": "PUBLIC",
-                         "visibleTo": [],
-                         "unlisted": False
-                         }
-
-        # test valid input with existing categories
-        response = self.client.post("/api/posts/", data=self.input_params)
-
-        created_post = Post.objects.all()[0]
-        created_post = PostSerializer(created_post).data
+        response = self.client.put("/api/posts/",  data=input_params, content_type="application/json")
         self.assertEqual(response.status_code, 200)
-        self.assert_create_post(created_post, expected_post)
-        self.assertEqual(json.loads(response.content), "Create Post Success")
+        # created_post = Post.objects.all()[0]
+        # created_post = PostSerializer(created_post).data
+        
+        # self.assert_create_post(created_post, expected_post)
+        # self.assertEqual(json.loads(response.content), "Create Post Success")
 
-        # test valid input with non-existing categories
-        expected_post["categories"] = ["non_existing_category"]
-        self.input_params["categories"] = ["non_existing_category"]
-        response = self.client.post("/api/posts/", data=self.input_params)
-        self.assertEqual(response.status_code, 200)
-        created_post = Post.objects.all()[1]
-        created_post = PostSerializer(created_post).data
-        self.assert_create_post(created_post, expected_post)
-        self.assertEqual(json.loads(response.content), "Create Post Success")
-        self.client.logout()
+    
+        # self.create_mock_post(public_post, self.authorProfile)
+
+    
+    
--- backend/api/view/CreatePostView.py
+++ backend/api/view/CreatePostView.py
@@ -2,21 +2,37 @@ from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from ..models import Category
 from ..serializers import PostSerializer
+from django.http import QueryDict
 
 
 class CreatePostView(generics.GenericAPIView):
     serializer_class = PostSerializer
     permission_classes = (permissions.IsAuthenticated,)
 
-    def post(self, request, *args, **kwargs):
-        input_category_list = request.data.getlist("categories")
-
+    def insert_post(self, request):
+        print("inside the postit boi")
+        # input_category_list = request.data.getlist("categories")
+        # print(request.data, "save me")
         for category in input_category_list:
             if (not Category.objects.filter(name=category).exists()):
                 Category.objects.create(name=category)
 
         serializer = PostSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save(author=self.request.user.authorprofile)
+            serializer.save(author=request.user.authorprofile)
             return Response("Create Post Success", status.HTTP_200_OK)
         return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+    def post(self, request, *args, **kwargs):
+        print("post")
+        print(request.data)
+        return self.insert_post(request)
+
+    def put(self, request, postid):
+        # print(request, "put boi")
+        # print(request.body)
+        # print(QueryDict(request.body))
+        # print(request.user, "save me fam")
+        # print("nani")
+        if 
+        return self.insert_post(QueryDict(request.body))
