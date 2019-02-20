7875a8376d18db57fc42d19b9abecc056a74c30e
htruong1 <htruong1@ualberta.ca>
2019-02-20 12:52:31 -0700

    Pushing put

--- backend/api/tests/test_Post.py
+++ backend/api/tests/test_Post.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
+from django.test.client import Client, RequestFactory
 from rest_framework.test import RequestsClient
 from ..models import Category, Post, AuthorProfile
 from ..serializers import PostSerializer
 import json
+from respite.middleware import *
 
 
 class AuthorProfileCase(TestCase):
@@ -33,43 +35,43 @@ class AuthorProfileCase(TestCase):
                                      user=self.user)
 
 
-    # def test_invalid_auth(self):
-    #     # test if the endpoint is protected by auth
-    #     response = self.client.post("/api/posts/", data=self.input_params)
-    #     self.assertEqual(response.status_code, 403)
+    # # def test_invalid_auth(self):
+    # #     # test if the endpoint is protected by auth
+    # #     response = self.client.post("/api/posts/", data=self.input_params)
+    # #     self.assertEqual(response.status_code, 403)
 
 
-    # def test_create_post_fail(self):
-    #     self.client.login(username=self.username, password=self.password)
+    # # def test_create_post_fail(self):
+    # #     self.client.login(username=self.username, password=self.password)
 
-    #     for key in self.input_params.keys():
-    #         if (key == "visibleTo" or key == "unlisted"):
-    #             continue
-    #         invalid_input = self.input_params.copy()
-    #         # test fields is empty
-    #         invalid_input[key] = ""
-    #         if (key == "categories"):
-    #             invalid_input[key] = []
-    #         response = self.client.post("/api/posts/", data=invalid_input)
-    #         self.assertEqual(response.status_code, 400)
+    # #     for key in self.input_params.keys():
+    # #         if (key == "visibleTo" or key == "unlisted"):
+    # #             continue
+    # #         invalid_input = self.input_params.copy()
+    # #         # test fields is empty
+    # #         invalid_input[key] = ""
+    # #         if (key == "categories"):
+    # #             invalid_input[key] = []
+    # #         response = self.client.post("/api/posts/", data=invalid_input)
+    # #         self.assertEqual(response.status_code, 400)
 
-    #         # test field is missing
-    #         invalid_input.pop(key)
-    #         response = self.client.post("/api/posts/", data=invalid_input)
-    #         self.assertEqual(response.status_code, 400)
+    # #         # test field is missing
+    # #         invalid_input.pop(key)
+    # #         response = self.client.post("/api/posts/", data=invalid_input)
+    # #         self.assertEqual(response.status_code, 400)
 
-    # # helper function for validating if the post is created since some attributes not possible to assert equal
-    # def assert_create_post(self, output, expected_post):
-    #     for key in expected_post.keys():
-    #         if key != "id" and key != "author" and key != "published":
-    #             self.assertEqual(output[key], expected_post[key])
-    #     # assert author part
-    #     for key in ["host", "displayName", "github"]:
-    #         self.assertEqual(output["author"][key], expected_post["author"][key])
+    # # # helper function for validating if the post is created since some attributes not possible to assert equal
+    def assert_create_post(self, output, expected_post):
+        for key in expected_post.keys():
+            if key != "id" and key != "author" and key != "published":
+                self.assertEqual(output[key], expected_post[key])
+        # assert author part
+        for key in ["host", "displayName", "github"]:
+            self.assertEqual(output["author"][key], expected_post["author"][key])
 
-    #     expected_id = "{}author/{}".format(self.authorProfile.host, self.authorProfile.id)
-    #     self.assertEqual(output["author"]["id"], expected_id)
-    #     self.assertEqual(output["author"]["url"], expected_id)
+        expected_id = "{}author/{}".format(self.authorProfile.host, self.authorProfile.id)
+        self.assertEqual(output["author"]["id"], expected_id)
+        self.assertEqual(output["author"]["url"], expected_id)
 
     # def test_create_post_success(self):
     #     self.client.login(username=self.username, password=self.password)
@@ -115,37 +117,52 @@ class AuthorProfileCase(TestCase):
     #     self.assertEqual(json.loads(response.content), "Create Post Success")
     #     self.client.logout()
 
-    # def create_mock_post(self, dict_input, author_profile):
-    #     post = Post.objects.create(title=dict_input["title"],
-    #                                source=dict_input["source"],
-    #                                origin=dict_input["origin"],
-    #                                description=dict_input["description"],
-    #                                contentType=dict_input["contentType"],
-    #                                content=dict_input["content"],
-    #                                author=author_profile,
-    #                                visibility=dict_input["visibility"],
-    #                                unlisted=dict_input["unlisted"])
-    #     post.categories.set(dict_input["categories"])
-    #     post.visibleTo.set(dict_input["visibleTo"
-    #                        ])
+    def create_mock_post(self, dict_input, author_profile):
+        post = Post.objects.create(title=dict_input["title"],
+                                   source=dict_input["source"],
+                                   origin=dict_input["origin"],
+                                   description=dict_input["description"],
+                                   contentType=dict_input["contentType"],
+                                   content=dict_input["content"],
+                                   author=author_profile,
+                                   visibility=dict_input["visibility"],
+                                   unlisted=dict_input["unlisted"])
+        post.categories.set(dict_input["categories"])
+        post.visibleTo.set(dict_input["visibleTo"
+                           ])
 
     def test_put_post(self):
-        input_params = {"title": "A post title about a post about web dev",
-                    "source": "http://lastplaceigotthisfrom.com/posts/yyyyy",
-                    "origin": "http://whereitcamefrom.com/posts/zzzzz",
-                    "description": "This post discusses stuff -- brief",
-                    "contentType": "text/plain",
-                    "content": "Some content",
-                    "categories": ["test_category_1", "test_category_2"],
-                    "visibility": "PUBLIC",
-                    "visibleTo": [],
-                    "unlisted": False,
-                    }
-        self.client.login(username=self.username, password=self.password)
         Post.objects.all().delete()
 
-        response = self.client.put("/api/posts/",  data=input_params, content_type="application/json")
+        input_params = {"title": "A post title about a post about web dev",
+                         "source": "http://lastplaceigotthisfrom.com/posts/yyyyy",
+                         "origin": "http://whereitcamefrom.com/posts/zzzzz",
+                         "description": "This post discusses stuff -- brief",
+                         "contentType": "text/plain",
+                         "content": "Some content to put in",
+                         "categories": ["test_category_1", "test_category_2"],
+                         "published": "2015-03-09T13:07:04+00:00",
+                         "id": "de305d54-75b4-431b-adb2-eb6b9e546013",
+                         "visibility": "PUBLIC",
+                         "visibleTo": [],
+                         "unlisted": False
+                         }
+        self.client.login(username=self.username, password=self.password)
+        print("this is PUT")
+        request = RequestFactory().put(
+            path = '/api/posts',
+            data = input_params,
+            content_type = "application/json"
+         )
+        HttpPutMiddleware().process_request(request)
+        # print(request, "dfa")
+        # response = self.client.put("/api/posts",  data=input_params, content_type = "application/json")
+        
+        # print("this is POST")
+        # response = self.client.post("/api/posts", data=input_params)
         self.assertEqual(response.status_code, 200)
+        # <'django.http.request.QueryDict'>
+        # self.assertEqual(response.status_code, 200)
         # created_post = Post.objects.all()[0]
         # created_post = PostSerializer(created_post).data
         
--- backend/api/view/CreatePostView.py
+++ backend/api/view/CreatePostView.py
@@ -3,30 +3,42 @@ from rest_framework.response import Response
 from ..models import Category
 from ..serializers import PostSerializer
 from django.http import QueryDict
+from rest_framework import mixins
+import json
+import requests
+import ast
+from pprint import pprint
 
 
 class CreatePostView(generics.GenericAPIView):
     serializer_class = PostSerializer
     permission_classes = (permissions.IsAuthenticated,)
+    test_list = []
 
-    def insert_post(self, request):
-        print("inside the postit boi")
-        # input_category_list = request.data.getlist("categories")
+    def insert_post(self, request, user):
+        # print(request, "save me")
+        input_category_list = request.getlist("categories")
+        self.test_list.append(request)
+  
         # print(request.data, "save me")
         for category in input_category_list:
             if (not Category.objects.filter(name=category).exists()):
                 Category.objects.create(name=category)
 
-        serializer = PostSerializer(data=request.data)
+        serializer = PostSerializer(data=request)
+        # print(serializer)
         if serializer.is_valid():
-            serializer.save(author=request.user.authorprofile)
+            # print("am i valid")
+            serializer.save(author=user.authorprofile)
             return Response("Create Post Success", status.HTTP_200_OK)
-        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+        else:
+            # print("or am i not valid")
+            return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
 
     def post(self, request, *args, **kwargs):
-        print("post")
-        print(request.data)
-        return self.insert_post(request)
+        # print(request.user, "post user")
+        print("\n",request.data, "dreaming")
+        return self.insert_post(request.data, request.user)
 
     def put(self, request, postid):
         # print(request, "put boi")
@@ -34,5 +46,37 @@ class CreatePostView(generics.GenericAPIView):
         # print(QueryDict(request.body))
         # print(request.user, "save me fam")
         # print("nani")
-        if 
-        return self.insert_post(QueryDict(request.body))
+        print("I am PUT")
+        pprint(vars(request))
+        print(request._data)
+        # if (len(postid) < 1):
+            
+            # responseData = QueryDict(request.body)
+            # print(request.data, "hello")
+            # request_body = request.body.decode("utf-8")
+
+            # print("\n",request_body)
+            # content_list = request_body[1:-1].split(",")
+            # request_data = QueryDict()
+            # query_dict_arg = ""
+            # print(content_list)z
+            # print(json.loads(request_body))
+            # print(request_body)
+
+            # request_body = request_body.replace("'", '"')
+            # request_body = request_body[1:-1]
+            # print(request_body)
+            # data = json.loads(request_body)
+            # print(data)
+            # hello = ast.literal_eval(request_body)
+            # print(hello)
+            # query_dict_param = ""
+            # for key, value in hello.items():
+            #     if (not (isinstance(value, str))):
+            #         value = str(value)
+            #     query_dict_param = query_dict_param + key + "=" + value + "&"
+            
+            # Q = QueryDict(query_dict_param[0:-1])
+            # print(Q)
+            # return self.insert_post(Q, request.user)
+        return Response("yay Success", status.HTTP_200_OK)
