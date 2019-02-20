25e310be4483a493558d221d52389a200bba4dac
hiufungk <hiufungk@ualberta.ca>
2019-02-20 05:38:36 -0500

    cleaned up test

--- backend/api/tests/test_Friends.py
+++ backend/api/tests/test_Friends.py
@@ -9,6 +9,23 @@ class AuthorProfileCase(TestCase):
     username = "test123"
     password = "pw123"
 
+    input_params = {
+        "query": "friendrequest",
+        "author": {
+            "id": "http://127.0.0.1:5454/author/de305d54-75b4-431b-adb2-eb6b9e546013",
+            "host": "http://127.0.0.1:5454/",
+            "displayName": "Greg Johnson",
+            "url": "http://127.0.0.1:5454/author/de305d54-75b4-431b-adb2-eb6b9e546013",
+        },
+        "friend": {
+            "id": "http://127.0.0.1:5454/author/de305d54-75b4-431b-adb2-eb6b9e637281",
+            "host": "http://127.0.0.1:5454/",
+            "displayName": "Lara Croft",
+            "url": "http://127.0.0.1:5454/author/de305d54-75b4-431b-adb2-eb6b9e546013",
+
+        }
+    }
+
     def setUp(self):
         self.user = User.objects.create_user(username=self.username, password=self.password)
         # self.authorProfile = AuthorProfile.objects.create(host="http://127.0.0.1:5454/",
@@ -22,31 +39,15 @@ class AuthorProfileCase(TestCase):
         self.assertEqual(response.status_code, 403)
 
     def test_missing_arguments(self):
-        input_params = {
-            "query": "friendrequest",
-            "author": {
-                "id": "http://127.0.0.1:5454/author/de305d54-75b4-431b-adb2-eb6b9e546013",
-                "host": "http://127.0.0.1:5454/",
-                "displayName": "Greg Johnson",
-                "url": "http://127.0.0.1:5454/author/de305d54-75b4-431b-adb2-eb6b9e546013",
-            },
-            "friend": {
-                "id": "http://127.0.0.1:5454/author/de305d54-75b4-431b-adb2-eb6b9e637281",
-                "host": "http://127.0.0.1:5454/",
-                "displayName": "Lara Croft",
-                "url": "http://127.0.0.1:5454/author/de305d54-75b4-431b-adb2-eb6b9e546013",
-
-            }
-        }
         self.client.login(username=self.username, password=self.password)
-        for key in input_params.keys():
-            invalid_input = input_params.copy()
+        for key in self.input_params.keys():
+            invalid_input = self.input_params.copy()
             invalid_input.pop(key)
             response = self.client.post("/api/friendrequest", data=invalid_input, content_type="application/json")
             self.assertEqual(response.status_code, 400)
 
         for key in ["author", "friend"]:
-            invalid_input = input_params.copy()
+            invalid_input = self.input_params.copy()
             invalid_input[key].pop("id")
             response = self.client.post("/api/friendrequest", data=invalid_input, content_type="application/json")
             self.assertEqual(response.status_code, 400)
@@ -64,64 +65,34 @@ class AuthorProfileCase(TestCase):
     def test_follow(self):
         Follow.objects.all().delete()
         self.client.login(username=self.username, password=self.password)
-        input_params = {
-            "query":"friendrequest",
-            "author": {
-                "id":"http://127.0.0.1:5454/author/de305d54-75b4-431b-adb2-eb6b9e546013",
-                "host":"http://127.0.0.1:5454/",
-                "displayName":"Greg Johnson",
-                        "url":"http://127.0.0.1:5454/author/de305d54-75b4-431b-adb2-eb6b9e546013",
-            },
-            "friend": {
-                "id":"http://127.0.0.1:5454/author/de305d54-75b4-431b-adb2-eb6b9e637281",
-                "host":"http://127.0.0.1:5454/",
-                "displayName":"Lara Croft",
-                "url":"http://127.0.0.1:5454/author/de305d54-75b4-431b-adb2-eb6b9e546013",
 
-            }
-        }
-        response = self.client.post("/api/friendrequest", data=input_params, content_type="application/json")
+        response = self.client.post("/api/friendrequest", data=self.input_params, content_type="application/json")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(json.loads(response.content), "Friend Request Success")
 
         row = Follow.objects.all()[0]
-        self.assertEqual(row.authorA, input_params["author"]["id"])
-        self.assertEqual(row.authorB, input_params["friend"]["id"])
+        self.assertEqual(row.authorA, self.input_params["author"]["id"])
+        self.assertEqual(row.authorB, self.input_params["friend"]["id"])
         self.assertEqual(row.status, "FOLLOWING")
 
     def test_friend(self):
         Follow.objects.all().delete()
         self.client.login(username=self.username, password=self.password)
-        input_params = {
-            "query": "friendrequest",
-            "author": {
-                "id": "http://127.0.0.1:5454/author/de305d54-75b4-431b-adb2-eb6b9e546013",
-                "host": "http://127.0.0.1:5454/",
-                "displayName": "Greg Johnson",
-                "url": "http://127.0.0.1:5454/author/de305d54-75b4-431b-adb2-eb6b9e546013",
-            },
-            "friend": {
-                "id": "http://127.0.0.1:5454/author/de305d54-75b4-431b-adb2-eb6b9e637281",
-                "host": "http://127.0.0.1:5454/",
-                "displayName": "Lara Croft",
-                "url": "http://127.0.0.1:5454/author/de305d54-75b4-431b-adb2-eb6b9e546013",
 
-            }
-        }
-        Follow.objects.create(authorA=input_params["friend"]["id"],
-                              authorB=input_params["author"]["id"],
+        Follow.objects.create(authorA=self.input_params["friend"]["id"],
+                              authorB=self.input_params["author"]["id"],
                               status="FOLLOWING")
 
-        response = self.client.post("/api/friendrequest", data=input_params, content_type="application/json")
+        response = self.client.post("/api/friendrequest", data=self.input_params, content_type="application/json")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(json.loads(response.content), "Friend Request Success")
 
         results = Follow.objects.all()
 
-        self.assertEqual(results[0].authorA, input_params["friend"]["id"])
-        self.assertEqual(results[0].authorB, input_params["author"]["id"])
+        self.assertEqual(results[0].authorA, self.input_params["friend"]["id"])
+        self.assertEqual(results[0].authorB, self.input_params["author"]["id"])
         self.assertEqual(results[0].status, "FRIENDS")
 
-        self.assertEqual(results[1].authorA, input_params["author"]["id"])
-        self.assertEqual(results[1].authorB, input_params["friend"]["id"])
+        self.assertEqual(results[1].authorA, self.input_params["author"]["id"])
+        self.assertEqual(results[1].authorB, self.input_params["friend"]["id"])
         self.assertEqual(results[1].status, "FRIENDS")
