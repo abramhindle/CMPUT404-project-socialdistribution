ec2912a4e0f637ae2be8f034966ae959c798de35
shchau <shchau1@ualberta.ca>
2019-02-21 15:17:01 -0500

    wip

--- frontend/src/util/HTTPFetchUtil.js
+++ frontend/src/util/HTTPFetchUtil.js
@@ -8,7 +8,7 @@ const getHeader = (requireAuth) => {
 
         return {"Content-Type": "application/json", 
                     'Authorization': 'Basic ' + 
-                    window.btoa(username + ':' + password)};
+                    window.btoa("test2" + ':' + "test2")};
     } else {
         return  {"Content-Type": "application/json"};
     }
