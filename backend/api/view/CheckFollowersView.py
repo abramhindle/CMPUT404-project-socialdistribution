b18d28ee95414659025b9a295d629d1a548db83c
hiufungk <hiufungk@ualberta.ca>
2019-03-12 18:06:51 -0400

    addressed feedback to change variables

--- backend/api/view/CheckFollowersView.py
+++ backend/api/view/CheckFollowersView.py
@@ -13,19 +13,19 @@ class CheckFollowersView(generics.GenericAPIView):
         if (self.kwargs['authorid'] == ""):
             return Response("Error: Author ID required!", status.HTTP_400_BAD_REQUEST)
         author_id = urllib.parse.unquote(self.kwargs['authorid'])
-        tmp = author_id.split("author/")
-        if (len(tmp) == 2):
-            query_set = AuthorProfile.objects.filter(host=tmp[0], id=tmp[1])
+        tmp_author_data = author_id.split("author/")
+        if (len(tmp_author_data) == 2):
+            query_set = AuthorProfile.objects.filter(host=tmp_author_data[0], id=tmp_author_data[1])
             if (len(query_set) != 1):
                 return Response("Error: Author Does Not Exist", status.HTTP_400_BAD_REQUEST)
         follow_list = Follow.objects.filter(authorB=author_id, status="FOLLOWING")
 
         follow_list_data = []
-        for follwer in follow_list:
-            follower_fulll_id = follwer.authorA
-            tmp = follower_fulll_id.split("author/")
-            host = tmp[0]
-            short_id = tmp[1]
+        for follower in follow_list:
+            follower_fulll_id = follower.authorA
+            tmp_follower_data = follower_fulll_id.split("author/")
+            host = tmp_follower_data[0]
+            short_id = tmp_follower_data[1]
             # todo: check if host belongs to our server, call cross server endpoint if doesnt
             follower_profile = AuthorProfile.objects.get(id=short_id)
             serialized_author_profile = AuthorProfileSerializer(follower_profile)
