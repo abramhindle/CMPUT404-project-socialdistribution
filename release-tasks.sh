8898d56dae36e2d88ed6a01c053649b15fdfea46
Michael Paradis <paradis2@ualberta.ca>
2019-03-12 22:03:25 -0600

    Heroku setup (#26)
    * added cors settings
    
    * added Server model
    
    * added migration
    
    * Added server to admin panel.
    Adding __str__ to server
    
    * stuff
    
    * Adding server preferences for node to node
    
    * Add migration
    
    * Heroku?
    
    * Fix error
    
    * Automatically migrate
    
    * Automatically migrate
    
    * Removed bug
    
    * Heroku db attempt
    
    * Heroku db attempt
    
    * Heroku db attempt
    
    * Heroku db attempt
    
    * Trying to get heroku deployed
    
    * Last line problem
    
    * Hack to get migrations working maybe
    
    * Hack to get migrations working maybe
    
    * Hack to get migrations working maybe
    
    * More verbose
    
    * Migration nuking ripperino
    
    * Filter posts based on server preferences
    
    * Remove unneeded static files
    
    * update git ignore

---
+++ release-tasks.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+python manage.py makemigrations
+python manage.py migrate
--- staticfiles/auth_wrapper.js
+++
@@ -1,6 +0,0 @@
-class AuthStore {
-    static header() {
-        return "Basic ZGV2OmxvbG5vb2IxMjM="
-        //TODO Actually set this up. Currently does nothing
-    }
-}
