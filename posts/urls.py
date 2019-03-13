baac48b5bea25233ee484c93f906a55ee399f126
Michael <paradis2@ualberta.ca>
2019-03-12 23:36:24 -0600

    User can now be registered. Very rough implementation

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from .views import AdminUserView
-from . import views
 from .views import UserView, PostView, PostViewID, FrontEndPostViewID, CommentViewList
-
+from .viewsfolder.login_reg_view import RegistrationPageView
 urlpatterns = [
     path('posts/', PostView.as_view(), name='posts'),
     path('frontend/posts/<pk>', FrontEndPostViewID.as_view(), name='frontpostid'),
@@ -10,4 +9,5 @@ urlpatterns = [
     path('posts/<post_id>/comments/', CommentViewList.as_view(), name='comments'),
     path('users/', UserView.as_view(), name='users'),
     path('approve/', AdminUserView.as_view(), name='admin-users'),
+    path('register/', RegistrationPageView.as_view(), name='register-users')
 ]
