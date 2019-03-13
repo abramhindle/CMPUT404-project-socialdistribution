9034ea8bd41359aa17636b72dbbbd4b072800e05
Michael <paradis2@ualberta.ca>
2019-03-12 21:50:44 -0600

    CSRF Hack and fixed serializer bug we should have seen

--- posts/serializers.py
+++ posts/serializers.py
@@ -5,13 +5,13 @@ import base64
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-    firstName = serializers.CharField(source='first_name')
-    lastName = serializers.CharField(source='last_name')
+    firstName = serializers.CharField(source='first_name', default="")
+    lastName = serializers.CharField(source='last_name', default="")
     displayName = serializers.CharField(source='username', validators=[UniqueValidator(User.objects.all())])
     password1 = serializers.CharField(write_only=True, required=False)
     password2 = serializers.CharField(write_only=True, required=False)
     github = serializers.URLField(allow_blank=True, required=False)
-    email = serializers.EmailField()
+    email = serializers.EmailField(default="")
 
     class Meta:
         model = User
