b9dec3a471f138d1bb182d61a1b1d2392caa43ca
tlaz4 <tlazaren@ualberta.ca>
2019-03-14 00:21:48 -0600

    Added pagination for comments and posts, should follow json format in example

--- posts/pagination.py
+++ posts/pagination.py
@@ -2,7 +2,7 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 # custom pagination class to mimic example json
-# override the response so that ours follows format
+# override the response so that ours follows format, only used for comments and posts
 class CustomPagination(PageNumberPagination):
 	def get_paginated_response(self, data, query):
 		response = {"query" : query,
