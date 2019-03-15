f3493d30123c6cf751a61a76d3a2f1a41168c5f4
shchau <shchau1@ualberta.ca>
2019-03-15 18:12:21 -0400

    padding underneath stream post to look less squished when it's not your post

---
+++ frontend/src/pages/SinglePost.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import StreamFeed from '../components/StreamFeed';
+import store from '../store/index.js';
+import './styles/Stream.css';
+
+//TODO: Change the urlPath to be the one for the logged in user.
+
+class SinglePost extends Component {	
+
+	render() {
+		const urlPath = "/posts/" + this.props.match.params.postId
+		const storeItems = store.getState().loginReducers;
+		return(	
+			<div className="pusher">
+				<StreamFeed userID={storeItems.userId} urlPath={urlPath} />
+			</div>
+			)
+    }
+}
+
+export default SinglePost;
