8da1930876f672071214e701dbf9f46da95ac1aa
shchau <shchau1@ualberta.ca>
2019-03-17 21:16:31 -0400

    made cookie time 1 hour instead of 15 minutes

--- frontend/src/actions/LoginActions.js
+++ frontend/src/actions/LoginActions.js
@@ -15,12 +15,12 @@ export const sendLogin = (urlPath, requireAuth, body) => {
                             displayName: results.displayName,
                         }
                         
-                        // Expiry time for cookie: 1/96 == 15 minutes. 
-      					Cookies.set("username", body.username, {expires: 1/96});
-						Cookies.set("userID", results.authorId, {expires: 1/96});
-						Cookies.set("displayName", results.displayName, {expires: 1/96});
+                        // Expiry time for cookie: 1/24 == 1 hour. 
+      					Cookies.set("username", body.username, {expires: 1/24});
+						Cookies.set("userID", results.authorId, {expires: 1/24});
+						Cookies.set("displayName", results.displayName, {expires: 1/24});
 						Cookies.set("userPass", window.btoa(body.username + ':' + body.password), 
-						{expires: 1/96} );
+						{expires: 1/24} );
                         
 
                         return dispatch({
