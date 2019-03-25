1c4047790cd9702c5c5f85de821675d419e3296d
htruong1 <htruong1@ualberta.ca>
2019-03-25 04:35:01 -0600

    Pushing postcommentsview

--- backend/api/view/PostCommentsView.py
+++ backend/api/view/PostCommentsView.py
@@ -13,28 +13,31 @@ class PostCommentsView(generics.GenericAPIView):
         if (postid == ""):
             return Response("Error: Post ID must be specified", status.HTTP_400_BAD_REQUEST)
         else:
-            post_id = self.kwargs["postid"]
-            author_post = Post.objects.get(id=post_id)
-
-            commenting_author = AuthorProfile.objects.get(user=request.user)
-            request_user_full_id = get_author_id(commenting_author, False)
-            if(not can_read(request_user_full_id, PostSerializer(author_post).data)):
-                response_obj = {
-                    "query": "addComment",
-                    "success": False,
-                    "message":"Comment not allowed"
-                }
-                return Response(response_obj, status.HTTP_403_FORBIDDEN)
-            else:
-                Comment.objects.create(
+            if(request.data["query"] == "addComment"):
+                post_id = self.kwargs["postid"]
+                try:
+                    author_post = Post.objects.get(id=post_id)
+                    commenting_author = AuthorProfile.objects.get(user=request.user)
+                    request_user_full_id = get_author_id(commenting_author, False)
+                    if(not can_read(request_user_full_id, PostSerializer(author_post).data)):
+                        response_obj = {
+                            "query": "addComment",
+                            "success": False,
+                            "message":"Comment not allowed"
+                        }
+                        return Response(response_obj, status.HTTP_403_FORBIDDEN)
+                    else:
+                        Comment.objects.create(
                             author=commenting_author,
-                            comment=request.data["comment"],
-                            contentType=request.data["contentType"],
+                            comment=request.data["comment"]["comment"],
+                            contentType=request.data["comment"]["contentType"],
                             post=author_post
                         )
-                response_obj = {
-                    "query": "addComment",
-                    "success": True,
-                    "message":"Comment Added"
-                }
-                return Response(response_obj, status.HTTP_200_OK)
+                        response_obj = {
+                            "query": "addComment",
+                            "success": True,
+                            "message": "Comment Added"
+                        }
+                        return Response(response_obj, status.HTTP_200_OK)
+                except:
+                    return Response("Error: No such posts exists", status.HTTP_400_BAD_REQUEST)
