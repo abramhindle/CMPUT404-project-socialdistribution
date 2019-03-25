232f817c28b014a879bc9e4897bcee7531c40451
forgeno <ima@ualberta.ca>
2019-03-25 19:00:13 -0400

    added changes

--- backend/api/view/CreatePostView.py
+++ backend/api/view/CreatePostView.py
@@ -63,10 +63,7 @@ class CreatePostView(generics.GenericAPIView):
         public_posts = []
         local_author = AuthorProfile.objects.filter(user=request.user).exists()
         if(local_author):
-            print("local author")
-            print(ServerUser.objects.all())
             for server_obj in ServerUser.objects.all():
-                print("inside loop")
                 server_host = server_obj.host
                 headers = {'Content-type': 'application/json'}
                 url = server_obj.host + "api/posts"
@@ -81,30 +78,6 @@ class CreatePostView(generics.GenericAPIView):
                     response_json = json.loads(response.content)
                     public_posts += response_json["posts"]
 
-            # query_set = Post.objects.filter(visibility="PUBLIC", unlisted=False).order_by("-published")
-            # public_posts +=  PostSerializer(query_set, many=True).data
-            # sorted_public_foreign_posts = sorted(public_posts, key=lambda k: k['published'], reverse=True)
-
-            # response_data = {
-            #     "query": "posts",
-            #     "count": len(sorted_public_foreign_posts),
-            #     "posts": sorted_public_foreign_posts
-            # }
-            # return Response(response_data, status.HTTP_200_OK)
-        # else:
-        #     for server_obj in ServerUser.objects.all():
-        #         server_host = server_obj.host
-        #         headers = {'Content-type': 'application/json'}
-        #         url = server_obj.host + "api/posts"
-        #         my_cross_server_username = settings.USERNAME
-        #         my_cross_server_password = settings.PASSWORD
-        #         response = requests.get(url, auth=(my_cross_server_username, my_cross_server_password),
-        #                                 headers=headers)
-        #         if response.status_code != 200:
-        #             return Response("Cross Server get post Request Fail", status.HTTP_400_BAD_REQUEST)
-        #         else:
-        #             response_json = json.loads(response.content)
-        #             public_posts += response_json["posts"]
 
         query_set = Post.objects.filter(visibility="PUBLIC", unlisted=False).order_by("-published")
         public_posts +=  PostSerializer(query_set, many=True).data
