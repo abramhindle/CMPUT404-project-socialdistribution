f950ca21dd4fa834f3fba1aecfd5f0e07fae13ff
hiufungk <hiufungk@ualberta.ca>
2019-03-28 20:47:24 -0400

    added abort signal

--- backend/api/view/GetPostsView.py
+++ backend/api/view/GetPostsView.py
@@ -1,13 +1,37 @@
+import uuid
+
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
-from ..models import Post, AuthorProfile
+from ..models import Post, AuthorProfile, ServerUser
 from ..serializers import PostSerializer, AuthorProfileSerializer
 from .Util import *
+import requests
 
 
 class GetPostsView(generics.GenericAPIView):
     serializer_class = AuthorProfileSerializer
     permission_classes = (permissions.IsAuthenticated,)
+
+    def get_local_author_post(self, request, request_user_full_id, author_id):
+        try:
+            author_profile = AuthorProfile.objects.get(id=author_id)
+            author_posts = Post.objects.filter(author=author_profile).order_by("-published")
+            posts = None
+            is_own_posts_author = str(author_id) == str(request.user.authorprofile.id)
+            posts = PostSerializer(author_posts, many=True).data
+            posts_response = []
+
+            for post in posts:
+                if(can_read(request_user_full_id, post) or is_own_posts_author):
+                    posts_response.append(post)
+            response_data = {
+                "query": "posts",
+                "count": len(posts_response),
+                "posts": posts_response
+            }
+            return Response(response_data, status.HTTP_200_OK)
+        except:
+            return Response("Error: Author does not exist!", status.HTTP_400_BAD_REQUEST)
 
     def get(self, request, authorid):
         author_id = self.kwargs['authorid']
@@ -15,26 +39,46 @@ class GetPostsView(generics.GenericAPIView):
         if(author_id == ""):
             status_code = status.HTTP_400_BAD_REQUEST
             return Response("Error: no author id was specified", status_code)
-        else:
-            try:
-                author_profile = AuthorProfile.objects.get(id=author_id)
-                author_posts = Post.objects.filter(author=author_profile, ).order_by("-published")
-                posts = None
-                isOwnPostsAuthor = str(author_id) == str(request.user.authorprofile.id)
-                posts = PostSerializer(author_posts, many=True).data
-                posts_response = []
 
+        author_profile_exists = AuthorProfile.objects.filter(user=request.user).exists()
+        server_user_exists = ServerUser.objects.filter(user=request.user).exists()
+
+        # from front end
+        if author_profile_exists:
+            is_local_uuid = True
+            # for foreign author:
+            # expect front end to send http://127.0.0.1:8000/api/author/http%3A%2F%2F127.0.0.1%3A1234%2Fapi%2Fauthor%2F163974c0-b350-4e9b-a708-b570acee826d/posts
+            # for local author:
+            # expect to front end to send http://127.0.0.1:8000/api/author/163974c0-b350-4e9b-a708-b570acee826d/posts
+            try:
+                uuid.UUID(author_id)
+            except ValueError:
+                is_local_uuid = False
+
+            if is_local_uuid:
                 user_profile = AuthorProfile.objects.get(user=request.user)
                 request_user_full_id = get_author_id(user_profile, False)
-                for post in posts:
-                    if(can_read(request_user_full_id, post) or isOwnPostsAuthor):
-                        posts_response.append(post)
-                response_data = {
-                    "query": "posts",
-                    "count": len(posts_response),
-                    "posts": posts_response
-                }
+                return self.get_local_author_post(request, request_user_full_id, author_id)
+            # front end requesting foreign authors
+            else:
+                try:
+                    parsed_url = urlparse(author_id)
+                    foreign_server = ServerUser.objects.get(host="{}://{}/".format(parsed_url.scheme, parsed_url.netloc))
+                    short_foreign_author_id = author_id.split("author/")[-1]
+                    url = "{}{}author/{}/posts".format(foreign_server.host, foreign_server.prefix, short_foreign_author_id)
+                    headers = {'Content-type': 'application/json'}
 
-                return Response(response_data, status.HTTP_200_OK)
-            except:
-                return Response("Error: Author does not exist!", status.HTTP_400_BAD_REQUEST)
+                    response = requests.get(url,
+                                            auth=(foreign_server.send_username, foreign_server.send_password),
+                                            headers=headers)
+                    return Response("Error: Get foreign author post failed", status.HTTP_400_BAD_REQUEST)
+                except ServerUser.DoesNotExist:
+                    return Response("Author not from allowed host", status.HTTP_400_BAD_REQUEST)
+        # when server make the request
+        elif server_user_exists:
+            request_user_full_id = request.META["HTTP_X_REQUEST_USER_ID"]
+            return self.get_local_author_post(request, request_user_full_id, author_id)
+        else:
+            return Response("Request not from valid server", status.HTTP_400_BAD_REQUEST)
+
+
