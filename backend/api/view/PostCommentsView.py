2d220efbf1fd9c1b1b6c14a1f4fd3cdd96d07be8
htruong1 <htruong1@ualberta.ca>
2019-03-31 23:57:25 -0400

    Able to comment ,server to server

--- backend/api/view/PostCommentsView.py
+++ backend/api/view/PostCommentsView.py
@@ -29,14 +29,18 @@ class PostCommentsView(generics.GenericAPIView):
     def insert_local_comment(self, request):
         post_data = request.data["post"]
         post_data_split = post_data.split("/")
-        # from server
+        print(post_data, "sunglasses")
+        print(post_data_split, "at night")
         post_short_id = post_data_split[-1]
+
+
         local_post_filter = Post.objects.filter(id=str(post_short_id))
+
         if(local_post_filter.exists()):
             author_post = local_post_filter[0]
-
+            print(author_post, "Save e")
             author_data = request.data["comment"]["author"]["id"].split("/")
-
+            print(author_data, "tokyo drift")
             if(can_read(str(author_data[-1]), PostSerializer(author_post).data)):
                 comment_info = request.data["comment"]
                 Comment.objects.create(
@@ -69,6 +73,7 @@ class PostCommentsView(generics.GenericAPIView):
             return Response(response_obj, status.HTTP_400_BAD_REQUEST)
 
     def post(self, request, postid):
+        print("Do i even get a request")
         if (postid == ""):
             return Response("Error: Post ID must be specified", status.HTTP_400_BAD_REQUEST)
 
@@ -78,6 +83,8 @@ class PostCommentsView(generics.GenericAPIView):
         else:
             author_profile_filter = AuthorProfile.objects.filter(user=request.user)
             server_user_exists = ServerUser.objects.filter(user=request.user).exists()
+
+
 
             if(author_profile_filter.exists()):
                 # request is from front end
@@ -92,6 +99,9 @@ class PostCommentsView(generics.GenericAPIView):
                 parsed_post_url = urlparse(request.data["post"])
 
                 post_host = '{}://{}/'.format(parsed_post_url.scheme, parsed_post_url.netloc)
+                print(author_profile.host)
+                print("############3")
+                print(post_host)
                 if(author_profile.host != post_host):
                     # forward the request
                     headers = {'Content-type': 'application/json'}
@@ -112,6 +122,7 @@ class PostCommentsView(generics.GenericAPIView):
                 else:
                     return self.insert_local_comment(request)
             elif(server_user_exists):
+                print("Hello, do i get called here")
                 return self.insert_local_comment(request)
             else:
                 return Response("Error: Request not from allowed hosts", status.HTTP_400_BAD_REQUEST)
