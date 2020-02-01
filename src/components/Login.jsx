2038e962f212f2dd1592449f913526104de1cf76
Maharsh Patel <patel@maharsh.net>
2020-01-31 18:22:13 -0700

    toggle between signup and signin

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,6 +8,7 @@ class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      signup: true,
       username: "",
       password: "",
       passwordReentry: "",
@@ -25,30 +26,55 @@ class Login extends Component {
     // TODO: send auth request here based on login/signup
     console.log("Submit");
   }
+
+  toggleMethod = () => {
+    // toggle between sign-up and sign-in screens
+    this.setState({
+      signup: !this.state.signup
+    })
+  }
   
 
   renderForm() {
+    const primaryButtonText = this.state.signup ? "Sign up" : "Sign in";
+
     return (
       <form onSubmit={this.handleFormSubmit}>
         <div className="login-message">
-          Let's Get Started
+          {
+            this.state.signup ?
+              "Let's Get Started!"
+            : "Welcome Back!"
+          }
         </div>
 
         <input type="text" name="username" className="login-field" placeholder="Username" onChange={this.handleInputChange} />
         <input type="password" name="password" className="login-field" placeholder="Password" onChange={this.handleInputChange} />
-        <input type="password" name="passwordReentry" className="login-field" placeholder="Re-enter Password" onChange={this.handleInputChange} />
 
-        <button className="login-button" type="submit">Sign up</button>
+        {
+          this.state.signup ?
+            <input type="password" name="passwordReentry" className="login-field" placeholder="Re-enter Password" onChange={this.handleInputChange} />
+          : null
+        }
+
+        <button className="login-button" type="submit">{primaryButtonText}</button>
       </form>
     );
   }
 
   renderSecondaryOption() {
+    let secondaryOptionText = "Already have account?";
+    let secondaryButtonText = "Sign in";
+    if (!this.state.signup) {
+      secondaryOptionText = "Don't have an account?";
+      secondaryButtonText = "Sign up";
+    }
+
     return(
       <div className="login-secondary-options">
-        <span className="login-secondary-options-text"> Already have an account?</span>
+        <span className="login-secondary-options-text">{secondaryOptionText}</span>
 
-        <button className="login-secondary-button" type="submit">Sign in</button>
+        <button className="login-secondary-button" type="submit" onClick={this.toggleMethod}>{secondaryButtonText}</button>
       </div>
     )
   }
