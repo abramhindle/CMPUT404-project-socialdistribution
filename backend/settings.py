fad7ba9560112dd1c01d2661399d93e98b0d30df
Yuxuan Zhao <yzhao9@ualberta.ca>
2020-02-02 23:16:57 -0700

    remove password common check (#48)

--- backend/settings.py
+++ backend/settings.py
@@ -112,9 +112,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
