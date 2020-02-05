015346107721031a4b29a7a52bdc30401125fb55
Roy Chowdhury <konishky@ualberta.ca>
2020-02-04 19:16:00 -0700

    updated more models for django. Not connected yet

--- backend/models.py
+++ backend/models.py
@@ -13,20 +13,37 @@ class User(AbstractUser):
     # Using: username, password, first_name, last_name, email inherited from Abstractuser
 
 class Post(models.Model):
-    post_id = models.UUIDField(primary_key=True,default=uuid.uuid4, editable=False, unique=True )
+    postId = models.UUIDField(primary_key=True,default=uuid.uuid4, editable=False, unique=True )
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title =models.CharField(max_length=50)
     timestamp = models.DateTimeField(auto_now_add=True)
-    description = models.CharField(max_length=250)
+    description = models.CharField(max_length=450)
     # default posts are public
-    ispublic = models.BooleanField(default=True)
+    isPublic = models.BooleanField(default=True)
 
 
-class post_access(models.Model):
+class PostAccess(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=CASCADE)
-    accessID = models.ForeignKey(User, on_delete=CASCADE)
+    accessId = models.ForeignKey(User, on_delete=CASCADE)
 
 
 class Comments(models.Model):
-    comment_id = models.UUIDField(primary_key=True, uuid=uuid.uuid4, editable=False, unique=True)
+    commentId = models.UUIDField(primary_key=True, uuid=uuid.uuid4, editable=False, unique=True)
+    content = models.CharField(max_length=250)
+    post = models.ForeignKey(Post, on_delete=CASCADE)
+    postedBy = models.ForeignKey(User, on_delete=CASCADE)
+    postedTo = models.ForeignKey(User, on_delete=CASCADE)
+
+class FriendRequest(models.Model):
+    fromUser = models.ForeignKey(User, on_delete=CASCADE)
+    toUser = models.ForeignKey(User, on_delete=CASCADE)
+    isAccepted = models.BooleanField(default=False)
+    sentDate = models.DateTimeField(auto_now_add=True)
+
+class Friend(models.Model):
+    fromUser = models.ForeignKey(User, on_delete=CASCADE)
+    toUser= models.ForeignKey(User, on_delete=CASCADE)
+    friendDate = models.DateTimeField(auto_now_add=True)
+    # unfriend date
+    unfriendDate = models.DateTimeField(null=True, blank=True)
