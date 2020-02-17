437a13d886ac235b231828e62ab394ed130cc629
Student <you@example.com>
2020-02-17 12:17:51 -0500

    testing

---
+++ myblog/src/User.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { DatePicker, message } from 'antd';
+
+
+class User extends React.Component {
+  state = {
+    date: null,
+  };
+
+  handleChange = date => {
+    message.info(`Selected Date: ${date ? date.format('YYYY-MM-DD') : 'None'}`);
+    this.setState({ date });
+  };
+
+  render() {
+    const { date } = this.state;
+    return (
+      <div style={{ width: 400, margin: '100px auto' }}>
+        <DatePicker onChange={this.handleChange} />
+        <div style={{ marginTop: 20 }}>
+          Selected Date: {date ? date.format('YYYY-MM-DD') : 'None'}
+        </div>
+      </div>
+    );
+  }
+}
+    
+export default User;
