76f41a94b478d1cce0d1f73cf226412e350b2456
Yuan Wang <wang17@ualberta.ca>
2020-02-23 23:12:37 -0700

    add handler to update author profile

--- mysite/user/views.py
+++ mysite/user/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .serializers import AuthorSerializer
 from rest_framework import viewsets
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import User
 # Create your views here.
@@ -19,5 +20,13 @@ class AuthorViewSet(viewsets.ModelViewSet):
     # only return the records of author
     def get_queryset(self):
         return self.queryset.filter(is_superuser=0)
+
+    # used to update the author's profile
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = AuthorSerializer(instance=instance,data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
 
     
