e47315e16765039b78f092247b6e63cf3dec83bf
Devin <devin@DESKTOP-OL5E28E.localdomain>
2020-02-25 22:54:13 -0700

    new functionalities image

--- myblog/src/UserSelf.js
+++ myblog/src/UserSelf.js
@@ -3,7 +3,8 @@ import 'antd/dist/antd.css';
 import { List, Avatar, Menu, Icon, Layout } from 'antd';
 import { Button} from 'antd';
 import { Input, Modal } from 'antd';
-
+import SimpleReactLightbox from "simple-react-lightbox";
+import { SRLWrapper } from "simple-react-lightbox"; 
 const { Header } = Layout;
 const { confirm } = Modal;
 
@@ -24,7 +25,7 @@ for (let i = 0; i < 50; i++) {
 
 function showDeleteConfirm() {
   confirm({
-    title: 'Are you sure delete this post?',
+    title: 'Are you sure you want to delete this post?',
     okText: 'Yes',
     okType: 'danger',
     cancelText: 'No',
@@ -127,10 +128,19 @@ class UserSelf extends React.Component {
                           <IconText type="message" text="0" key="list-vertical-message" />,
                           ]}
                           extra={
-                          <img
-                              width={272}
-                              alt=""
-                              src="https://i.pinimg.com/originals/1f/53/25/1f53250c9035c9d657971712f6b38a99.jpg"/>
+                            <SimpleReactLightbox>
+                              <SRLWrapper>
+                                <img
+                                  width={250}
+                                  alt=""
+                                  src="https://wallpaperaccess.com/full/628286.jpg"/>
+                                <img
+                                width={250}
+                                alt=""
+                                src="https://i.pinimg.com/originals/1f/53/25/1f53250c9035c9d657971712f6b38a99.jpg"/> 
+
+                              </SRLWrapper> 
+                            </SimpleReactLightbox>
                           }
                       >
                       <List.Item.Meta
