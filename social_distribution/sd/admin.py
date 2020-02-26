9f36d2f76c1376d3eee916ffe90bdd7bf77dd54f
Austin Goebel <argoebel@ualberta.ca>
2020-02-25 21:18:55 -0700

    Created models for projectpart1

--- social_distribution/sd/admin.py
+++ social_distribution/sd/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Author, Post, Comment
 
 # Register your models here.
+admin.site.register(Author)
+admin.site.register(Post)
+admin.site.register(Comment)
--- social_distribution/sd/models.py
+++ social_distribution/sd/models.py
@@ -1,3 +1,30 @@
+from rest_framework.authtoken.models import Token
 from django.db import models
+from django.contrib.auth.models import AbstractUser, User
+import uuid
 
 # Create your models here.
+
+
+class Author(AbstractUser):
+    # Using username, password from AbstractUser
+    uuid = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    stream = models.CharField(max_length=500)
+    # Still need to add relationships between users
+
+
+class Post(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    body = models.CharField(max_length=5000)
+    date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=10)
+    link_to_image = models.CharField(max_length=100)
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    body = models.CharField(max_length=5000)
+    date = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
