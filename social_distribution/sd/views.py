caabe44dd7e4893af7db4f3da7f92f8259ced090
wlt30 <wlt@ualberta.ca>
2020-02-27 16:34:24 -0700

    adding pages to views.py

--- social_distribution/sd/views.py
+++ social_distribution/sd/views.py
@@ -1,4 +1,25 @@
 from django.http import HttpResponse
 
-def index(request):
-	return HttpResponse("Social Distribution Home Page")
+def login(request):
+	return HttpResponse("Login Page")
+
+def create_account(request):
+	return HttpResponse("Create Account Page")
+
+def forget_pass(request):
+	return HttpResponse("Forgotten Password Page")
+
+def home(request):
+	return HttpResponse("Home Page")
+
+def search(request):
+	return HttpResponse("User Search Page")
+
+def friends(request):
+	return HttpResponse("Friends Page")
+
+def requests(request):
+	return HttpResponse("Friend Requests Page")
+
+def account(request):
+	return HttpResponse("Your Account Page")
