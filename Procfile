d5d2a3c7f2eff7f464639529ca3f57803bf7b804
jcpoulin <jcpoulin@ualberta.ca>
2020-02-28 11:45:07 -0700

    Added Procfile and other django-heroku modules

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn social_distribution.social_distribution.settings
--- social_distribution/social_distribution/settings.py
+++ social_distribution/social_distribution/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django-heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -128,3 +129,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = "sd.Author"
+
+django_heroku.settings(locals())
