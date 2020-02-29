e7e4a8b4a21278e2ebf79cf8da28c337e096c877
Yuan Wang <wang17@ualberta.ca>
2020-02-28 21:18:24 -0700

    update friend viewsets

--- mysite/friend/urls.py
+++ mysite/friend/urls.py
@@ -1,11 +1,12 @@
 from rest_framework import routers
 from django.urls import path, include
-from .views import FriendViewSet
+from .views import FriendViewSet,FriendRequestViewSet
 
-router = routers.DefaultRouter()
-router.register("friendRequest", FriendViewSet, basename="friend")
+router = routers.SimpleRouter()
 
+router.register("myFriends", FriendViewSet, basename="friend")
+router.register("friendRequest", FriendRequestViewSet, basename="friend")
 
 urlpatterns = [
-    path("", include(router.urls)),
+    path("",include(router.urls))
 ]
--- mysite/friend/views.py
+++ mysite/friend/views.py
@@ -15,9 +15,14 @@ class FriendViewSet(viewsets.ModelViewSet):
     serializer_class = FriendSerializer
     permission_classes = [IsAdminUser | OwnerPermissions]
     lookup_field = "id"
+    queryset = Friend.objects.filter(status='A')
 
-    def get_queryset(self):
-        return self.request.user.f1Ids.all()
+
+class FriendRequestViewSet(viewsets.ModelViewSet):
+    serializer_class = FriendSerializer
+    permission_classes = [IsAdminUser | OwnerPermissions]
+    lookup_field = "id"
+    queryset = Friend.objects.filter(status='U')
 
     def perform_create(self, serializer):
         serializer.save(f1Id=self.request.user)
