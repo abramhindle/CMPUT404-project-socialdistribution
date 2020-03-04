9c82e1e8b2bf2eb90323393f0d54bf88c4d19be1
wlt30 <wlt@ualberta.ca>
2020-03-03 19:06:28 -0700

    updated urls and removed extras

--- social_distribution/sd/urls.py
+++ social_distribution/sd/urls.py
@@ -6,16 +6,15 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login',views.login, name='login'),
     path('create_account', views.create_account, name='create_account'),
-    path('forgot_pass',views.forgot_pass, name='forgot_pass'),
-    path('home', views.home, name='home'),
-    path('search',views.search, name='search'),
-    path('friends', views.friends, name='friends'),
     path('requests',views.requests, name='requests'),
-    path('account', views.account, name='account'),
-
     path('author/posts', views.feed, name='private_feed'),
     path('posts', views.explore, name='explore'),
     path('author/<int:author_id>/posts', views.author, name='author_page'),
+    path('posts/<int:post_id>', views.post, name='post'),
+    path('posts/<int:post_id>/comments', views.post_comment, name='post_comment'),
 
-
+    # """Optional Pages"""
+    # path('search',views.search, name='search'),
+    # path('friends', views.friends, name='friends'),
+    # path('account', views.account, name='account'),
 ]
--- social_distribution/sd/views.py
+++ social_distribution/sd/views.py
@@ -4,15 +4,8 @@ from .models import *
 from django.utils import timezone
 import os
 
-BASE = "127.0.0.1/8000/sd/"
-
 def index(request):
-	"""
-	# check cookie for logged in  and user details
-	# if logged in, go to /me/posts
-	# else go to a global list of public posts 
-	"""
-	return redirect('home', permanent=True)
+	return redirect('explore', permanent=True)
 
 def login(request):
 	return HttpResponse("Login Page")
@@ -20,32 +13,34 @@ def login(request):
 def create_account(request):
 	return HttpResponse("Create Account Page")
 
-def forgot_pass(request):
-	return HttpResponse("Forgotten Password Page")
-
-def home(request):
-	feed = Post.objects.all()
-	page = 'sd/index.html'
-	return render(request, page, {'feed':feed})
-
-def search(request):
-	return HttpResponse("User Search Page")
-
-def friends(request):
-	return HttpResponse("Friends Page")
-
 def requests(request):
 	return HttpResponse("Friend Requests Page")
-
-def account(request):
-	return HttpResponse("Your Account Page")
 
 def feed(request):
 	return HttpResponse("Your Feed")
 
 def explore(request):
-	return HttpResponse("Public Posts")
+	feed = Post.objects.all()
+	page = 'sd/index.html'
+	return render(request, page, {'feed':feed})
 
 def author(request, author_id):
 	author = Author.objects.get(uuid=author_id)
 	return HttpResponse(author.username+"'s Page")
+
+def post(request, post_id):
+	post = Post.objects.get(uuid=post_id)
+	return HttpResponse(post)
+
+def post_comment(request, post_id):
+	post = Comment.objects.get(post=post_id)
+	return HttpResponse(post)
+
+# def search(request):
+# 	return HttpResponse("User Search Page")
+
+# def friends(request):
+# 	return HttpResponse("Friends Page")
+
+# def account(request):
+# 	return HttpResponse("Your Account Page")
