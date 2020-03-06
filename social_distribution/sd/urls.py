df8c4accb88bfb37511b86f4a28357f0857655ef
wlt30 <wlt@ualberta.ca>
2020-03-06 16:01:15 -0700

    handling path queries

--- social_distribution/sd/urls.py
+++ social_distribution/sd/urls.py
@@ -9,12 +9,12 @@ urlpatterns = [
     path('requests',views.requests, name='requests'),
     path('author/posts', views.feed, name='private_feed'),
     path('posts', views.explore, name='explore'),
-    path('author/<int:author_id>/posts', views.author, name='author_page'),
+    path('author/<uuid:author_id>/posts', views.author, name='author_page'),
     path('posts/<int:post_id>', views.post, name='post'),
     path('posts/<int:post_id>/comments', views.post_comment, name='post_comment'),
+    path('author/<uuid:author_id>/friends', views.friends, name='friends')
 
     # """Optional Pages"""
     # path('search',views.search, name='search'),
-    # path('friends', views.friends, name='friends'),
     # path('account', views.account, name='account'),
 ]
--- social_distribution/sd/views.py
+++ social_distribution/sd/views.py
@@ -3,6 +3,19 @@ from django.shortcuts import render, redirect
 from .models import *
 from django.utils import timezone
 import os
+import pdb
+
+def get_page_info(request):
+	path = request.get_full_path()
+	num = path.split('page=')[1]
+	page_num=1
+	size=10
+	if('&' in num):
+		page_num = num.split('&')[0]
+	temp = path.split('size=')
+	if len(temp)>1:
+		size=temp[1]
+	return page_num, size
 
 def index(request):
 	return redirect('explore', permanent=True)
@@ -13,34 +26,38 @@ def login(request):
 def create_account(request):
 	return HttpResponse("Create Account Page")
 
-def requests(request):
+def requests(request, page_num=1, size=10):
 	return HttpResponse("Friend Requests Page")
 
-def feed(request):
+def feed(request, page_num=1, size=10):
 	return HttpResponse("Your Feed")
 
 def explore(request):
 	feed = Post.objects.all()
+	page_num, size = get_page_info(request)
+	print("PAGE NUMBER:", page_num)
+	print('SIZE:', size)
 	page = 'sd/index.html'
 	return render(request, page, {'feed':feed})
 
-def author(request, author_id):
+def author(request, author_id, page_num=1, size=10):
 	author = Author.objects.get(uuid=author_id)
 	return HttpResponse(author.username+"'s Page")
 
-def post(request, post_id):
+def post(request, post_id, page_num=1, size=10):
 	post = Post.objects.get(uuid=post_id)
 	return HttpResponse(post)
 
-def post_comment(request, post_id):
+def post_comment(request, post_id, page_num=1, size=10):
 	post = Comment.objects.get(post=post_id)
 	return HttpResponse(post)
+
+def friends(request, page_num=1, size=10):
+	return HttpResponse("Friends Page")
 
 # def search(request):
 # 	return HttpResponse("User Search Page")
 
-# def friends(request):
-# 	return HttpResponse("Friends Page")
 
 # def account(request):
 # 	return HttpResponse("Your Account Page")
