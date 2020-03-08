3fe29f54189af28eced90d805389edb5e6c9821f
XiaoleZ <xiaole2@ualberta.ca>
2020-03-07 22:44:09 -0700

    Add http error page: 404 403 500

--- socialdistribution/socialdistribution/views.py
+++ socialdistribution/socialdistribution/views.py
@@ -13,3 +13,19 @@ def index_register(request):
     template ='login/register.html'
 
     return render(request,template,{})
+
+# the http error page
+def error_404(request):
+    template ='404.html'
+
+    return render(request,template,{})
+
+def error_403(request):
+    template ='403.html'
+
+    return render(request,template,{})
+
+def error_500(request):
+    template ='500.html'
+
+    return render(request,template,{})
