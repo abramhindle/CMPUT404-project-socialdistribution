ee0b5a57606d0b6679913df7342f4cc8b9ee65bb
willfenton <wfenton@ualberta.ca>
2020-03-08 16:51:03 -0600

    Made url path names more clear

--- socialdistribution/api/urls.py
+++ socialdistribution/api/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("posts", views.all_posts, name="api_all_posts"),
-    path("posts/<uuid:post_id>", views.single_post, name="api_single_post"),
-    path("posts/<uuid:post_id>/comments", views.post_comments, name="api_post_comments"),
-    path("author/<uuid:author_id>/posts", views.author_posts, name="api_author_posts"),
+    path("posts", views.all_posts, name="api_get_all_public_posts"),
+    path("posts/<uuid:post_id>", views.single_post, name="api_get_single_post"),
+    path("posts/<uuid:post_id>/comments", views.post_comments, name="api_get_post_comments"),
+    path("author/<uuid:author_id>/posts", views.author_posts, name="api_get_author_posts"),
 ]
