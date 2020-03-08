de48cab9409d156322013406bd8113eda6307799
Annabelle Zhang <qiaoyan1@ualberta.ca>
2020-03-07 18:58:33 -0700

    add header to postInput page

--- myblog/src/PostInput.js
+++ myblog/src/PostInput.js
@@ -4,6 +4,8 @@ import './index.css';
 import { Form, Input, Button, Upload, Modal, Icon, Radio} from 'antd';
 import axios from 'axios';
 import './components/PostInput.css';
+import './components/Header.css'
+import AuthorHeader from './components/AuthorHeader'
 
 const { TextArea } = Input;
 
@@ -142,93 +144,96 @@ class PostInput extends React.Component {
         
 
         return(
-            <div className={'postInput'} style={{display: 'flex',  justifyContent:'center'}} >
-              <Form {...formItemLayout}>
-                
-              <Form.Item>
-                  {getFieldDecorator("postTitle", {
-                    rules: [
-                      {
-                        required: true,
-                        message: "Enter your title here",
-                        whitespace: true
-                      }
-                    ]
-                  })(<Input placeholder="Enter your title here"/>)}
-                </Form.Item>
-                
+            <div>
+              <AuthorHeader/>
+              <div className={'postInput'} style={{display: 'flex',  justifyContent:'center'}} >
+                <Form {...formItemLayout}>
+                    
                 <Form.Item>
-                  {getFieldDecorator("postContent", {
-                    rules: [
-                      {
-                        required: true,
-                        message: "Enter your post body here",
-                        whitespace: true
-                      }
-                    ]
-                  })(<TextArea rows={13} placeholder="Enter your post body here"/>)}
-                </Form.Item>
-        
-                <Form.Item>
-                  {getFieldDecorator("Visibility", {
-                    rules: [
-                      {
-                        required: true,
-                      },
-                    ]
-                  })( <Radio.Group>
-                        <Radio.Button value="PUBLIC">Public</Radio.Button>
-                        <Radio.Button value="FRIENDS">Friends</Radio.Button>
-                        <Radio.Button value="FOAF">Friends to friends</Radio.Button>
-                        <Radio.Button value="PRIVATE">Private</Radio.Button>
-                      </Radio.Group>)}
-                </Form.Item>
+                    {getFieldDecorator("postTitle", {
+                        rules: [
+                        {
+                            required: true,
+                            message: "Enter your title here",
+                            whitespace: true
+                        }
+                        ]
+                    })(<Input placeholder="Enter your title here"/>)}
+                    </Form.Item>
+                    
+                    <Form.Item>
+                    {getFieldDecorator("postContent", {
+                        rules: [
+                        {
+                            required: true,
+                            message: "Enter your post body here",
+                            whitespace: true
+                        }
+                        ]
+                    })(<TextArea rows={13} placeholder="Enter your post body here"/>)}
+                    </Form.Item>
+            
+                    <Form.Item>
+                    {getFieldDecorator("Visibility", {
+                        rules: [
+                        {
+                            required: true,
+                        },
+                        ]
+                    })( <Radio.Group>
+                            <Radio.Button value="PUBLIC">Public</Radio.Button>
+                            <Radio.Button value="FRIENDS">Friends</Radio.Button>
+                            <Radio.Button value="FOAF">Friends to friends</Radio.Button>
+                            <Radio.Button value="PRIVATE">Private</Radio.Button>
+                        </Radio.Group>)}
+                    </Form.Item>
 
-                <Form.Item>
-                  {getFieldDecorator("Type", {
-                    rules: [
-                      {
-                        required: true,
-                      },
-                    ]
-                  })(<Radio.Group>
-                        <Radio.Button value="text/plain">Plain Text</Radio.Button>
-                        <Radio.Button value="text/markdown">Markdown</Radio.Button>
-                      </Radio.Group>
-                  )}
-                </Form.Item>
-        
+                    <Form.Item>
+                    {getFieldDecorator("Type", {
+                        rules: [
+                        {
+                            required: true,
+                        },
+                        ]
+                    })(<Radio.Group>
+                            <Radio.Button value="text/plain">Plain Text</Radio.Button>
+                            <Radio.Button value="text/markdown">Markdown</Radio.Button>
+                        </Radio.Group>
+                    )}
+                    </Form.Item>
+            
 
-                <Form.Item>
-                  {getFieldDecorator("imageUpload", {
-                    rules: [
-                      {
-                        required: false,
-                      },
-                    ]
-                  })(<div><Upload
-                      action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-                      listType="picture-card"
-                      fileList={fileList}
-                      onPreview={this.handlePreview}
-                      onChange={this.handleChange}
-                    >
-                      {fileList.length >= 8 ? null : uploadButton}
-                    </Upload>
-                    <Modal visible={previewVisible} footer={null} onCancel={this.handleCancel}>
-                        <img alt="example" style={{ width: '100%' }} src={previewImage} />
-                    </Modal></div>
-                  )}
-                </Form.Item>
+                    <Form.Item>
+                    {getFieldDecorator("imageUpload", {
+                        rules: [
+                        {
+                            required: false,
+                        },
+                        ]
+                    })(<div><Upload
+                        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                        listType="picture-card"
+                        fileList={fileList}
+                        onPreview={this.handlePreview}
+                        onChange={this.handleChange}
+                        >
+                        {fileList.length >= 8 ? null : uploadButton}
+                        </Upload>
+                        <Modal visible={previewVisible} footer={null} onCancel={this.handleCancel}>
+                            <img alt="example" style={{ width: '100%' }} src={previewImage} />
+                        </Modal></div>
+                    )}
+                    </Form.Item>
 
 
 
-                <Form.Item {...tailFormItemLayout}>
-                  <Button type="primary" htmlType="button" onClick={this.handleSubmit}>
-                        Post it
-                  </Button>
-                </Form.Item>
-              </Form>
+                    <Form.Item {...tailFormItemLayout}>
+                    <Button type="primary" htmlType="button" onClick={this.handleSubmit}>
+                            Post it
+                    </Button>
+                    </Form.Item>
+                </Form>
+              </div>
             </div>
 
         )
