91f08de2c7f46656ea8b954765a7cdf70c67f8cb
HumphreyLu6 <zlu@ualberta.ca>
2020-03-09 00:26:39 -0600

    add user tests

--- mysite/user/tests.py
+++ mysite/user/tests.py
@@ -2,31 +2,91 @@ import json
 from pprint import pprint
 from django.test import TestCase, RequestFactory
 from django.shortcuts import render
-from rest_framework.test import APIRequestFactory
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
 from .models import User
 from .views import AuthorViewSet
 
-
+SERVER_HOST = "http://127.0.0.1:8000"
 # Create your tests here.
-class UserTestCase(TestCase):
+class UserTestCase(APITestCase):
     def setUp(self):
-        self.factory = APIRequestFactory()
-        self.factory.post(
-            "/api/user/signup/",
-            {
-                "username": "user1",
-                "email": "user1@email.com",
-                "password1": "passqweruser1",
-                "password2": "passqweruser2",
-            },
+        self.user = User.objects.create_user(
+            email="user1@email.com", username="user1", password="passqweruser1",
         )
+        self.token = Token.objects.create(user=self.user)
 
-    def test_register_user(self):
-        request = self.factory.get("/api/user/author/")
-        response = AuthorViewSet.as_view({"get": "list"})(request, username="user1")
+    def test_create_user(self):
+        request_1_body = {
+            "username": "user2",
+            "email": "user2@email.com",
+            "password1": "passqweruser2",
+            "password2": "passqweruser2",
+        }
+        response = self.client.post("/api/user/signup/", request_1_body)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)  # user1 created
+
+        request_2_body = {
+            "email": request_1_body["email"],
+            "password": request_1_body["password1"],
+        }
+        response = self.client.post("/api/user/login/", request_2_body)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_username_conflict(self):
+        request_body = {
+            "username": self.user.username,
+            "email": self.user.email,
+            "password1": self.user.password,
+            "password2": self.user.password,
+        }
+        response = self.client.post("/api/user/signup/", request_body)
+        self.assertEqual(
+            response.status_code, status.HTTP_400_BAD_REQUEST
+        )  # user1 existed
+
+    def test_list_user(self):
+        response = self.client.get("/api/user/author/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_retrieve_user(self):
+        response = self.client.get("/api/user/author/user1/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         response = response.render()
-        print(response)
-        self.assertEqual(response.status_code, 200)
-        user_dict = json.loads(response.content)
-        print(user_dict)
-        self.assertEqual(user_dict["email"], self.user1.email)
+        data = json.loads(json.dumps(response.data))
+        self.assertEqual(data["email"], self.user.email)
+
+    def test_login_user(self):
+        # request_body = {
+        #     "email": self.user.email,
+        #     "password": self.user.password,
+        # }
+        # response = self.client.post("/api/user/login/", request_body)
+        # pprint(vars(response))
+        # self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # self.assertEqual(
+        #     self.client.login(email=self.user.email, password=self.user.password), True,
+        # )
+        pass
+
+    def test_update_user(self):
+        request_body = {
+            "bio": "Hi, I am user1",
+        }
+        response = self.client.patch(
+            "/api/user/author/user1/",
+            request_body,
+            HTTP_AUTHORIZATION="Token " + self.token.key,
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response = response.render()
+        data = json.loads(json.dumps(response.data))
+        self.assertEqual(data["bio"], request_body["bio"])
+
+    def test_delete_user(self):
+        response = self.client.delete(
+            "/api/user/author/user1/", HTTP_AUTHORIZATION="Token " + self.token.key,
+        )
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
