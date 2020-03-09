22a95dcdf80c975034cc86e027ffc5555e0fff45
Yuan Wang <wang17@ualberta.ca>
2020-03-09 15:42:54 -0600

    code clean

--- mysite/post/views.py
+++ mysite/post/views.py
@@ -38,7 +38,6 @@ class PostsViewSet(viewsets.ModelViewSet):
                 "f1Id", flat=True
             )
             friends_usernames = list(user_f2_ids) + list(user_f1_ids)
-            print(friends_usernames)
             f2_foaf = Friend.objects.filter(
                 Q(status="A") | Q(f1Id__in=list(friends_usernames))
             ).values_list("f2Id", flat=True)
@@ -46,7 +45,6 @@ class PostsViewSet(viewsets.ModelViewSet):
                 Q(status="A") | Q(f2Id__in=list(friends_usernames))
             ).values_list("f1Id", flat=True)
             foaf = list(f1_foaf) + list(f2_foaf)
-            print(foaf)
             q2_1 = Q(visibility="FOAF")
             q2_2 = Q(author__username__in=foaf)
 
