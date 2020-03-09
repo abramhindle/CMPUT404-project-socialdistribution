94e07eb08ae1fe981c62be1e00a996abf0a4bb69
HumphreyLu6 <zlu@ualberta.ca>
2020-03-08 18:34:54 -0600

    save user test

--- mysite/user/tests.py
+++ mysite/user/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from .models import User
 
 # Create your tests here.
+class UserTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_users_existence(self):
+        user1 = User.objects.get(username="user1")
+        self.assertEqual(user1.email, "user1@email.com")
