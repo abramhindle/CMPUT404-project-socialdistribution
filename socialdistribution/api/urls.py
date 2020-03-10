2cf149909f42f0dc5dbcc6bbd45491265f3345ce
willfenton <wfenton@ualberta.ca>
2020-03-10 10:07:05 -0600

    /author/posts endpoint

--- socialdistribution/api/urls.py
+++ socialdistribution/api/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("posts", views.posts, name="api_get_all_public_posts"),
     path("posts/<uuid:post_id>", views.single_post, name="api_get_single_post"),
     path("posts/<uuid:post_id>/comments", views.post_comments, name="api_get_post_comments"),
-    path("author/<uuid:author_id>/posts", views.author_posts, name="api_get_author_posts"),
+    path("author/<uuid:author_id>/posts", views.specific_author_posts, name="api_get_specific_author_posts"),
+    path("author/posts", views.author_posts, name="api_get_author_posts")
 ]
