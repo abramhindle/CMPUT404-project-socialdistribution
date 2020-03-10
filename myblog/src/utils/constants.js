af25dd0f59d201354cabd0e40cfdaf70c6aa4048
Isaac <36417130+PoopBear1@users.noreply.github.com>
2020-03-10 17:49:54 -0600

    Update constants.js

--- myblog/src/utils/constants.js
+++ myblog/src/utils/constants.js
@@ -7,3 +7,4 @@ export const AUTHOR_API = HOST+"/api/user/author/";
 export const FETCH_POST_API = HOST+"/api/user/author/current_user/";
 export const FRIEND_API = HOST+"/api/friend/my_friends/";
 export const FRIEND_REQUEST_API=HOST+"/api/friend/friend_request/"; 
+export const TOKEN_API = HOST+'/api/user/author/current_user/';
