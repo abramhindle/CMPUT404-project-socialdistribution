bc222718f4e0779c18dc89f476404a19721e3d19
JohnDoeMask <heyuehuang@hotmail.com>
2020-03-11 14:46:19 -0600

    Edit post

--- socialdistribution/posts/views.py
+++ socialdistribution/posts/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Post, Comment
-from .forms import CommentForm
+from .forms import CommentForm, PostForm
 from django.contrib.auth.decorators import login_required
+import base64
 
 
 @login_required
@@ -51,5 +53,37 @@ def view_post(request, post_id):
         'comments': comments,
         'comment_form': comment_form,
     }
+
+    return render(request, template, context)
+
+@login_required
+def edit_post(request, post_id):
+    author = request.user
+    template = 'posts/posts_edit.html'
+    post = Post.objects.get(id=post_id)
+
+    form = PostForm(request.POST or None, request.FILES or None,
+                       instance=post)
+    # print(form)
+
+    print(post.content_type)
+    context = {
+        'form': form,
+        'author': author,
+    }
+
+    if request.method == 'POST':
+        # print(form)
+        if form.is_valid():
+            new_content = form.save(commit=False)
+            cont_type = form.cleaned_data['content_type']
+            if(cont_type == "image/png;base64" or cont_type == "image/jpeg;base64"):
+                img = form.cleaned_data['image_file']
+                new_content.content = (base64.b64encode(img.file.read())).decode("utf-8")
+            print(new_content)
+            new_content.save()
+            url = reverse('details', kwargs={'post_id': post.id})
+            return HttpResponseRedirect(url)
+
 
     return render(request, template, context)
