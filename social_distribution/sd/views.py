341a94f4302eb3f879866579db061e80d9e98904
wlt30 <wlt@ualberta.ca>
2020-03-12 18:24:58 -0600

    adding print statements before every redirect

--- social_distribution/sd/views.py
+++ social_distribution/sd/views.py
@@ -18,9 +18,9 @@ def index(request):
 
 def explore(request):
     if valid_method(request):
+        result = paginated_result(all_posts, request, "feed", query="feed")
         if authenticated(request):
             all_posts = Post.objects.all()
-            result = paginated_result(all_posts, request, "feed", query="feed")
             return render(request, 'sd/index.html', {'current_user': get_current_user(request), 'authenticated': True, 'result': result})
         else:
             return render(request, 'sd/index.html', {'current_user': None, 'authenticated': False, 'result': result})
@@ -48,6 +48,7 @@ def account(request):
             page = 'sd/account.html'
             return render(request, page, {'current_user': user, 'authenticated': True})
         else:
+            print("Redirecting from Account because no one is logged in")
             return redirect('login')
     else:
         return HttpResponse(status_code=405)
@@ -55,14 +56,22 @@ def account(request):
 
 def search(request):
     if valid_method(request):
-        return render(request, 'sd/search.html')
+        if authenticated(request):
+            return render(request, 'sd/search.html')
+        else:
+            print("Redirecting from Search because no one is logged in")
+            return redirect('login')
     else:
         return HttpResponse(status_code=405)
 
 
 def notifications(request):
     if valid_method(request):
-        return render(request, 'sd/notifications.html')
+        if authenticated(request):
+            return render(request, 'sd/notifications.html')
+        except:            
+            print("Redirecting from Notifications because no one is logged in")
+            return redirect('login')
     else:
         return HttpResponse(status_code=405)
 
@@ -83,11 +92,10 @@ def login(request):
                 request.session.pop('auth-user')
                 request.session.flush()
             except KeyError as k:
-                print("Not currently authenticated, returning to feed")
+                print(k)
 
         if request.method == "GET":
             return render(request, 'sd/login.html', {'current_user': None, 'authenticated': False})
-
         info = request._post
         user_name = info['username']
         pass_word = info['password']
@@ -95,24 +103,36 @@ def login(request):
             user = Author.objects.get(username=user_name)
         except:
             request.session['authenticated'] = False
+            print(user_name+" not found, please try again")
             return redirect('login')
 
         if (pass_word != user.password) and not (check_password(pass_word, user.password)):
+            print("Incorrect password for "+user_name+", please try again")
             return redirect('login')
 
         request.session['authenticated'] = True
-        key = Author.objects.get(username=user_name).uuid
+        user = Author.objects.get(username=user_name) 
+        key = user.uuid
         request.session['auth-user'] = str(key)
         request.session['SESSION_EXPIRE_AT_BROWSER_CLOSE'] = True
+        print(user.username+" successfully logged in, redirecting to feed")
         return redirect('my_feed')
     else:
         return HttpResponse(status_code=405)
 
 def register(request):
     if valid_method(request):  
+        if authenticated(request):
+            print("Logging out "+ get_current_user(request).username)
+            try:
+                request.session['authenticated'] = False
+                request.session.pop('auth-user')
+                request.session.flush()
+            except KeyError as k:
+                print(k)
+
         if request.method == "GET":
             return render(request, 'sd/register.html', {'current_user': None, 'authenticated': False})
-
         info = request._post
         serializer = CreateAuthorSerializer(data=info)
         if serializer.is_valid():
@@ -122,6 +142,7 @@ def register(request):
             key = user.uuid
             request.session['auth-user'] = str(key)
             request.session['SESSION_EXPIRE_AT_BROWSER_CLOSE'] = True
+            print(user.username+" successfully registered! Redirecting to your feed")
             return redirect('my_feed')
         else:
             return render(request, 'sd/register.html', {'current_user': None, 'authenticated': False} )
@@ -138,6 +159,7 @@ def logout(request):
             except KeyError as k:
                 print("Not currently authenticated, returning to feed")
         else:
+            print("No one currently logged in, redirecting to explore")
             return redirect('explore')
     else:
         return HttpResponse(status_code=405)
@@ -146,6 +168,7 @@ def logout(request):
 def new_post(request):
     if valid_method(request):
         if not authenticated(request):
+            print("Please login to create posts, redirecting to login page")
             return redirect('login')
 
         user = get_current_user(request)
@@ -163,7 +186,7 @@ def new_post(request):
             if serializer.is_valid():
                 serializer.save()
                 page = 'sd/feed.html'
-                print('POST SUCCESSFUL')
+                print('POST SUCCESSFUL. Redirecting to your feed.')
                 return redirect('my_feed')
             else:
                 form = NewPostForm()
