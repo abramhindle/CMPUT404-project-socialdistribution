2508e3efcf79b5d593100aab99f675c25bb5f2b2
Austin Goebel <argoebel@ualberta.ca>
2020-03-22 15:01:06 -0600

    added Node model

--- social_distribution/sd/admin.py
+++ social_distribution/sd/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Author, Post, Comment, FriendRequest, Follow, Friend
+from .models import Author, Post, Comment, FriendRequest, Follow, Friend, Node
 
 # Register your models here.
 admin.site.register(Author)
@@ -8,3 +8,4 @@ admin.site.register(Comment)
 admin.site.register(FriendRequest)
 admin.site.register(Follow)
 admin.site.register(Friend)
+admin.site.register(Node)
