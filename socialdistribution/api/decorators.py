d98c5abd0eb8809a00e490f3a6701014a186ed6b
cmput274 <gregdraghici@gmail.com>
2020-03-22 14:57:36 -0400

    Removed try/except block

--- socialdistribution/api/decorators.py
+++ socialdistribution/api/decorators.py
@@ -19,31 +19,27 @@ def check_auth(view):
             return view(request, *args, **kwargs)
 
         # They are not logged in. See if they provided HTTP BASIC AUTH credentials
-        try:
-            if 'HTTP_AUTHORIZATION' in request.META:
-                auth = request.META['HTTP_AUTHORIZATION'].split()
-                if auth[0].lower() == "basic":
-                    uname, passwd = base64.b64decode(auth[1]).decode('utf-8').split(':', 1)
+        if 'HTTP_AUTHORIZATION' in request.META:
+            auth = request.META['HTTP_AUTHORIZATION'].split()
+            if auth[0].lower() == "basic":
+                uname, passwd = base64.b64decode(auth[1]).decode('utf-8').split(':', 1)
 
-                    #Check if user exists
-                    user = authenticate(username=uname, password=passwd)
+                #Check if user exists
+                user = authenticate(username=uname, password=passwd)
 
-                    #If user exists, and is active, login user and then check that they're authenticated, then return view
-                    if user is not None:
-                        if user.is_active:
-                            login(request, user)
-                            request.user = user
-                            if request.user.is_authenticated:
-                                return view(request, *args, **kwargs)
-            else:
-                response = HttpResponse()
-                response.status_code = 401
-                response['WWW-Authenticate'] = 'Basic'
-                return response
+                #If user exists, and is active, login user and then check that they're authenticated, then return view
+                if user is not None:
+                    if user.is_active:
+                        login(request, user)
+                        request.user = user
+                        if request.user.is_authenticated:
+                            return view(request, *args, **kwargs)
+        else:
+            response = HttpResponse()
+            response.status_code = 401
+            response['WWW-Authenticate'] = 'Basic'
+            return response
 
-        # Could get error if HTTP_AUTHORIZATION not in headers
-        except:
-            pass
 
         # If request does not have active user, or not HTTP Basic Auth, return 403.
         response = HttpResponse()
