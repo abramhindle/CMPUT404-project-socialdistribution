985d2efaf1f35ee989604fd1b1b03535dfe84bbd
Muhammad Umer Khan <um4r12@gmail.com>
2020-03-24 19:23:30 -0600

    modified the model to incorporate some user stories such as authentication, and server permissions

--- socialdistribution/servers/models.py
+++ socialdistribution/servers/models.py
@@ -1,7 +1,26 @@
-from django.db import models
 import uuid
+from django.db import models
+from django.utils.crypto import get_random_string
+
 
 class Server(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    host = models.URLField(max_length=1000, unique=True)
-    # is_active = models.BooleanField(default=True)
+
+    # https://127.0.0.1:8000
+    remote_server_url = models.URLField(max_length=255, unique=True)
+    # https://127.0.0.1:8000/api/
+    remote_server_api_location = models.URLField(max_length=255, unique=True)
+
+    # basic auth username provided by remote server
+    remote_server_user = models.CharField(max_length=255)
+    # basic auth password provided by remote server
+    remote_server_pass = models.CharField(max_length=255)
+
+    # basic auth username provided to remote server, default uuid4 user
+    local_server_user = models.UUIDField(default=uuid.uuid4)
+    # basic auth password provided to remote server, default 16 letter string
+    local_server_pass = models.CharField(default=get_random_string(length=16))
+
+    # permissions for server
+    is_active = models.BooleanField(default=True)
+    share_posts = models.BooleanField(default=True)
+    share_images = models.BooleanField(default=True)
