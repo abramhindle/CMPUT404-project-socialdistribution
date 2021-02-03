4b23efc58a214f785ffd2ffd96fcac601e8ca08c
Ryan Kang <rkang2009@gmail.com>
2021-02-03 16:57:12 -0700

    removed secret key

--- manager/manager/settings.py
+++ manager/manager/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cxs73xfnbs-t(of3$v71(g9=3hi5d(%$mvf9+f0s1uukh9x0ru'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -80,8 +80,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'KonnectDB',
-        'USER': 'ryankang',
-        'PASSWORD': 'yuanrui',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
