a895bb5d8c880f91138267bbed1338d9db4c5ac2
Ryan Kang <rkang2009@gmail.com>
2021-02-03 14:19:39 -0700

    basic frontend wired up to urls. Imported bootstrap

--- manager/manager/urls.py
+++ manager/manager/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('frontend.urls')),
     path('', include('backend.urls')),
     path('admin/', admin.site.urls),
 ]
