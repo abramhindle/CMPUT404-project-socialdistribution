97782991ef1b02faf2fe28a62baf94121d166b00
Ryan Kang <rkang2009@gmail.com>
2021-02-03 13:55:41 -0700

    added basic CRUD REST api to django

--- manager/backend/models.py
+++ manager/backend/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class Teacher(models.Model):
-    name = models.CharField(max_length=80)
-    age = models.IntegerField()
+
+# THIS IS JUST FOR TESTING PURPOSES
+class Lead(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+    message = models.CharField(max_length=500, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
