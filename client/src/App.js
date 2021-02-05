f6d1830ee220d7aec8ced3607e0acd7b7f236982
hz6 <hz6@ualberta.ca>
2021-02-05 12:01:36 +0800

    frontend init

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Social Distribution</h1>
+    </div>
+  );
+}
+
+export default App;
