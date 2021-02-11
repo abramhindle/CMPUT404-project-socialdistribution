246d50722d2f58486dd19ef34cb3bc9370872c27
hz6 <hz6@ualberta.ca>
2021-02-11 14:37:04 +0800

    re-style navbar using css

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,24 +1,17 @@
 import React, { Component } from 'react';
+import '../styles/navbar.css'
 
 class Navbar extends Component {
   render() {
     return (
 
-      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">Social Distribution</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav">
-              <a className="nav-link" href="/">Login</a>
-              <a className="nav-link" href="/">Register</a>
-            </div>
-          </div>
-        </div>
+      <nav id="navbar">
+        <h1> <a href="/">Social Distribution</a> </h1>
+        <ul>
+          <li > <a href="/">Sign up</a> </li>
+          <li > <a href="/">Log in</a> </li>
+        </ul>
       </nav>
-
     )
   }
 }
