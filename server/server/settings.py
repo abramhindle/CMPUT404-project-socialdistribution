46d8fbeac7eff26630920c2f3c73073ffdf0770e
Lida Ling <lida1@ualberta.ca>
2021-02-11 23:04:27 -0700

    change to postgresql

--- server/server/settings.py
+++ server/server/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'socialdistribution',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
