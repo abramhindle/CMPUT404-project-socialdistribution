34da97f016977d98c3d894de10bc319f5c6e6de3
Lida Ling <lida1@ualberta.ca>
2021-02-17 22:31:19 -0700

    create post

--- server/socialdistribution/api_views/author_view.py
+++ server/socialdistribution/api_views/author_view.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login, logout
 from rest_framework.decorators import api_view
 from rest_framework import status
 from socialdistribution.models import Author
@@ -11,20 +11,18 @@ from socialdistribution.serializers import RegistrationSerializer, AuthorSeriali
 @api_view(['POST'])
 def register(request):
     serializer = RegistrationSerializer(data=request.data)
-    data = {}
     if serializer.is_valid(): # make sure data match the model
         author = serializer.save()
-        data['authorID'] = author.authorID
-        return JsonResponse(data, status=status.HTTP_201_CREATED)
+        return JsonResponse({'authorID':author.authorID}, status=status.HTTP_201_CREATED)
     else:
-        return JsonResponse(data, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+        return JsonResponse({'message':serializer.errors}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
 
 @api_view(['GET', 'POST'])
 def author_detail(request, authorID):
     if request.method == "GET":
+        # get author data
         author = get_object_or_404(Author, authorID=authorID)
-
         serializer = AuthorSerializer(author)
         return JsonResponse(serializer.data, safe=False)
 
@@ -39,11 +37,7 @@ def author_detail(request, authorID):
 @api_view(['POST'])
 def login_view(request):
     user = authenticate(email=request.data['email'].lower(), password=request.data['password'])
-    data = {}
     if user is not None:
-        data['authorID'] = user.authorID
-        return JsonResponse(data, status=status.HTTP_200_OK)
+        return JsonResponse({'authorID':user.authorID}, status=status.HTTP_200_OK)
     else:
-        return JsonResponse(data, status=status.HTTP_401_UNAUTHORIZED)
-
-
+        return JsonResponse({'message':"incorrect email or password"}, status=status.HTTP_401_UNAUTHORIZED)
---
+++ server/socialdistribution/api_views/post_view.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from django.contrib.auth import authenticate
+from rest_framework.decorators import api_view
+from rest_framework import status
+from socialdistribution.models import Post, Author
+from socialdistribution.serializers import PostSerializer, AuthorSerializer
+
+@api_view(['GET', 'POST'])
+def post_view(request, authorID):
+    if request.method == "GET":
+        # get recent posts of author (paginated)
+        pass
+    elif request.method == "POST":
+        # create a new post
+        data = request.data
+        data['authorID'] = authorID
+        serializer = PostSerializer(data=data)
+        if serializer.is_valid():
+            post = serializer.save()
+            return JsonResponse({"postID":post.id}, status=status.HTTP_201_CREATED)
+        return JsonResponse({'message':serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'POST', 'DELETE', 'PUT'])
+def post_detail_view(request, authorID, id):
+    if request.method == "GET":
+        # get post data
+        post = get_object_or_404(Post, id=id)
+        post_serializer = PostSerializer(post)
+        # get author data
+        author = get_object_or_404(Author, authorID=authorID)
+        author_serializer = AuthorSerializer(author)
+
+        temp = post_serializer.data
+        del temp['authorID']
+        temp['author'] = author_serializer.data # add author data to post data
+        return JsonResponse(temp, safe=False)
