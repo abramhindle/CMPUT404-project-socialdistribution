0ca2bf0cfe0b8838100fee6adc87d47d000cafb9
xuechun <noglues666@gmail.com>
2021-02-20 00:48:07 -0500

    update model

--- backend/presentation/models.py
+++ backend/presentation/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import uuid
 
 VISIBILILTY_CHOICES = [
     ('PUBLIC', 'PUBLIC'),
@@ -7,45 +8,46 @@ VISIBILILTY_CHOICES = [
 
 
 class Author(models.Model):
-    type = "author"
-    authorID = models.URLField(max_length=200)
-    host = models.URLField(max_length=200)  # the home host of the author
-    # the display name of the author
+    _type = "author"
+    id = models.URLField(primary_key=True, max_length=200)
+    host = models.URLField(max_length=200)
     displayName = models.CharField(max_length=50)
     url = models.URLField(max_length=200)  # url to the authors profile
     github = models.URLField(max_length=200)  # HATEOS url for Github API
 
 
-class Follower(models.Model):
-    type = "followers"
-    list = models.ManyToManyField(
-        Author,
-        blank=True,
-        related_name='follower_list')  # contain Author Objects
+class Follow(models.Model):
+    sender = models.URLField(max_length=200)
+    receiver = models.URLField(max_length=200)
+    status = models.BooleanField()
 
 
 class Comment(models.Model):
-    type = "comment"
-    author = models.ForeignKey(
-        "Author", on_delete=models.CASCADE, related_name="comment_author")
+    _type = "comment"
+    author = models.URLField(max_length=200)
+    post = models.URLField(max_length=200)
     comment = models.TextField()
     contentType = models.CharField(max_length=50)
     published = models.DateField(auto_now=False, auto_now_add=False)
-    commentID = models.URLField(max_length=200)
+    id = models.CharField(primary_key=True, max_length=200, unique=True)
+
+    def save(self, *args, **kwargs):
+        cuuid = str(uuid.uuid4().hex)
+        self.id = f"{self.post}/comments/{cuuid}"
+        super().save(*args, **kwargs)
 
 
 class Post(models.Model):
-    type = "post"
+    _type = "post"
     title = models.CharField(max_length=50)
-    postID = models.URLField(max_length=200)
+    id = models.CharField(primary_key=True, max_length=200, unique=True)
     source = models.URLField(max_length=200)
     origin = models.URLField(max_length=200)
-    description = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
     contentType = models.CharField(max_length=50)
-    content = models.TextField()
-    author = models.ForeignKey(
-        Author, on_delete=models.CASCADE, related_name="post_author")
-    categories = models.TextField()  # a list of string
+    content = models.TextField(blank=True)
+    author = models.URLField(max_length=200)
+    categories = models.CharField(max_length=200)  # a list of string
     count = models.IntegerField()
     size = models.IntegerField()
     comment = models.URLField(max_length=200)
@@ -59,18 +61,24 @@ class Post(models.Model):
     # unlisted means it is public if you know the post name -- use this for images, it's so images don't show up in timelines
     unlisted = models.BooleanField()
 
+    # https://stackoverflow.com/questions/62588857/how-can-i-create-custom-id-in-django/62588993#62588993
+    def save(self, *args, **kwargs):
+        puuid = str(uuid.uuid4().hex)
+        self.id = f"{self.author}/posts/{puuid}"
+        super().save(*args, **kwargs)
+
 
 class Request(models.Model):
-    type = "Follow"
+    _type = "Follow"
     summary = models.CharField(max_length=50)
-    actor = models.ForeignKey(
-        "Author", on_delete=models.CASCADE, related_name="request_author")
-    object = models.ForeignKey(
-        "Author", on_delete=models.CASCADE, related_name="request_object")
+    # send request
+    actor = models.URLField(max_length=200)
+    # recieve request
+    object = models.URLField(max_length=200)
 
 
 class Inbox(models.Model):
-    type = "inbox"
+    _type = "inbox"
     author = models.URLField(max_length=200)
     items = models.ManyToManyField(
         Post,
@@ -78,16 +86,15 @@ class Inbox(models.Model):
 
 
 class Likes(models.Model):
-    type = "Like"
+    _type = "Like"
     context = models.URLField(max_length=200)  # @context?
     summary = models.CharField(max_length=50)
-    author = models.ForeignKey(
-        "Author", on_delete=models.CASCADE, related_name="likes_author")
+    author = models.URLField(max_length=200)
     object = models.URLField(max_length=200)
 
 
 class Liked(models.Model):
-    type = "liked"
+    _type = "liked"
     items = models.ManyToManyField(
         Likes,
         blank=True, related_name="liked_items")  # contain Likes Objects
