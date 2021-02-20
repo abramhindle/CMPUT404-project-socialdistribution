de4bde513f0fdcecf22fc2883d2fe13bafca2679
yanshan24 <yanshan@ualberta.ca>
2021-02-20 13:57:37 +0800

    merge, add friend view file

--- server/socialdistribution/models.py
+++ server/socialdistribution/models.py
@@ -1,57 +1,3 @@
-<<<<<<< HEAD
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.conf import settings
-from friendship.models import Friend, Follow, FriendshipRequest
-import uuid
-
-
-def uuid_hex():
-    return uuid.uuid4().hex
-
-class Author(AbstractUser):
-    # model for author
-    email = models.EmailField(max_length=60, unique=True)
-    username = models.CharField(max_length=30)
-    is_admin = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-
-    authorID = models.CharField(unique=True, default=uuid_hex, editable=False, max_length=40)
-    github = models.URLField(max_length=200, blank=True)
-
-    USERNAME_FIELD = 'email' # use email to login
-    REQUIRED_FIELDS = ['username']
-
-    def get_id(self):
-        return settings.LOCAL_HOST_URL + "author/" + self.authorID
-
-    def get_host(self):
-        return settings.LOCAL_HOST_URL
-
-
-class Post(models.Model):
-    title = models.CharField(max_length=200)
-    postID = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True)
-    source = models.URLField(max_length=200)
-    origin = models.URLField(max_length=200)
-    description = models.TextField()
-    contentType = models.CharField(max_length=20, default="text/plain")
-    content = models.TextField()
-    authorID = models.CharField(max_length=40)
-    # categories
-    count = models.IntegerField(default=0)
-
-    # comments dict
-    published = models.DateTimeField(auto_now_add=True)
-    visibility = models.CharField(max_length=10, default="PUBLIC")
-    unlisted = models.BooleanField(default=False)
-
-    def get_post_id(self):
-        return "{}author/{}/posts/{}".format(settings.LOCAL_HOST_URL, self.authorID, str(self.postID))
-
-    def get_comments_url(self):
-        return self.get_post_id() + "/comments"
-=======
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
@@ -88,7 +34,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     postID = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True)
     source = models.URLField(max_length=200)
-    origin = models.URLField(max_length=200)  
+    origin = models.URLField(max_length=200)
     description = models.TextField()
     contentType = models.CharField(max_length=20, default="text/plain")
     content = models.TextField()
@@ -109,4 +55,3 @@ class Post(models.Model):
 
     def get_type(self):
         return "post"
->>>>>>> master
--- server/socialdistribution/serializers.py
+++ server/socialdistribution/serializers.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from rest_framework import serializers
 from .models import *
 
@@ -7,89 +6,6 @@ class RegistrationSerializer(serializers.ModelSerializer):
         model = Author
         fields = ['authorID', 'email', 'username', 'password', 'github']
 
-    def save(self):
-        author = Author(
-            email=self.validated_data['email'],
-            username=self.validated_data['username'],
-            github=self.validated_data['github']
-        )
-        password = self.validated_data['password']
-        author.set_password(password)
-        author.save()
-        return author
-
-
-class AuthorSerializer(serializers.ModelSerializer):
-    id = serializers.CharField(source='get_id')
-    host = serializers.URLField(source='get_host')
-    displayName = serializers.CharField(source='username')
-    url = serializers.CharField(source='get_id')
-
-    class Meta:
-        model = Author
-        fields = ['id', 'host', 'displayName', 'url', 'github']
-
-
-class PostSerializer(serializers.ModelSerializer):
-    id = serializers.CharField(source='get_post_id', required=False)
-    comments = serializers.URLField(source='get_comments_url', required=False)
-
-    def to_representation(self, instance):
-        response = super(PostSerializer, self).to_representation(instance)
-        author = Author.objects.get(authorID=instance.authorID)
-        author_serializer = AuthorSerializer(author)
-        del response['authorID']
-        del response['postID']
-        response['author'] = author_serializer.data # add author data
-
-        return response
-
-    class Meta:
-        model = Post
-        fields = ['title', 'id', 'authorID', 'postID', 'source', 'origin', 'description', 'contentType',
-            'content', 'count', 'comments', 'published', 'visibility', 'unlisted']
-
-
-class FriendSerializer(serializers.ModelSerializer):
-    # the follower
-    id = serializers.CharField(source='get_id')
-
-    class Meta:
-        model = Friend
-        fields = ['type', 'id', 'url', 'host', 'displayName', 'github']
-
-class FriendRequestSerializer(serializers.ModelSerializer):
-    summary = serializers.CharField(max_length=20)
-    actor = AuthorSerializer()
-    object = FriendSerializer()
-    """ terminology: Greg wants to follow Lara
-    Greg is the actor
-    Lara is the object """
-
-    class Meta:
-        model = FriendRequest
-        fields = ['type', 'summary', 'actor', 'object']
-
-    def sendRequest(self, instance):
-        sender = self.validated_data.get('sender')
-        friend_serializer = FriendSerializer(data=sender)
-        friend_serializer.is_valid()
-        friend_serializer.save()
-        actor = Friend.objects.get(id=requestor_data.get('id'))
-
-        receiver = self.validated_data.get('receiver')
-        object = get_object_or_404(Profile, id=friend_data.get('id'))
-        if object not in Follow.objects.following(instance.authorID):
-            Follow.objects.add_follower(instance.authorID, object)
-=======
-from rest_framework import serializers
-from .models import *
-
-class RegistrationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Author
-        fields = ['authorID', 'email', 'username', 'password', 'github']
-    
     def save(self):
         author = Author(
             email=self.validated_data['email'],
@@ -126,12 +42,10 @@ class PostSerializer(serializers.ModelSerializer):
         del response['authorID']
         del response['postID']
         response['author'] = author_serializer.data # add author data
-        
+
         return response
 
     class Meta:
         model = Post
-        fields = ['type', 'title', 'id', 'authorID', 'postID', 'source', 'origin', 'description', 'contentType', 
+        fields = ['type', 'title', 'id', 'authorID', 'postID', 'source', 'origin', 'description', 'contentType',
             'content', 'count', 'comments', 'published', 'visibility', 'unlisted']
-
->>>>>>> master
