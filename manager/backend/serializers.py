dd75b700cf240458b066e7fd6bf8adc9e267aaf0
timvm1108 <timvanmaren@hotmail.com>
2021-02-26 15:41:43 -0800

    Added Post API

--- manager/backend/serializers.py
+++ manager/backend/serializers.py
@@ -1,14 +1,16 @@
 from rest_framework import serializers
-from backend.models import Author
+from .models import Author, Post
 
 # Author Serializer
+
+
 class AuthorSerializer(serializers.ModelSerializer):
 
     type = serializers.SerializerMethodField('get_type')
     id = serializers.SerializerMethodField('set_id')
 
     def get_type(self, Author):
-      return "author" 
+    	return "author"
 
     def set_id(self, Author):
         return str(Author.host)+"/author/"+str(Author.id)+'/'
@@ -16,3 +18,10 @@ class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
         fields = ('type', 'id', 'host', 'displayName', 'url', 'github')
+
+class PostSerializer(serializers.ModelSerializer):
+
+	class Meta:
+		model = Post
+		fields = "__all__"
+
