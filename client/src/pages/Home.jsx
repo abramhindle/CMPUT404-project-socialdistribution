08221832053e0f21203989afcd0301f3c337c6ea
hz6 <hz6@ualberta.ca>
2021-02-27 20:34:23 +0800

    Post Form is up in /client/src/components/posts/PostForm.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,8 @@ import NoUserHeader from "../components/headers/NoUserHeader";
 import UserHeader from "../components/headers/UserHeader";
 import { connect } from "react-redux";
 import axios from "axios";
-// import ReactMarkdown from "react-markdown";
+// import PostForm from "../components/posts/PostForm";
+// import ReactMarkDown from "react-markdown";
 
 class Home extends Component {
 
@@ -38,8 +39,9 @@ class Home extends Component {
       <div>
         {this.renderHeader()}
         <h1 id="home-title" style={{ textAlign: "center", fontFamily: "sans-serif", padding: 15 }}>Home</h1>
-        {/* <ReactMarkdown children="*hello*" /> */}
-        {/* <ReactMarkdown># Hello, *world*!</ReactMarkdown> */}
+        {/* <PostForm /> */}
+        {/* <ReactMarkDown children="*hello*" /> */}
+        {/* <ReactMarkDown># Hello, *world*!</ReactMarkdown> */}
       </div>
     )
   }
