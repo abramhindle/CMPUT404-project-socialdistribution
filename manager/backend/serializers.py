9cd2a249d45760e6279bdd6f596445675b4369d2
Ryan Kang <rkang2009@gmail.com>
2021-02-27 10:48:37 -0700

    fixed bug where login requires an email :

--- manager/backend/serializers.py
+++ manager/backend/serializers.py
@@ -22,17 +22,17 @@ class AuthorSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
   class Meta:
     model = User
-    fields = ('id', 'username', 'email')
+    fields = ('id', 'username')
 
 # Register Serializer
 class RegisterSerializer(serializers.ModelSerializer):
   class Meta:
     model = User
-    fields = ('id', 'username', 'email', 'password')
+    fields = ('id', 'username', 'password')
     extra_kwargs = {'password': {'write_only': True}}
 
   def create(self, validated_data):
-    user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
+    user = User.objects.create_user(username=validated_data['username'], password=validated_data['password'])
 
     return user
 
