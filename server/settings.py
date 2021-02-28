8a3731df2317fa753344edef86ee3e51291205c0
Lida Ling <lida1@ualberta.ca>
2021-02-28 16:45:02 -0700

    initial

--- server/settings.py
+++ server/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'cmput-404-socialdistribution.herokuapp.com']
 
 
 # Application definition
