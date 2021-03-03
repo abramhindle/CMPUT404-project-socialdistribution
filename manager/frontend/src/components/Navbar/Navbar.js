15af4bd8974ac27d6ab7d39febd61f2557850695
Ryan Kang <rkang2009@gmail.com>
2021-03-03 15:49:07 -0700

    hyperlinked icon to profile page

--- manager/frontend/src/components/Navbar/Navbar.js
+++ manager/frontend/src/components/Navbar/Navbar.js
@@ -1,19 +1,44 @@
-import React from 'react'
+import React from 'react';
+import { useHistory } from "react-router-dom";
+
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles(() => ({
-  root: {
-    flexGrow: 1,
-    backgroundColor: "#FFF",
-    marginBottom: "30px"
-  },
+    root: {
+        flexGrow: 1,
+        backgroundColor: "#FFF",
+        marginBottom: "30px"
+    },
+    container: {
+        display: 'flex'
+    },
+    logo: {
+        height: '45px',
+        width: '45px',
+        backgroundColor: '#D1305E',
+        borderRadius: '10px',
+        marginLeft: '10%'
+    },
+    icon: {
+        height: '45px',
+        width: '45px',
+        backgroundColor: '#D1305E',
+        borderRadius: '100px',
+        marginLeft: '10%',
+        left: '70%'
+    },
 }));
 
 export default function Navbar() {
     const classes = useStyles();
+    const history = useHistory();
+    
+    const onIconClick = (e) => {
+        history.push("/profile");
+    };
 
     return (
         <AppBar
@@ -22,9 +47,19 @@ export default function Navbar() {
             elevation={0}
         >
             <Toolbar >
-                <Typography variant="h6" color="textPrimary">
-                    Konnect
-                </Typography>
+                <div className='container-fluid'>
+                    <div className='row align-items-start'>
+                        <div className={'col-8'}>
+                            <div className={classes.logo}></div>
+                        </div>
+                        <div className={'col-4'}>
+                            <div
+                                className={classes.icon}
+                                onClick={onIconClick}
+                            ></div>
+                        </div>
+                    </div>
+                </div>
             </Toolbar>
         </AppBar>
     )
