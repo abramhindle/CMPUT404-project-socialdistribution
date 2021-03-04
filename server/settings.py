4c81bc5926a33d95652675448b52568e7fd7485f
Lida Ling <lida1@ualberta.ca>
2021-03-03 22:04:08 -0700

    add settings

--- server/settings.py
+++ server/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'server.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -137,6 +137,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'build/static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 AUTH_USER_MODEL = 'socialdistribution.Author'
 
 # Variables
--- server/urls.py
+++ server/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from d
 
 urlpatterns = [
 	path('', include('socialdistribution.urls')),
     path('admin/', admin.site.urls),
+    re_path('.*', TemplateView.as_view(template_name='index.html')),
 ]
