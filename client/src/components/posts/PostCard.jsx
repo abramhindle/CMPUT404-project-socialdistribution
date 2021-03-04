e35e8e132304bbd1b4aa148ecbbc4df414dfcfc2
yixianLu <43053645+yixianLu@users.noreply.github.com>
2021-03-04 17:07:54 +0800

    Update PostCard.jsx

--- client/src/components/posts/PostCard.jsx
+++ client/src/components/posts/PostCard.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
+import axios from 'axios';
 // import ReactMarkDown from "react-markdown";
 
 // This component is used to display the Post
@@ -12,6 +14,34 @@ class PostCard extends Component {
     content: "",
     visibility: "PUBLIC",
     unlisted: false,
+    like_button_text:"click to like the post"
+  }
+  likepostClick= async()=>{
+    var author_url = this.props.post.author.id
+    var author_data = author_url.split("/")
+    var post_author_id = author_data[4]
+    var login_author_id = this.props.authorID.authorID
+    var post_id = this.props.post.postID
+
+    var post_information = {
+      "summary": "post",
+      "type":"like",
+      "author_like_ID":login_author_id,
+      "postID":post_id
+    }
+    try{
+      let doc = await axios.post(`service/author/${post_author_id}/inbox/`,post_information)
+      if(doc.status == 200){
+        console.log(doc)
+        this.setState({like_button_text :"you have liked!"});
+
+      }
+
+    }catch (err) {
+      console.log(err.response.status)
+    }
+
+    
   }
 
   render() {
@@ -20,9 +50,15 @@ class PostCard extends Component {
         <h1>Title: {this.props.post.title}</h1>
         <h2>Description: {this.props.post.description}</h2>
         <p>Content: {this.props.post.content}</p>
+         <button onClick = {this.likepostClick}>{this.state.like_button_text}</button> 
       </div>
     )
   }
 }
 
-export default PostCard;
+const mapStateToProps = (state) => ({
+  authorID: state.user.authorID
+})
+
+
+export default connect(mapStateToProps)(PostCard);
