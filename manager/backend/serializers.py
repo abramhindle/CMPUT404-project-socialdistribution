55f90a61fa446c8fad7d613fd5cb240868d7bde9
Tim van Maren <timvanmaren@hotmail.com>
2021-03-05 15:51:08 -0700

    Started Api Tests

--- manager/backend/serializers.py
+++ manager/backend/serializers.py
@@ -58,35 +58,6 @@ class AuthorSerializer(serializers.ModelSerializer):
 		fields = ('type', 'id', 'host', 'displayName', 'url', 'github')
 
 
-
-# Post Serializer
-class PostSerializer(serializers.ModelSerializer):
-	"""
-	This class serializes Post data to output all of the field names for the Post object.
-	"""
-	author = AuthorSerializer()
-	type = serializers.SerializerMethodField('get_type')
-	id = serializers.SerializerMethodField('get_id')
-
-	def get_type(self, Post):
-		"""
-		The get_type method is run every time serialization occurs and returns the appropriate string for the JSON 'type' field.
-		"""
-		return "post"
-
-	def get_id(self, Post):
-		"""
-		The get_id method is run every time serialization occurs and returns the 'id' field as the proper url format. This is because ids are stored as just the uuid vlaue in the DB,
-		but the API requires the uuid be returned as a url
-		"""
-		return "http://" + str(Post.host) + "/author/" + str(Post.author_id) + "/posts/" + str(Post.id)
-
-	class Meta:
-		model = Post
-		fields = ('type', 'title', 'id', 'source', 'origin', 'description', 'content_type', 'content', 'categories', 'count', 'published', 'visibility', 'unlisted', 'author')
-		depth = 1
-
-
 class CommentSerializer(serializers.ModelSerializer):
 	"""
 	This class serializes the Comment data to ouput the neccessary fields from the table.
@@ -111,6 +82,38 @@ class CommentSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Comment
 		fields = ('type', 'author', 'comment', 'contentType', 'published', 'id')
+		depth = 1
+
+# Post Serializer
+class PostSerializer(serializers.ModelSerializer):
+	"""
+	This class serializes Post data to output all of the field names for the Post object.
+	"""
+	author = AuthorSerializer()
+	type = serializers.SerializerMethodField('get_type')
+	id = serializers.SerializerMethodField('get_id')
+	comments = CommentSerializer(many=True)
+	commentLink = serializers.SerializerMethodField('get_comment_link')
+
+	def get_type(self, Post):
+		"""
+		The get_type method is run every time serialization occurs and returns the appropriate string for the JSON 'type' field.
+		"""
+		return "post"
+
+	def get_id(self, Post):
+		"""
+		The get_id method is run every time serialization occurs and returns the 'id' field as the proper url format. This is because ids are stored as just the uuid vlaue in the DB,
+		but the API requires the uuid be returned as a url
+		"""
+		return "http://" + str(Post.host) + "/author/" + str(Post.author_id) + "/posts/" + str(Post.id)
+
+	def get_comment_link(self, Post):
+		return  "http://" + str(Post.host) + "/author/" + str(Post.author_id) + "/posts/" + str(Post.id) + "/comments/"
+
+	class Meta:
+		model = Post
+		fields = ('type', 'title', 'id', 'source', 'origin', 'description', 'content_type', 'content', 'categories', 'count', 'commentLink', 'comments', 'published', 'visibility', 'unlisted', 'author')
 		depth = 1
 
 
--- manager/backend/tests.py
+++ manager/backend/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+import rest_framework
+from rest_framework.test import APIClient, APITestCase
+from rest_framework import status
+from .models import *
+from django.urls import reverse
+import uuid
 
 # Create your tests here.
+# {"type":"author","id":"http://localhost:8000/author/f19f85f5605f4aed86376ced9fede5d9","host":"localhost:8000","displayName":"Tom van Maren","url":"http://localhost:8000/author/f19f85f5605f4aed86376ced9fede5d9","github":"https://github.com/"}
+class AuthorViewSetTestCase(APITestCase):
+
+	def setUp(self):
+			self.client = Client(HTTP_HOST='127.0.0.1:8000', ALLOWED_HOSTS='127.0.0.1')
+			response = self.client.post('/api/auth/register', {"displayName":"Test User","github":"https://github.com/", "password": "test1234", "username": "testuser"}, format='json')
+			print(response.context)
+			self.author_id = response.context["id"].split("/")[-1]
+			return super().setUp()
+
+	def test_get_author(self):
+		response = self.client.get('author/' + self.author_id)
+		print(response)
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
+		self.assertEqual(response.content["displayName"], "Test User")
+		#CREATED_AUTHOR = 
+		#self.assertEqual(Author.objects.filter(token=response.id.split("/")[-1]).get().displayName, "Test User")
+
+		
+
+#class PostTest(APITestCase):
+#    	def 
