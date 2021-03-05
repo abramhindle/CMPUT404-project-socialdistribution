bdb46f9378db44b0edeab6170cd521a1fe02d69f
Ryan Kang <rkang2009@gmail.com>
2021-03-05 16:09:13 -0700

    removed some variables

--- manager/manager/settings.py
+++ manager/manager/settings.py
@@ -139,13 +139,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 import django_heroku
 django_heroku.settings(locals())
-
-STATIC_URL = '/static/'
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT='staticfiles'
-STATICFILES_DIRS = []
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'build', 'media')
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
