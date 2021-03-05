22c06b39fe187b8e547f3a3a56ec947766ebe022
Jawad Rizvi <jawadrizvi40@gmail.com>
2021-03-05 12:04:51 -0700

    Styling Post

--- manager/frontend/src/components/Posts/Post/Post.js
+++ manager/frontend/src/components/Posts/Post/Post.js
@@ -7,9 +7,45 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(() => ({
     root: {
-        minHeight: '200px',
+        height: '300px',
         backgroundColor: 'white',
-        marginBottom: '40px'
+        marginBottom: '40px',
+        borderRadius: "8px",
+    },
+    postCard: {
+        // height: '300px',
+        // marginBottom: '40px',
+        padding: "1em",
+        // backgroundColor: 'white',
+        overflow: "hidden",
+    },
+    postHead: {
+        height: "50px",
+        margin: "1em",
+    },
+    displayName: {
+        fontWeight: 'bold',
+        fontSize: '1.15em',
+        margin: '0em 1em'
+    },
+    postBody: {
+        height: "11.5em",
+        margin: "1em",
+        overflow: "hidden",
+    },
+    divider: {
+        margin: '1em 1em',
+        opacity: '0.2'
+    },
+    title: {
+        // fontSize: '1.25em',
+        // margin: '0em 1em'
+    },
+    textField: {
+        // fontSize: '1.15em',
+        // margin: '0em 1em',
+        overflow: "hidden",
+        lineHeight: "1.5em"
     },
   }));  
 
@@ -29,15 +65,40 @@ export default function Post(props) {
 
     return (
         <div className={classes.root}>
-            <p>Post by: {postData.author.displayName}</p>
-            <p>Title: {postData.title}</p>
-            <p>Content: {postData.content}</p>
-
-            <a 
-                onClick={() => handleSeeFullPost(postData.id)}
+            <div 
+                className={classes.postCard}
             >
-                {`Comments (${postData.count})`}
-            </a>
+                <p 
+                    className={classes.title}
+                >
+                    Post by: {postData.author.displayName}
+                </p>
+                <div 
+                    className={classes.postBody}
+                >
+                    <p
+                        className={classes.title}
+                    >
+                        Title: {postData.title}
+                    </p>
+                    <p
+                        className={classes.textField}
+                    >
+                        Content: {postData.content}
+                    </p>
+
+                    <hr className={classes.divider}></hr>
+                    
+                    <a 
+                        className={classes.title}
+                        onClick={() => handleSeeFullPost(postData.id)}
+                    >
+                        {`Comments (${postData.count})`}
+                    </a>
+                </div>
+                
+            </div>
+            
         </div>
     )
 }
