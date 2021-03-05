0b96d172ddc0222d0e166d01e3456919878f4263
Jawad Rizvi <jawadrizvi40@gmail.com>
2021-03-05 15:56:39 -0700

    Reeee I forgot to make it a string

--- manager/manager/settings.py
+++ manager/manager/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(KONNECT_SECRET_KEY, True)
+SECRET_KEY = os.environ.get('KONNECT_SECRET_KEY', True)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
