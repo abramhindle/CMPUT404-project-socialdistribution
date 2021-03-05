d7909de426d6f39c5b73ff20649bbeac07b62a11
xuechun <noglues666@gmail.com>
2021-03-05 09:28:56 -0500

    update friend component

--- frontend/src/components/Friends/index.js
+++ frontend/src/components/Friends/index.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { Button, Modal, List, Avatar } from "antd";
+import { Button, List, Avatar, message } from "antd";
 import { UserOutlined, UserSwitchOutlined } from "@ant-design/icons";
+import { getFriendList } from "../../requests/requestFriends";
+import { domain, port } from "../../requests/URL";
 
 export default class Friends extends React.Component {
   constructor(props) {
@@ -10,7 +12,36 @@ export default class Friends extends React.Component {
       authorID: this.props.authorID,
       friends: [],
     };
+    console.log("friends", this.props.authorID);
   }
+
+  componentDidMount() {
+    if (this.state.authorID === undefined || this.state.authorID === "") {
+      // get author
+      fetch(`${domain}:${port}/user-author/`, {
+        headers: {
+          Authorization: `JWT ${localStorage.getItem("token")}`,
+        },
+      })
+        .then((res) => res.json())
+        .then((json) => {
+          this.setState({
+            authorID: json.id,
+          });
+        });
+    } else {
+      console.log("friends2", this.state.authorID);
+      getFriendList({ authorID: this.state.authorID }).then((res) => {
+        if (res.status === 200) {
+          this.setState({ friends: res.data });
+        } else {
+          message.error("Fail to load friend list.");
+        }
+      });
+    }
+  }
+
+  clickFollowBtn = () => {};
 
   render() {
     return (
@@ -23,7 +54,10 @@ export default class Friends extends React.Component {
             <List.Item>
               <Avatar icon={<UserOutlined />} />
               <p>{item.comment}</p>
-              <Button icon={<UserSwitchOutlined />}></Button>
+              <Button
+                icon={<UserSwitchOutlined />}
+                onClick={this.clickFollowBtn}
+              ></Button>
             </List.Item>
           )}
         />
