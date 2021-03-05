cf85b7cc0a1f10ed804044774eddad8ba73d6a2a
Ryan Kang <rkang2009@gmail.com>
2021-03-05 14:26:45 -0700

    make it better

--- manager/manager/settings.py
+++ manager/manager/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = KONNECT_SECRET_KEY
+SECRET_KEY = 'cxs73xfnbs-t(of3$v71(g9=3hi5d(%$mvf9+f0s1uukh9x0ru'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
