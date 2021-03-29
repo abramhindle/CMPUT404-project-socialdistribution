a97ee6356dafb4c402e97bfd68956ddec1a7c358
7bw <boylee99@hotmail.com>
2021-03-28 18:23:25 -0600

    search for users

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { Layout, Avatar, Menu } from "antd";
+import { Layout, Avatar, Menu, Select, Drawer, Button, message } from "antd";
 import {
   BookOutlined,
   UserOutlined,
   TeamOutlined,
   HomeOutlined,
   MailOutlined,
+  UserAddOutlined,
 } from "@ant-design/icons";
 import "./App.css";
 import PublicAndMyPost from "./components/PublicAndMyPost";
@@ -15,8 +16,11 @@ import Inbox from "./components/Inbox";
 import Friends from "./components/Friends";
 import Profile from "./components/Profile";
 import LoginComp from "./components/LoginComp";
+import { getAllAuthors } from "./requests/requestAuthor";
+import { postRequest } from "./requests/requestFriendRequest";
 
 const { Header, Content, Footer } = Layout;
+const { Option } = Select;
 
 export default class App extends React.Component {
   constructor(props) {
@@ -27,6 +31,11 @@ export default class App extends React.Component {
       username: localStorage.getItem("username"),
       displayName: localStorage.getItem("displayName"),
       github: localStorage.getItem("github"),
+      authorList: [{authorName: "bowei1"}],
+      objectID: undefined,
+      authorValue: undefined,
+      authorGithub: undefined,
+      drawerVisible: false,
       currentTab: "/",
     };
     this._isMounted = false;
@@ -37,11 +46,26 @@ export default class App extends React.Component {
     if (this._isMounted) {
       this.setState({ currentTab: window.location.pathname });
     }
+    getAllAuthors().then((res) => {
+      if (res.status === 200) {
+        this.getAuthorDataSet(res.data).then((value) => {
+          this.setState({ authorList: value });
+        });
+      } else {
+        message.error("Request failed!");
+      }
+    });
   }
 
   componentWillUnmount() {
     this._isMounted = false;
   }
+
+  hide = () => {
+    this.setState({
+      drawerVisible: false,
+    });
+  };
 
   logout = () => {
     localStorage.removeItem("token");
@@ -57,6 +81,50 @@ export default class App extends React.Component {
     this.setState({ currentTab: e.key });
   };
 
+  onChange = (value) => {
+    var authorInfo = value.split(",");
+    this.setState({
+      authorValue: authorInfo[0],
+      authorGithub: authorInfo[1],
+      objectID: authorInfo[2],
+      drawerVisible: true,
+    });
+  };
+
+  handleClickFollow = async () => {
+    let params = {
+      actor: this.state.authorID,
+      object: this.state.objectID,
+      summary: "I want to follow you!",
+    };
+    postRequest(params).then((response) => {
+      if (response.status === 200) {
+        message.success("Request sent!");
+        window.location.reload();
+      } else if (response.status === 409) {
+        message.error("Invalid request!");
+      } else {
+        message.error("Request failed!");
+      }
+    });
+  };
+
+  getAuthorDataSet = (authorData) => {
+    let promise = new Promise(async (resolve, reject) => {
+      const authorArray = [];
+      for (const author of authorData) {
+        console.log("authorIDs:", author.id);
+        authorArray.push({
+          authorID: author.id,
+          authorName: author.displayName,
+          authorGithub: author.github,
+        });
+      }
+      resolve(authorArray);
+    });
+    return promise;
+  };
+
   render() {
     const {
       loggedIn,
@@ -65,7 +133,11 @@ export default class App extends React.Component {
       displayName,
       github,
       currentTab,
+      authorValue,
+      authorGithub,
     } = this.state;
+
+    const options = this.state.authorList.map(d => <Option key={[d.authorName, d.authorGithub, d.authorID]}>{d.authorName}</Option>);
 
     let content = loggedIn ? (
       <Layout className="layout">
@@ -104,6 +176,18 @@ export default class App extends React.Component {
               </span>
               <Link to="/my-friends" />
             </Menu.Item>
+            <Select
+              showSearch
+              style={{ width: 200 }}
+              placeholder="Search for a user"
+              optionFilterProp="children"
+              onChange={this.onChange}
+              filterOption={(input, option) =>
+                option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+              }
+            >
+              {options}
+            </Select>
             <Menu.Item key="/my-profile" style={{ float: "right" }}>
               <span>
                 <Avatar icon={<UserOutlined />} />
@@ -114,6 +198,18 @@ export default class App extends React.Component {
               <Link to="/my-profile" />
             </Menu.Item>
           </Menu>
+          <Drawer
+            title="User Info"
+            placement="right"
+            closable={false}
+            onClose={this.hide}
+            visible={this.state.drawerVisible}
+          > 
+            <Avatar icon={<UserOutlined />} />
+            <p>{authorValue}</p>
+            <p>{authorGithub}</p>
+            <Button icon={<UserAddOutlined />} onClick={this.handleClickFollow} />
+          </Drawer>
         </Header>
         <Content
           style={{
