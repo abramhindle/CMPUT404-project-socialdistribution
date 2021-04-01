5a2ba0be0f3de05b8461db278167adf91f6d8dd0
7bw <boylee99@hotmail.com>
2021-03-31 20:58:24 -0600

    small fix

--- frontend/src/requests/URL.js
+++ frontend/src/requests/URL.js
@@ -28,7 +28,7 @@ if (_config64) {
   _remoteDomain = "https://social-distribution-t1v2.herokuapp.com";
   _remoteDomain4 = "https://c404posties.herokuapp.com";
   _remoteDomain20 = "";
-  _port = ""; //"8000"; //8000;
+  _port = 8000; //"8000"; //8000;
   _port4 = 443;
   _port20 = "";
   _auth = "Basic UmVtb3RlMTpyZW1vdGUxMjM0";
