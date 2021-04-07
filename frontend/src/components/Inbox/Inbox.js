58557d2ad4a23d95c5db149ad2705da90b353364
Ryan Kang <rkang2009@gmail.com>
2021-04-07 13:42:00 -0600

    sends post to all friends

--- frontend/src/components/Inbox/Inbox.js
+++ frontend/src/components/Inbox/Inbox.js
@@ -10,7 +10,15 @@ export default function Inbox(props) {
             if (d.type === 'Follow') {
                 return <FollowRequest key={i} request={d} author={props.author} postFriendRequest={props.postFriendRequest}/>;
             } else if (d.type === 'post') {
-                return <Post key={i} postData={d} onLikeClicked={props.postLiked} author={props.author} createComment={props.createComment} getLikes={props.getLikes}/>;
+                return <Post
+                            key={i}
+                            postData={d}
+                            onLikeClicked={props.postLiked}
+                            author={props.author}
+                            createComment={props.createComment}
+                            getLikes={props.getLikes}
+                            sharePost={props.sharePost}
+                        />;
             } else if (d.type === 'like') {
                 return <Like key={i} data={d}/>;
             }
