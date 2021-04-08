32c96d7574b1acbd05287fa4f1bcfbed3a46660b
Tim van Maren <timvanmaren@hotmail.com>
2021-04-08 15:18:10 -0600

    Admin and Utils Changes
    Co-Authored-By: Fraser Redford <57697667+fredford@users.noreply.github.com>

--- backend/admin.py
+++ backend/admin.py
@@ -1,29 +1,32 @@
 from django.contrib import admin
+from django.contrib.admin.options import ModelAdmin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 from .models import Author, Comment, Like, Post, Follow, Inbox, Node
 
 class NodeInline(admin.StackedInline):
-    model = Node
-    can_delete = True
-    verbose_name_plural = 'node'
+	model = Node
+	can_delete = True
+	verbose_name_plural = 'node'
 
 class UserAdmin(UserAdmin):
-    inlines = (NodeInline,)
-    list_filter = (
-        ('node', admin.RelatedOnlyFieldListFilter),
-        'is_active',
-        'is_staff',
-        'is_superuser',
-    )
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'node_check')
+	inlines = (NodeInline,)
+	list_filter = (
+		('node', admin.RelatedOnlyFieldListFilter),
+		'is_active',
+		'is_staff',
+		'is_superuser',
+	)
+	list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'node_check')
 
-    def node_check(self, user):
-        if not user.node.remote_username == "":
-            return user.node.host
+	def node_check(self, user):
+		if not user.node.remote_username == "":
+			return user.node.host
 
+	node_check.short_description = 'Node'
 
-    node_check.short_description = 'Node'
+class AuthorAdmin(ModelAdmin):
+	list_display = ('author', 'user')
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
