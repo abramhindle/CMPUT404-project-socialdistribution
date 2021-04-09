6c0d3e0d466db5b1f81ea7e855c8e4f3b2286572
Tim van Maren <timvanmaren@hotmail.com>
2021-04-09 17:45:38 -0600

    Change Structure of Post Object
    Comment link now under comments field
    
    Co-Authored-By: Fraser Redford <57697667+fredford@users.noreply.github.com>

--- backend/apis/commentviewset.py
+++ backend/apis/commentviewset.py
@@ -76,7 +76,10 @@ class CommentViewSet(viewsets.ModelViewSet):
 							s = requests.Session()
 							s.auth = (node.remote_username, node.remote_password)
 							s.headers.update({'Content-Type':'application/json'})
-							response_comment = s.post(node.host+"author/"+author_id+"/posts/"+post_id+"/comments", json=body)
+							url = node.host+"author/"+author_id+"/posts/"+post_id+"/comments"
+							if 'konnection' in node.host:
+								url += '/'
+							response_comment = s.post(url, json=body)
 					else:
 						raise Exception("The comment link of the remote post is not present!")
 				except Exception:
