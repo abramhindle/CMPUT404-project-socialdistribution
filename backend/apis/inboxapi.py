74a156cc4dcc9bed5ba12c729f5aff873e9988cb
Tim van Maren <timvanmaren@hotmail.com>
2021-04-09 23:55:12 -0600

    Revitalized Inbox, Update Comments and utils
    Co-Authored-By: Fraser Redford <57697667+fredford@users.noreply.github.com>

--- backend/apis/inboxapi.py
+++ backend/apis/inboxapi.py
@@ -3,8 +3,10 @@ from manager.paginate import ResultsPagination
 from ..models import Author, Comment, Follow, Inbox, Like, Post, Node
 from ..serializers import CommentSerializer, FollowSerializer, LikeSerializer, PostSerializer
 
+import requests
 from rest_framework import permissions, status, viewsets
 from rest_framework.response import Response
+from .. import utils
 
 import json
 
@@ -77,6 +79,11 @@ class InboxAPI(viewsets.ModelViewSet):
 
 	def create(self, request, author_id=None, *args, **kwargs):
 
+		# Check if the inbox owner is a valid author on our server
+		try:
+			inbox_author = Author.objects.filter(id=author_id).get()
+		except:
+			return Response(data="URL author not found",status=status.HTTP_404_NOT_FOUND)
 		# Check that the author ID is for a valid author
 		try:
 			author = Author.objects.filter(id=author_id).get()
@@ -91,228 +98,115 @@ class InboxAPI(viewsets.ModelViewSet):
 		# If the sent object is of type Follow
 		if body['type'].lower() == 'follow':
 
-			# Retrieve the author ID's for the actor and object
-
-			actor_id = body['actor']['id'].split("/")[-1]
-			object_id = body['object']['id'].split("/")[-1]
-
-			if actor_id == "" or object_id == "":
-				return Response(data="Invalid author ID passed", status=status.HTTP_400_BAD_REQUEST)
+			# Get the actor author information
+			try:
+				actor_author, isLocal = utils.get_author_by_ID(request, body['actor']['id'].split("/")[-1], "actor")
+			except Exception as e:
+				return Response(data=str(e), status=status.HTTP_400_BAD_REQUEST)
+			# Get the object author information
+			try:
+				object_author, isLocal = utils.get_author_by_ID(request, body['object']['id'].split("/")[-1], "object")
+			except Exception as e:
+				return Response(data=str(e), status=status.HTTP_400_BAD_REQUEST)
 
 			# Check if author ID is matching
-			if object_id != author.id:
+			if object_author.id != inbox_author.id:
 				return Response(data="Author doesn't match request followee", status=status.HTTP_400_BAD_REQUEST)
-			# Check if the object exists in the database
-			try:
-				actor = Author.objects.filter(id=actor_id).get()
-			except:
-				actor = Author(
-					id = actor_id,
-					user = request.user,
-					displayName = body["actor"]["displayName"],
-					github = body["actor"]["github"],
-					host = body["actor"]["host"],
-					url = body["actor"]["url"]
-				)
-				actor.save()
 
-			# Check if this follow already exists
+			# Get the follow object and the serialized data
 			try:
-				alreadyFollow = Follow.objects.filter(follower=actor, followee=author).get()
-				return Response(data="Follow already exists"+str(actor_id)+str(object_id), status=status.HTTP_409_CONFLICT)
-			except:
-				pass
+				follow, follow_data, reverse_follow = utils.add_follow(inbox_author, actor_author)
+			except Exception as e:
+				return Response(data=str(e), status=status.HTTP_409_CONFLICT)
 
-			# Check if the authors are already following each other
-			check_follow = Follow.objects.filter(follower=author, followee=actor)
-			# If the actor is already followed by the object
-			if check_follow:
-				follow = Follow(
-					follower=actor,
-					followee=author,
-					friends = True,
-					summary=actor.displayName + " wants to follow " + author.displayName
-				)
-				check_follow.update(friends=True)
-			# If the authors are not currently following each other
-			else:
-				follow = Follow(
-					follower=actor,
-					followee=author,
-					summary=actor.displayName + " wants to follow " + author.displayName
-				)
-			follow.save()
-			serializer = self.get_serializer(follow)
 			# Add this follow to the inbox of the author ID provided
 			inbox = Inbox(
-				author=author,
+				author=inbox_author,
 				follow=follow
 			)
 			inbox.save()
 			# Return the follow object
-			return Response(serializer.data, status=status.HTTP_201_CREATED)
+			return Response(data="Follow sent to inbox", status=status.HTTP_201_CREATED)
 		# If the sent object is of type Like
 		elif body['type'].lower() == 'like':
 
 			# Parse the input object and author ID
 			object_data = body['object'].split('/')
 			like_author_data = body['author']['id'].split('/')
+			# Parse the data and return an author, post and comment object
+			try:
+				object_author, post, comment = utils.get_object_data(request, "object")
+			except Exception as e:
+				return Response(data=str(e), status=status.HTTP_404_NOT_FOUND)
 
-			comment = None
-			post = None
-
-			# Check the length of the object URL, 9 is for Comment likes
-			if len(object_data) == 9 or len(object_data) == 10:
-				try:
-					# Get the comment and post associated
-					comment = Comment.objects.filter(id=object_data[-1]).get()
-					post = Post.objects.filter(id=object_data[-3]).get()
-				except:
-					return Response(data="Comment does not exist", status=status.HTTP_404_NOT_FOUND)
-			# A length of 7 is for Post likes
-			elif len(object_data) == 7 or len(object_data) == 8:
-				try:
-					# Get the post associated
-					post = Post.objects.filter(id=object_data[-1]).get()
-				except:
-					return Response(data="Post does not exist", status=status.HTTP_404_NOT_FOUND)
-			else:
-				return Response(data="Object being liked not found", status=status.HTTP_404_NOT_FOUND)
-
-			# Check if the request URL ID is for the post or comment creator
-			if post.author.id == author_id or comment.author.id == author_id:
-
-				# Retrieve the author of the Like or create an author if they are remote
-				try:
-					like_author = Author.objects.filter(id=like_author_data[-1]).get()
-				except:
-					if like_author_data[-1] != "":
-						like_author = Author(
-							id = like_author_data[-1],
-							user = request.user,
-							displayName = body["author"]["displayName"],
-							github = body["author"]["github"],
-							host = body["author"]["host"],
-							url = body["author"]["url"]
-							)
-						like_author.save()
-					else:
-						return Response(data="Like author ID not valid", status=status.HTTP_400_BAD_REQUEST)
-
-				# If the Like is on a Comment
-				if comment is not None:
-					# Check if the Like already exists in the database for the Comment
-					if not Like.objects.filter(author=like_author, comment=comment):
+			# Check if the post/comment creator is the same as the inbox recipient
+			if object_author.id != inbox_author.id:
+				return Response(data="Creator does not match inbox owner", status=status.HTTP_400_BAD_REQUEST)
+			# Get the like author and if they are local or remote
+			try:
+				like_author, isLocal = utils.get_author_by_id(request, like_author_data[-1], "author")
+			except Exception as e:
+				return Response(data=str(e), status=status.HTTP_404_NOT_FOUND)
+			# If the like is on a Post
+			if comment is None:
+				# Check that the author of the post is the one receiving the like
+				if post.author.id == author_id:
+					# Check if the like already exists
+					if not Like.objects.filter(author=like_author, post=post):
+						# Create the like and send to their inbox
 						try:
-							like = Like(
-								post=post,
-								author=like_author,
-								comment=comment,
-								summary=like_author.displayName+" Likes your comment"
-							)
-							inbox = Inbox(
-								author=comment.author,
-								like=like
-							)
-							like.save()
-							inbox.save()
+							like, like_data = utils.add_like(request, like_author, object_author, post, "post")
 						except Exception as e:
 							return Response(data=str(e), status=status.HTTP_400_BAD_REQUEST)
 					else:
-						return Response(data="Like already exists for this comment", status=status.HTTP_409_CONFLICT)
-
-				# If the Like is on a Post
-				elif post is not None:
-					# Check if the Like already exists in the database for the Post
-					if not Like.objects.filter(author=like_author, post=post):
+						return Response(data="Like already exists for this post", status=status.HTTP_409_CONFLICT)
+			# If the like is on a Comment
+			else:
+				# Check that the author of the comment is the one receiving the like
+				if comment.author.id == author_id:
+					# Check if the like already exists
+					if not Like.objects.filter(author=like_author, comment=comment):
+						# Create the like and send to their inbox
 						try:
-							like = Like(
-								post=post,
-								author=like_author,
-								summary=like_author.displayName+" Likes your post"
-							)
-							inbox = Inbox(
-								author=post.author,
-								like=like
-							)
-							like.save()
-							inbox.save()
+							like, like_data = utils.add_like(request, like_author, object_author, comment, "comment")
 						except Exception as e:
 							return Response(data=str(e), status=status.HTTP_400_BAD_REQUEST)
 					else:
 						return Response(data="Like already exists for this post", status=status.HTTP_409_CONFLICT)
 
+			# Check if the request needs to be sent to a remote server
+			try:
+				node = Node.objects.filter(host__icontains=object_author.host).get()
+			except:
+				pass
+			else:
+				s = requests.Session()
+				s.auth = (node.remote_username, node.remote_password)
+				s.headers.update({'Content-Type':'application/json'})
+				url = node.host+"author/"+object_author.id+"/inbox"
+				if 'konnection' in node.host:
+					url += '/'
+				response_like = s.put(url, json=like_data)
 
-				serializer = LikeSerializer(like, context={'request': request})
-				return Response(serializer.data, status=status.HTTP_201_CREATED)
+				if not response_like.status_code in [200, 201]:
+					like.delete()
+					return Response(data="Like not accepted by remote host", status=status.HTTP_400_BAD_REQUEST)
+
+			# Respond with a 201 created
+			return Response(data="Like sent to inbox", status=status.HTTP_201_CREATED)
+
 		# If the sent object is of type Post
 		elif body['type'].lower() == 'post':
-			# Parse request for author of the post and post ID
-			request_author_id = body['author']['id'].split('/')[-1]
-			request_post_id = body['id'].split('/')[-1]
-			# Check if the ID is blank
-			if request_author_id == "":
-				request_author_id = body['author']['id'].split('/')[-2]
-			# Check if the ID is blank
-			if request_post_id == "":
-				request_post_id = body['id'].split('/')[-2]
-			# Check if the author exists in the database, otherwise add the author
+			# Get or create a local copy of the author of the post
 			try:
-				post_author = Author.objects.filter(id=request_author_id).get()
-			except:
-				post_author = Author(
-								id = request_author_id,
-								user = request.user,
-								displayName = body['author']['displayName'],
-								github = body['author']['github'],
-								host = body['author']['host'],
-								url = body['author']['url'],
-								)
-				post_author.save()
-			# Check if the post exists in the database
+				post_author, isLocal = utils.get_author_by_ID(request, body['author']['id'].split('/')[-1], "author")
+			except Exception as e:
+				return Response(data=str(e), status=status.HTTP_400_BAD_REQUEST)
+			# Get or create a local copy of the post
 			try:
-				post = Post.objects.filter(id=request_post_id).get()
-			# Otherwise create the post locally for distribution
-			except:
-				# Check the contentType for images
-				try:
-					if any([types in request.data["contentType"] for types in ['application/base64', 'image/png', 'image/jpeg']]):
-						post = Post(
-							author = post_author,
-							id = request_post_id,
-							title = body["title"],
-							source = body["source"],
-							origin = body["origin"],
-							description = body["description"],
-							contentType = body["contentType"],
-							image_content = body["content"],
-							categories = body["categories"],
-							visibility = body["visibility"],
-							unlisted = body["unlisted"]
-						)
-					# If the contentType is not for images
-					else:
-						post = Post(
-							author = post_author,
-							id = request_post_id,
-							title = body["title"],
-							source = body["source"],
-							origin = body["origin"],
-							description = body["description"],
-							contentType = body["contentType"],
-							content = body["content"],
-							categories = body["categories"],
-							visibility = body["visibility"],
-							unlisted = body["unlisted"]
-						)
-					post.save()
-				except Exception as e:
-					return Response(data=str(e), status=status.HTTP_400_BAD_REQUEST)
-			# Check if the inbox owner is a valid author
-			try:
-				inbox_author = Author.objects.filter(id=author_id).get()
-			except:
-				return Response(status=status.HTTP_404_NOT_FOUND)
+				post, post_data, exists = utils.add_post(request, post_author, body['id'].split('/')[-1])
+			except Exception as e:
+				return Response(data=str(e), status=status.HTTP_400_BAD_REQUEST)
 			# Add the post to the inbox
 			try:
 				inbox = Inbox(
@@ -322,85 +216,41 @@ class InboxAPI(viewsets.ModelViewSet):
 				inbox.save()
 			except Exception as e:
 					return Response(data=str(e), status=status.HTTP_400_BAD_REQUEST)
-
-			return Response(status=status.HTTP_201_CREATED)
+			# Respond with a 201 created
+			return Response(data="Post sent to inbox",status=status.HTTP_201_CREATED)
 
 		# If the sent object is of type Comment
 		elif request.data['type'].lower() == 'comment':
-
-			# Parse the author and id of the body to find the comment id, the author of the comment, and the post id that the comment is on
-			request_author_id = request.data['author']['id'].split('/')[-1]
-			request_comment_id = body['id'].split('/')[-1]
-			request_post_id = body['id'].split('/')[-3]
-
-			# Check if any of the previous values is blank, and try to locate it elsewhere
-			if request_author_id == "":
-				request_author_id = body['author']['id'].split('/')[-2]
-
-			if request_comment_id == "":
-				request_comment_id = body['id'].split('/')[-2]
-
-			if request_post_id == "comments":
-				request_comment_id = body['id'].split('/')[-4]
-
-			# Get the author of the comment, if they do not exists create a local author object for them to associate to the comment
+			# Get the post author, post and comment
 			try:
-				comment_author = Author.objects.filter(author=request_author_id).get()
-			except:
+				post_author, post, comment = utils.get_object_data(request, "id")
+			except Exception as e:
+				return Response(data=str(e), status=status.HTTP_404_NOT_FOUND)
+			# Get the author of the comment if they exist
+			try:
+				comment_author, isLocal = utils.get_author_by_ID(request, body["author"]["id"].split('/')[-1], "author")
+			except Exception as e:
+				return Response(data=str(e), status=status.HTTP_400_BAD_REQUEST)
+			# Check if the author of the post is the recipient of the inbox item
+			if post_author.id != inbox_author.id:
+				return Response(data="Creator does not match inbox owner", status=status.HTTP_400_BAD_REQUEST)
+			# If a comment does not exist locally
+			if comment is None:
 				try:
-					comment_author = Author(
-									id = request_author_id,
-									user = request.user,
-									displayName = request.data['author']['displayName'],
-									github = request.data['author']['github'],
-									host = request.data['author']['host'],
-									url = request.data['author']['url'],
-									)
-					comment_author.save()
+					comment, comment_data = utils.add_comment(request, comment_author, post)
 				except Exception as e:
-					return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
-			# Check that the post being commented on exists
-			try:
-				post = Post.objects.filter(id=request_post_id).get()
-			except:
-				return Response(status=status.HTTP_404_NOT_FOUND, data="Could not find the post the comment belongs to!")
-			# Check if the comment already exists, if it does not add the comment to the database
-			try:
-				received_comment = Comment.objects.filter(id=request_comment_id).get()
-			except:
-				# Create th comment object in the database
-				try:
-					request_host = request.data['id'].split('/')[2]
-					received_comment = Comment(
-						id = request_comment_id,
-						author = comment_author,
-						post = post,
-						comment = request.data.get('comment', None),
-						contentType = request.data.get('contentType', None),
-						host = request_host,
-						post_author = post.author.id,
-					)
-					received_comment.save()
-				except Exception as e:
-					return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
-
-			# Try to get the author whos inbox the comment needs to be added to and return a 404 if they are not found
-			try:
-				inbox_author = Author.objects.filter(id=author_id).get()
-			except:
-				return Response(status=status.HTTP_404_NOT_FOUND)
-
-			# Add the post to the inbox
+					return Response(data=str(e), status=status.HTTP_400_BAD_REQUEST)
+			# Create an inbox item
 			try:
 				inbox = Inbox(
 					author=inbox_author,
-					icomment=received_comment
+					icomment=comment
 				)
 				inbox.save()
 			except Exception as e:
 					return Response(data=str(e), status=status.HTTP_400_BAD_REQUEST)
-		else:
-			return Response(data=request, status=status.HTTP_410_GONE)
+			# Respond that the comment was created with a 201
+			return Response(data="Comment sent to inbox", status=status.HTTP_201_CREATED)
 
 	def destroy(self, request, author_id=None, *args, **kwargs):
 		"""
