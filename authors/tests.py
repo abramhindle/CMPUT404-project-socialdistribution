d9b9956adb88c796b4cc367aaa3b90d772b4a9b1
c25vdw <monoro@pm.me>
2021-09-26 16:07:45 -0600

    authors: Add basic GET unittests.

--- authors/tests.py
+++ authors/tests.py
@@ -1,3 +1,37 @@
+import json
 from django.test import TestCase
+from rest_framework.test import APIClient
+
+from django.contrib.auth.models import User
+from authors.models import Author
 
 # Create your tests here.
+
+client = APIClient() # the mock http client
+
+class AuthorTestCase(TestCase):
+    def setup_single_user_and_author(self):
+        self.user = User.objects.create_user('test_username', 'test_email', 'test_pass')
+        self.author = Author.objects.create(user=self.user, displayName=self.user.username)
+    def setUp(self):
+        self.setup_single_user_and_author()
+
+    def test_get_author_list(self):
+        res = client.get('/authors/', format='json')
+        content = json.loads(res.content)
+
+        # content should look like [{'id': 'adfsadfasdfasdf', 'displayName': 'test_username', 'url': '', 'host': '', 'user': 1, 'friends': []}]
+        assert len(content) == 1
+        assert content[0]['displayName'] == 'test_username'
+        assert content[0]['user'] == self.user.pk
+        assert res.status_code == 200
+        
+    def test_get_author_detail(self):
+        res = client.get(f'/author/{self.author.id}/', format='json')
+        content = json.loads(res.content)
+
+        # content should look like {'id': 'adfsadfasdfasdf', 'displayName': 'test_username', 'url': '', 'host': '', 'user': 1, 'friends': []}
+        assert content['displayName'] == 'test_username'
+        assert content['user'] == self.user.pk
+        assert res.status_code == 200
+        
