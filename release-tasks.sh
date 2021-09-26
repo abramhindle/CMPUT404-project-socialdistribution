be9f735c7874c4fbbd5ca7f4cafcba6e6ca019f4
c25vdw <monoro@pm.me>
2021-09-25 21:25:03 -0600

    Superuser creation if only it doesn't exist.

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
 # https://devcenter.heroku.com/articles/release-phase#specifying-release-phase-tasks
 python manage.py migrate
-python manage.py createsuperuser --noinput
+
+# create DJANGO_SUPERUSER_USERNAME if it doesn't exist
+cat <<EOF | python manage.py shell
+from django.contrib.auth import get_user_model
+import os
+
+DJANGO_SUPERUSER_USERNAME = os.getenv('DJANGO_SUPERUSER_USERNAME')
+DJANGO_SUPERUSER_PASSWORD = os.getenv('DJANGO_SUPERUSER_PASSWORD')
+DJANGO_SUPERUSER_EMAIL = os.getenv('DJANGO_SUPERUSER_EMAIL')
+
+User = get_user_model()  # get the currently active user model,
+
+User.objects.filter(username=DJANGO_SUPERUSER_USERNAME).exists() or \
+    User.objects.create_superuser(DJANGO_SUPERUSER_USERNAME, DJANGO_SUPERUSER_EMAIL, DJANGO_SUPERUSER_PASSWORD)
+EOF
+
