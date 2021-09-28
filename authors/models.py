a1b6728e3e07b19673e4b44c14d2ec40a1c04506
c25vdw <monoro@pm.me>
2021-09-28 00:32:59 -0600

    authors: Add /author/<id>/ POST.

--- authors/models.py
+++ authors/models.py
@@ -1,23 +1,53 @@
 import uuid
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.urls import reverse 
 from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class Author(models.Model):
     id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
     user = models.OneToOneField(User, on_delete=models.CASCADE) # one2one with django user
-    friends = models.ManyToManyField("self") # bidirectional/symmetrical by default
+    friends = models.ManyToManyField('Author', blank=True, symmetrical=True) # bidirectional/symmetrical by default, allow empty
 
     display_name = models.CharField(max_length=30) # maximum 30 chars for display name
-    url = models.URLField() # the url to the author profile
-    host = models.URLField() # the host server node url, ours is https://social-distance-api.herokuapp.com/
+    github_url = models.URLField(null=True) # the url to the author github profile
+    url = models.URLField(editable=False) # the url to the author profile
+    host = models.URLField(editable=False) # the host server node url, ours is https://social-distance-api.herokuapp.com/
 
     # following: Authors, added by related name, see AuthorFollowingRelation
     # followers: Authors, added by related name, see AuthorFollowingRelation
 
+    # used by serializer
+    def get_public_id(self):
+        return self.url or self.id
+
+    # used by serializer
+    def get_api_type(self):
+        # https://stackoverflow.com/a/18396622
+        return 'author'
+
+    # used internally
     def get_absolute_url(self):
         return reverse('author-detail', args=[str(self.id)])
+
+    # clean up whenever trying to validate model object
+    def clean(self):
+        # enforce author-user one2one
+        if self.user is None:
+            raise ValidationError(_('Author object has to have a User object linked.'))
+
+        # default display_name to username
+        if not self.display_name:
+            self.display_name = self.user.username
+
+    # used by serializer
+    def update_url_with_request(self, request):
+        self.url = request.build_absolute_uri(self.get_absolute_url())
+        self.save()
+
+
 
 class AuthorFriendRequest(models.Model):
     """
--- authors/tests.py
+++ authors/tests.py
@@ -14,24 +14,28 @@ class AuthorTestCase(TestCase):
         self.user = User.objects.create_user('test_username', 'test_email', 'test_pass')
         self.author = Author.objects.create(user=self.user, display_name=self.user.username)
     def setUp(self):
-        self.setup_single_user_and_author()
+        pass
 
     def test_get_author_list(self):
+        self.setup_single_user_and_author()
         res = client.get('/authors/', format='json')
         content = json.loads(res.content)
 
-        # content should look like [{'id': 'adfsadfasdfasdf', 'display_name': 'test_username', 'url': '', 'host': '', 'user': 1, 'friends': []}]
+        # content should look like [{'id': 'adfsadfasdfasdf', 'displayName': 'test_username', 'url': '', 'host': '', 'user': 1, 'friends': []}]
         assert len(content) == 1
-        assert content[0]['display_name'] == 'test_username'
-        assert content[0]['user'] == self.user.pk
+
+        # API fields as per spec, not model fields.
+        assert content[0]['displayName'] == 'test_username'
+        assert content[0]['id'] == str(self.author.id)
         assert res.status_code == 200
         
     def test_get_author_detail(self):
+        self.setup_single_user_and_author()
         res = client.get(f'/author/{self.author.id}/', format='json')
         content = json.loads(res.content)
 
-        # content should look like {'id': 'adfsadfasdfasdf', 'display_name': 'test_username', 'url': '', 'host': '', 'user': 1, 'friends': []}
-        assert content['display_name'] == 'test_username'
-        assert content['user'] == self.user.pk
+        # content should look like {'id': 'adfsadfasdfasdf', 'displayName': 'test_username', 'url': '', 'host': '', 'user': 1, 'friends': []}
+        assert content['displayName'] == 'test_username'
+        assert content['id'] == str(self.author.id)
         assert res.status_code == 200
         
