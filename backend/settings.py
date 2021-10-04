8140e24f7ab230c08ba620a244f90059fcecaf05
Zubier Hagi <zubier@Zubiers-MacBook-Pro.local>
2021-10-03 23:24:57 -0600

    masking secret keys

--- backend/settings.py
+++ backend/settings.py
@@ -9,18 +9,22 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import os, django_heroku
+import os, django_heroku, dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g74(3r4zsypvlf0s7%ao5vsn^qg6cjtpt#!p8e-=-qpxx$w3$v'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
