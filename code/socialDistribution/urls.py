3a64d1636e0fc6825636fb6bb7c56fd624729c66
Tyler Elston <24640479+tylerelston@users.noreply.github.com>
2021-10-07 14:42:55 -0600

    Base html file (#54)
    Co-authored-by: Jawad Hossain <jawad.hossain@ibm.com>

--- code/socialDistribution/urls.py
+++ code/socialDistribution/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.index, name='index')
+  path('', views.index, name='index'),
+  path('home/', views.home, name='home'),
+  path('authors/', views.authors, name='authors'),
+  path('create/', views.create, name='create'),
+  path('post/', views.post, name='post'),
+  path('profile/', views.profile, name='profile'),
+  path('register/', views.register, name='register'),
+  path('user/', views.user, name='user'),
 ]
--- code/socialDistribution/views.py
+++ code/socialDistribution/views.py
@@ -5,3 +5,23 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the Login/SignUp Page.")
 
+def home(request):
+    return render(request, 'home/index.html')
+
+def authors(request):
+    return render(request, 'authors/index.html')
+
+def create(request):
+    return render(request, 'create/index.html')
+
+def post(request):
+    return render(request, 'post/index.html')
+
+def profile(request):
+    return render(request, 'profile/index.html')
+
+def register(request):
+    return render(request, 'register/index.html')
+
+def user(request):
+    return render(request, 'user/index.html')
