8aa84297fde419a5ba8513583456882f987f0b2d
Lucas <lucaszeng99@gmail.com>
2021-10-07 22:59:04 -0600

    auth: JWT, fleshed.

--- social_distance/serializers.py
+++ social_distance/serializers.py
@@ -2,21 +2,57 @@ from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
 
 from rest_framework import serializers
+from rest_framework_simplejwt.tokens import RefreshToken
+from authors.models import Author
 
-class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+from authors.serializers import AuthorSerializer
+
+
+class CommonAuthenticateSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(write_only=True, required=True)
+
+    access_token = serializers.CharField(read_only=True)
+    refresh_token = serializers.CharField(read_only=True)
+    author = AuthorSerializer(read_only=True)
+
+    def to_representation(self, instance):
+        """
+        final customization to make for the output object:
+        - add tokens to the reponse body
+        """
+        base_dict = {
+            'username': instance.username,
+            'author': AuthorSerializer(instance.author).data if hasattr(instance, 'author') else None
+        }
+        refresh = RefreshToken.for_user(instance)
+        access = refresh.access_token
+
+        base_dict['access_token'] = str(access)
+        base_dict['refresh_token'] = str(refresh)
+
+        return base_dict
+
+
+class RegisterSerializer(CommonAuthenticateSerializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(
+        write_only=True, required=True, validators=[validate_password])
     display_name = serializers.CharField(required=False)
     github_url = serializers.URLField(required=False)
 
-    class Meta:
-        model = User
-        fields = ('username', 'password', 'display_name', 'github_url')
-        
     def create(self, validated_data):
+        """
+        called when calling .save(), create objects from validated_data
+        """
         user = User.objects.create(
             username=validated_data['username'],
         )
         user.set_password(validated_data['password'])
         user.save()
-
+        author = Author(user=user, display_name=validated_data.get(
+            'display_name', user.username), github_url=validated_data.get('github_url'))
+        # modify url to be server path
+        author.update_fields_with_request(self.context['request'])
+        author.save()
         return user
--- social_distance/urls.py
+++ social_distance/urls.py
@@ -15,21 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from rest_framework_simplejwt.views import TokenRefreshView
+
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
-from .views import register
+from .views import register, login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('register/', register),
-    # jwt auth
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # auth
+    path('register/', register),  # includes author info
+    path('login/', login),  # includes author info
+    path('token-refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     # authors app
     path('authors/', include('authors.urls_authors')),
@@ -37,8 +36,6 @@ urlpatterns = [
 
     # root
     path('schema/', SpectacularAPIView.as_view(), name='open-schema'),
-    path('', SpectacularSwaggerView.as_view(url_name='open-schema'), name='api-root'),
-
-    # TODO login, logout, register
+    path('', SpectacularSwaggerView.as_view(
+        url_name='open-schema'), name='api-root'),
 ]
-
