ee724f062d629a2f98fe2467065957ec35864a17
Jihoon Og <jihoonog@gmail.com>
2021-10-10 17:17:15 +0000

    minor fix

--- social/models.py
+++ social/models.py
@@ -4,5 +4,4 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    id = models.ForeignKey(User, on_dlete=models.CASCADE, related_name='blog_post')
     content = models.TextField()
