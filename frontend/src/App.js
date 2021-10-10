865040aca3da6597b21d2ff35fd77f553c1d7fb2
Maristella Jho <56034486+hyunseo6579@users.noreply.github.com>
2021-10-10 15:52:54 -0600

    initialize react

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import Headers from "./components/Headers";
+
+function App() {
+  return (
+    <div className="App">
+      <Headers />
+      <main>
+        <h1>Hello World</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Headers.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Navbar, Nav, Container, Row, Col } from "react-bootstrap";
+
+function Headers() {
+  return (
+    <header>
+      <Navbar bg="primary" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="#home">Social Distribution</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+}
+
+export default Headers;
