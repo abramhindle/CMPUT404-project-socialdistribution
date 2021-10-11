dd9776c72409ff650a12c590aaa57963469bbdde
Student <zhpeng@ualberta.ca>
2021-10-11 02:00:05 -0400

    add posts and comment endpoints

--- posts/serializers.py
+++ posts/serializers.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext_lazy as _
 
 from rest_framework import serializers
 
-from .models import Post
+from .models import Post, Comment
 from authors.serializers import AuthorSerializer
 
 class PostSerializer(serializers.ModelSerializer):
@@ -42,3 +42,23 @@ class PostSerializer(serializers.ModelSerializer):
             'unlisted'
         ]
 
+class CommentSerializer(serializers.ModelSerializer):
+    # type is only provided to satisfy API format
+    type = serializers.CharField(default="comment", source="get_api_type", read_only=True)
+    # public id should be the full url
+    id = serializers.CharField(source="get_public_id", read_only=True)
+    
+    #TODO: is the the author who commented or the post's author?
+    author = AuthorSerializer(read_only=True)
+    contentType = serializers.ChoiceField(choices=Post.ContentType.choices, source='content_type')
+
+    class Meta:
+        model = Comment
+        fields = [
+            "type",
+            "author",
+            "comment",
+            "contentType",
+            "published",
+            "id"
+        ]
--- posts/views.py
+++ posts/views.py
@@ -5,9 +5,10 @@ from rest_framework.response import Response
 from rest_framework import status, permissions
 
 from authors.models import Author
-from .models import Post
-from .serializers import PostSerializer
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
 
+import uuid
 import copy
 
 class PostDetail(APIView):
@@ -15,7 +16,7 @@ class PostDetail(APIView):
         return PostSerializer
 
     """
-    Get author posts
+    Get author post with the post_id
     """
     def get(self, request, author_id, post_id):
         try:
@@ -82,6 +83,76 @@ class PostDetail(APIView):
                 **serializer.validated_data
             )
             post.update_fields_with_request(request)
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class PostList(APIView):
+    def get_serializer_class(self):
+        return PostSerializer
+
+    # """
+    # Get recent posts of author (paginated)
+    # """
+    # TODO:
+    # def get(self, request, author_id):
+    #     pass
+    
+    """
+    Create a Post with generated post id
+    """
+    def post(self, request, author_id):
+        post_id = uuid.uuid4()
+        return PostDetail().put(request, author_id, post_id)
+
+class CommentDetail(APIView):
+    def get_serializer_class(self):
+        return CommentSerializer
+
+    """
+    get comments of the post
+    """
+    def get(self, request, author_id, post_id):
+        try:
+            comments = Comment.objects.filter(post_id=post_id).order_by('-published')
+        except Post.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        params = request.query_params.dict()
+        try:
+            # defaulting to page 1 with size 20
+            page = int(params["page"]) if "page" in params else 1
+            size = int(params["size"]) if "size" in params else 20
+        except ValueError:
+            # page and size must be integers
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+        serializer = CommentSerializer(comments, many=True)
+        lower_index = (page - 1) * size
+        upper_index = page * size
+        if (lower_index >= len(serializer.data)):
+            # when requesting pages that are out of bound
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        return Response(serializer.data[lower_index:upper_index])
+    
+    """
+    add comment to the post
+    """
+    def post(self, request, author_id, post_id):
+        try:
+            author = Author.objects.get(pk=author_id)
+            post = Post.objects.get(pk=post_id)
+        except (Author.DoesNotExist, Post.DoesNotExist):
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            comment = Comment.objects.create(
+                author=author, 
+                post=post,
+                **serializer.validated_data
+            )
+            comment.update_fields_with_request(request)
             return Response(status=status.HTTP_204_NO_CONTENT)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
