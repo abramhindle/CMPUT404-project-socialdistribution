fee6e58d900951b2b96a28162244c60a90926a5e
mrboisvert <michaelboisvert6@gmail.com>
2021-10-17 13:14:14 -0600

    get comments

--- post/serializers.py
+++ post/serializers.py
@@ -1,13 +1,13 @@
 from rest_framework import serializers
 from .models import Comment, Like, Post
-from author.serializers import AuthorCreationSerializer
+from author.serializers import AuthorSerializer
 
 class CommentSerializer(serializers.ModelSerializer):
     id = serializers.CharField(source="get_id", read_only=True)
     type = serializers.CharField(default="comment", read_only=True)
     comment = serializers.CharField(source="get_content", read_only=True)
     published = serializers.DateTimeField(source="get_date", read_only=True)
-    author = AuthorCreationSerializer(source="get_author")
+    author = AuthorSerializer(source="get_author")
 
     class Meta:
         model = Comment
--- post/views.py
+++ post/views.py
@@ -6,14 +6,25 @@ from rest_framework.response import Response
 from .models import Post, Like, Comment
 import json
 from django.core.paginator import Paginator
+from .serializers import CommentSerializer
 
 class index(APIView):
     pass
 
 class comments(APIView):
-    def get(self, request):
-        page = request.query_params.get("page")
-        return Response(status=200)
+    def get(self, request, author_id, post_id):
+        try:
+            post_comments = Comment.objects.filter(postID=post_id).order_by("date")
+        except:
+            return Response("The requested post does not exist.", status=404)
+        size = request.query_params.get("size", 5)
+        page = request.query_params.get("page", 1)
+        paginator = Paginator(post_comments, size)
+        comment_serializer = CommentSerializer(paginator.get_page(page), many=True)
+        url = request.build_absolute_uri('')
+        post = url[:-len("/comments")]
+        response = {"type": "comments", "page": page, "size": size, "post": post, "id": url, "comments": comment_serializer.data}
+        return Response(response, status=200)
 
     def post(self, request, comment):
         pass
