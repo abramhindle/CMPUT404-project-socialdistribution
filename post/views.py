9428eec0e4caeb4b23bd304a627a5e2dfb9ccb77
mrboisvert <michaelboisvert6@gmail.com>
2021-10-17 14:03:27 -0600

    added authorization to comment GET

--- post/views.py
+++ post/views.py
@@ -4,26 +4,37 @@ from django.http import HttpResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Post, Like, Comment
+from author.models import Author
 import json
 from django.core.paginator import Paginator
 from .serializers import CommentSerializer
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class index(APIView):
     pass
 
 class comments(APIView):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
     def get(self, request, author_id, post_id):
         try:
+            post = Post.objects.get(postID=post_id, ownerID=author_id)
             post_comments = Comment.objects.filter(postID=post_id).order_by("date")
-        except:
+        except Exception as e:
+            print(e)
             return Response("The requested post does not exist.", status=404)
+        if not post.isPublic and str(request.user.author.authorID) != author_id:
+            # only the author of the post can view the comments if the post is not public
+            return Response("This post's comments are private.", status=403)
         size = request.query_params.get("size", 5)
         page = request.query_params.get("page", 1)
         paginator = Paginator(post_comments, size)
         comment_serializer = CommentSerializer(paginator.get_page(page), many=True)
         url = request.build_absolute_uri('')
-        post = url[:-len("/comments")]
-        response = {"type": "comments", "page": page, "size": size, "post": post, "id": url, "comments": comment_serializer.data}
+        post_url = url[:-len("/comments")]
+        response = {"type": "comments", "page": page, "size": size, "post": post_url, "id": url, "comments": comment_serializer.data}
         return Response(response, status=200)
 
     def post(self, request, comment):
