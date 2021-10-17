fb20c42daf7f1f833bce93beb37004e64b8819f1
Ben Wang <xiaoyu.w.99@gmail.com>
2021-10-17 16:05:17 -0600

    fixed errors in post views

--- post/serializers.py
+++ post/serializers.py
@@ -1,11 +1,17 @@
 from django.db import models
 from .models import Post,Like,Comment
 from rest_framework import serializers
+from author.serializers import AuthorSerializer
 
 class PostSerializer(serializers.ModelSerializer):
     id = serializers.CharField(source="get_url", read_only=True)
     url = serializers.CharField(source="get_url", read_only=True)
     type = serializers.CharField(default="post", read_only=True)
+    author = AuthorSerializer(source="ownerID")
     class Meta:
         model = Post
-        fields = ['id','author','content','content-type','isPublic','isListed','hasImage','host','url','type']
+        fields = ['id', 'author', 'content', 'content-type', 'isPublic', 'isListed', 'hasImage', 'host', 'url', 'type']
+        extra_kwargs = {
+            # rename contentType to content-type
+            'content-type': {'source': 'contentType'},
+        }
--- post/views.py
+++ post/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.db.models import Subquery
-from django.http import HttpResponse, http404
+from django.http import HttpResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Post, Like, Comment
@@ -22,22 +22,22 @@ class post(APIView):
         #url only has owner_id: want all posts of a user
         post_ids = None
         if not post_id:
-            post_ids = Post.objects.filter(ownerID = owner_id)
+            post_ids = Post.objects.filter(ownerID = author_id)
             #nothing was returned for that user-Id
         #url has both owner_id and post_id: want specific post from a user
-        elif post_id and owner_id:
-            post_ids = Post.objects.filter(ownerID = owner_id, postID = post_id)
-        
+        elif post_id and author_id:
+            post_ids = Post.objects.filter(ownerID=author_id, postID=post_id)
+
         if not post_ids:
             return Response(status = 404)
 
-        serializer = PostSerializer(post_ids)
-        response = {'type':'posts', 'items': serializer}
+        serializer = PostSerializer(post_ids, many=True)
+        response = {'type':'posts', 'items': serializer.data}
         return Response(response)
-    
+
     def post():
         pass
-    
+
     def put():
         pass
 
