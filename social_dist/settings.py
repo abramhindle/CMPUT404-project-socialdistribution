964dd1808e73d33a1cef9d18665c09b9bb2aced2
Jihoon Og <jihoonog@gmail.com>
2021-10-26 17:00:18 +0000

    Updated settings for heroku

--- social_dist/settings.py
+++ social_dist/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -146,3 +147,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+django_on_heroku.settings(locals())
