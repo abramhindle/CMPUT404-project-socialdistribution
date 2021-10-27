8fe005696d4c065ec9f02b8e69994afe9080e08f
Jihoon Og <jihoonog@gmail.com>
2021-10-27 03:54:11 +0000

    Revert "fixed url"
    This reverts commit 932c4d3c14f5424821816608d4db399e85357643.

--- social_dist/urls.py
+++ social_dist/urls.py
@@ -20,6 +20,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index.html')),
-    path('', include('backend.urls'))
+    path('api/', include('backend.urls'))
 ]
  
