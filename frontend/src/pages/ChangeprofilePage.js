d5975fc56d81ca6c1d0c3967ea0c0f695683c95a
dazhi-z <dazhi@ualberta.ca>
2021-10-26 20:47:23 -0600

    add profile page and user can change their info

---
+++ frontend/src/pages/ChangeprofilePage.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { Container, Row, Col, Button, Image, Alert, Stack, Nav, Form } from "react-bootstrap";
+import Headers from "../components/Headers";
+import SideBar from "../components/SideBar";
+import Avatar from "../images/avatar.jpg";
+import {LinkContainer} from "react-router-bootstrap";
+
+function ChangeprofilePage() {
+  return (
+    <Container className="App fluid min-vh-100 min-vw-100 d-flex flex-column p-0">
+      <Headers />
+      <Row className="flex-grow-1 m-0">
+        <Col className="bg-secondary col-md-2 border">
+          <SideBar />
+        </Col>
+        <Col className="justify-content-center">
+            <Stack gap={3} style={{marginTop:"7%", marginLeft:"40%"}}>
+                <Image src={Avatar} width="20%" height="15%"></Image>
+                <Form style={{marginLeft:"-13%", width:"50%"}}>
+                  <Form.Group className="mb-3" controlId="formBasicUsername">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control type="username" placeholder="This is the original username"/>
+                  </Form.Group>
+
+                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" placeholder="This is the original email"/>
+                  </Form.Group>
+
+                  <Form.Group className="mb-3" controlId="formBasicUsername">
+                    <Form.Label>Github url</Form.Label>
+                    <Form.Control type="githuburl" placeholder="This is the original Github url"/>
+                  </Form.Group>
+                </Form>
+                
+
+                <Stack 
+                  gap={5} 
+                  direction="horizontal" 
+                  style={{marginLeft:"-10%", marginTop:"-3%"}}
+                  >
+                  <LinkContainer 
+                    to="/profile" 
+                    style={{width:"7%", height:"10%", marginTop:"3%", marginRight:"3%"}}
+                  >
+                    <Nav.Link>
+                      <Button 
+                        style={{backgroundColor:"orange", width:""}}
+                      >
+                        Save
+                      </Button>
+                    </Nav.Link>
+                  </LinkContainer>
+
+                  <LinkContainer 
+                    to="/profile" 
+                    style={{width:"7%", height:"10%", marginTop:"3%"}}
+                  >
+                    <Nav.Link>
+                      <Button 
+                        style={{backgroundColor:"black", width:""}}
+                      >
+                        Cancel
+                      </Button>
+                    </Nav.Link>
+                  </LinkContainer>
+                </Stack>
+            </Stack>
+        </Col>
+      </Row>
+    </Container>
+  
+  );
+}
+
+export default ChangeprofilePage;
---
+++ frontend/src/pages/ProfilePage.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Container, Row, Col, Button, Image, Alert, Stack } from "react-bootstrap";
+import Headers from "../components/Headers";
+import SideBar from "../components/SideBar";
+import Avatar from "../images/avatar.jpg";
+import EditIcon from "../images/edit.png";
+import {LinkContainer} from "react-router-bootstrap";
+
+function ProfilePage() {
+  return (
+    <Container className="App fluid min-vh-100 min-vw-100 d-flex flex-column p-0">
+      <Headers />
+      <Row className="flex-grow-1 m-0">
+        <Col className="bg-secondary col-md-2 border">
+          <SideBar />
+        </Col>
+        <Col className="justify-content-center">
+            <Stack gap={3} style={{marginTop:"7%", marginLeft:"40%"}}>
+                <Image src={Avatar} width="20%" height="15%"></Image>
+                <Alert style={{marginLeft:"-13%", width:"50%"}}>I'm username</Alert>
+                <Alert style={{marginLeft:"-13%", width:"50%"}}>I'm email</Alert>
+                <Alert style={{marginLeft:"-13%", width:"50%"}}>I'm Github url</Alert>
+
+                <LinkContainer to="/changeprofile" style={{marginLeft:"90%", marginTop:"10%", backgroundColor:"orange"}}>
+                  <Button>
+                    <img src={EditIcon} style={{width:"20px"}}></img>
+                  </Button>
+                </LinkContainer>
+                
+            </Stack>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default ProfilePage;
