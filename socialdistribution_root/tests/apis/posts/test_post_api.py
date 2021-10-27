578d3778fc68c0ef700bbf28761b93a3f9e69f14
Student <bondarau@ualberta.ca>
2021-10-27 00:20:25 -0400

    update failing tests

--- socialdistribution_root/tests/apis/posts/test_post_api.py
+++ socialdistribution_root/tests/apis/posts/test_post_api.py
@@ -6,6 +6,9 @@ from rest_framework.test import APIClient
 
 from socialdistribution_root.tests.test_helper.auth_helper import AuthHelper
 
+from socialdistribution.pagination import DEFAULT_PAGE
+from socialdistribution.pagination import DEFAULT_PAGE_SIZE
+
 class CoreViewTests(TestCase):
 
     def setup(self):
@@ -21,7 +24,11 @@ class CoreViewTests(TestCase):
         response = self.client.get(reverse('post_api:posts', kwargs={'author_id':author.id}))
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(json.loads(response.content), [])
+        dict_resp = json.loads(response.content)
+
+        # checking default pagination
+        self.assertEqual(dict_resp["page"], DEFAULT_PAGE)
+        self.assertEqual(dict_resp["size"], DEFAULT_PAGE_SIZE)
 
     def test_post_posts(self):
         self.setup()
@@ -41,13 +48,14 @@ class CoreViewTests(TestCase):
         }
         
         response = self.client.post(reverse('post_api:posts', kwargs={'author_id':author.id}), data, format="json")
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 201)
     
         dict_resp = json.loads(response.content)
-        self.assertEqual(dict_resp["type"], data["type"])
-        self.assertEqual(dict_resp["title"], data["title"])
-        self.assertEqual(dict_resp["description"], data["description"])
-        self.assertEqual(dict_resp["contentType"], data["contentType"])
-        self.assertEqual(dict_resp["visibility"], data["visibility"])
+        dict_resp_data = dict_resp["data"]
+        self.assertEqual(dict_resp_data["type"], data["type"])
+        self.assertEqual(dict_resp_data["title"], data["title"])
+        self.assertEqual(dict_resp_data["description"], data["description"])
+        self.assertEqual(dict_resp_data["contentType"], data["contentType"])
+        self.assertEqual(dict_resp_data["visibility"], data["visibility"])
         # Public post uri-id contains it's authors id in it
-        self.assertIn(str(author.id), dict_resp["id"])
+        self.assertIn(str(author.id), dict_resp_data["id"])
