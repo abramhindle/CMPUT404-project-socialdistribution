5010e58c7b9889f9d064c6610e60a0afcbcb024a
Abdelrahman Abdalla <ayabdall@ualberta.ca>
2021-10-29 16:56:37 -0400

    Priliminary Front end for posts that just displayed post names in a list

--- socialdistribution_root/apps/posts/views.py
+++ socialdistribution_root/apps/posts/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.views import generic
+from django.shortcuts import render
+from django.template import loader
 
-# Create your views here.
-class IndexView(generic.TemplateView):
-    template_name = 'posts/index.html'
+from .models import Post
+
+def index(request):
+    posts = Post.objects.all()
+
+    template = loader.get_template('posts/index.html')
+    context = {'posts': posts,}
+    return render(request, 'posts/index.html', context)
