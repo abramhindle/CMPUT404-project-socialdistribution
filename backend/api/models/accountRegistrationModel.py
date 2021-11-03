43d592de36ccd50a9666255d7c83cbb63e187410
Ayo Akindele <a.akindele01@gmail.com>
2021-11-03 15:36:27 -0600

    make displayName a required field for accountRegistration

--- backend/api/models/accountRegistrationModel.py
+++ backend/api/models/accountRegistrationModel.py
@@ -23,7 +23,7 @@ def validateGithub(github):
 # Account Registration Model 
 class accountRequest(models.Model):  
     # Account display name
-    displayName = models.CharField(blank=True, max_length=100)
+    displayName = models.CharField(max_length=100)
     # Account password
     password = models.CharField(max_length=128, verbose_name='password')
     # Account Github
