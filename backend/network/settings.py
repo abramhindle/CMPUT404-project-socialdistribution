286c03b633e2eb6d43965adc42339b7816665ebb
Ayo Akindele <a.akindele01@gmail.com>
2021-11-05 04:00:11 -0600

    edit settings.py

--- backend/network/settings.py
+++ backend/network/settings.py
@@ -178,7 +178,7 @@ APPEND_SLASH = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'build/static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR,'build/static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'build', 'media')
