131a31738c94f99427f882af3361996f4ceb90df
Ayo Akindele <a.akindele01@gmail.com>
2021-11-05 00:32:46 -0600

    add static files

--- backend/network/settings.py
+++ backend/network/settings.py
@@ -13,7 +13,7 @@ import os, django_heroku, dotenv, dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 dotenv_file = os.path.join(BASE_DIR, ".env")
 if os.path.isfile(dotenv_file):
