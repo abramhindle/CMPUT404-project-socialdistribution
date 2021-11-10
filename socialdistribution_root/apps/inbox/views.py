d1a882151e08e601657b3e028eca12907c0dd232
Matthew Mullen <matthew.mullenx@gmail.com>
2021-11-10 15:38:19 -0700

    added some html for the inbox

--- socialdistribution_root/apps/inbox/views.py
+++ socialdistribution_root/apps/inbox/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from django.template import loader
+from .models import InboxItem
 
-# Create your views here.
+def index(request):
+    inbox = InboxItem.objects.all()
+    template = loader.get_template('inbox/index.html')
+    context={'inboxitems',inbox}
+    return render(request,'inbox/index.html',context)    
