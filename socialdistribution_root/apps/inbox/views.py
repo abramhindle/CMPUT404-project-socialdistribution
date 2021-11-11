54ccc7cc0f203c15a68b848f9a50847c72d3da53
Matthew Mullen <mtmullen@ualberta.ca>
2021-11-10 22:01:13 -0700

    defined variable types

--- socialdistribution_root/apps/inbox/views.py
+++ socialdistribution_root/apps/inbox/views.py
@@ -7,8 +7,9 @@ from django.http.request import HttpRequest
 
 def index(request: HttpRequest):
     inbox = InboxItem.objects.all()
-    print("HERE IS THE REQUEST")
-    print(request)
+    print("HERE IS THE REQUEST PATH")
+    path=request.path()
+    print(path)
     template = loader.get_template('inbox/index.html')
     context={'inboxitems',inbox}
     return render(request,'inbox/index.html',context)    
