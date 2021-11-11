aae262af1053d0ea7ed2c99c561d99b92c6f4bba
Matthew Mullen <mtmullen@ualberta.ca>
2021-11-10 21:57:59 -0700

    defined variable types

--- socialdistribution_root/apps/inbox/views.py
+++ socialdistribution_root/apps/inbox/views.py
@@ -1,9 +1,11 @@
+from django.http.request import HttpRequest
 from django.shortcuts import render
 from django.views import generic
 from django.template import loader
 from .models import InboxItem
+from django.http.request import HttpRequest
 
-def index(request):
+def index(request: HttpRequest):
     inbox = InboxItem.objects.all()
     print("HERE IS THE REQUEST")
     print(request)
--- socialdistribution_root/apps/posts/views.py
+++ socialdistribution_root/apps/posts/views.py
@@ -2,12 +2,11 @@ from django.shortcuts import render
 from django.views import generic
 from django.shortcuts import render
 from django.template import loader
-
+from django.http.request import HttpRequest
 from .models import Post
 
-def index(request):
+def index(request: HttpRequest):
     posts = Post.objects.all()
-
     template = loader.get_template('posts/index.html')
     context = {'posts': posts,}
     return render(request, 'posts/index.html', context)
