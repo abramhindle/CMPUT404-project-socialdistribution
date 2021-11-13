4032ad1dba9ab3ddb3d7d5da09a36891f5a890a3
Ben Wang <xiaoyu.w.99@gmail.com>
2021-11-13 12:05:55 -0700

    create new internal api service for fetching all posts

--- server/urls.py
+++ server/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    
+    path('feed/', views.feed.as_view(), name='feed'),
 ]
