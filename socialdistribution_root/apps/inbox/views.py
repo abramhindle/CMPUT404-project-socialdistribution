f054fe66544b049bbce7a61c270b82dffc2d5ab0
Matthew Mullen <mtmullen@ualberta.ca>
2021-11-12 20:54:09 -0700

    done

--- socialdistribution_root/apps/inbox/views.py
+++ socialdistribution_root/apps/inbox/views.py
@@ -1,12 +1,16 @@
+from django.db.models.fields.related import ForeignKey
+from django.db.models.query import RawQuerySet
 from django.http.request import HttpRequest
 from django.shortcuts import render
 from django.views import generic
 from django.template import loader
 from .models import InboxItem
 from django.http.request import HttpRequest
+from apps.core.models import Author
 
 def index(request: HttpRequest):
     inbox = InboxItem.objects.all()
+    currentAuthor=Author.objects.filter(userId=request.user).first()
     template = loader.get_template('inbox/index.html')
-    context={'inboxitems':inbox}
+    context={'inboxitems':inbox,'author_id':currentAuthor.id}
     return render(request,'inbox/index.html',context)    
