9d0b94ecb766d0eb3f817a4859ff3b2680075db4
mrboisvert <michaelboisvert6@gmail.com>
2021-11-15 16:43:45 -0700

    Created Node model

--- server/admin.py
+++ server/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Setting
+from .models import Setting, Node
 
 class SettingAdmin(admin.ModelAdmin):
     list_display = ['allow_user_sign_up']
 
 # Register your models here.
 admin.site.register(Setting, SettingAdmin)
+admin.site.register(Node)
