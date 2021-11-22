bafe8f5cc2ba260d10901a9f277c78ba8e034718
dazhi-z <dazhi@ualberta.ca>
2021-11-21 22:22:56 -0700

    search user

--- frontend/src/pages/SearchResultPage.js
+++ frontend/src/pages/SearchResultPage.js
@@ -2,10 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row, Col, Button, Form, Stack, Alert } from "react-bootstrap";
 import Headers from "../components/Headers";
 import SideBar from "../components/SideBar";
-import searchicon from "../images/search.png";
 import { useDispatch, useSelector } from "react-redux";
-import { LinkContainer } from "react-router-bootstrap";
-import SearchResultItem from "../components/SearchResults";
 import { getPosts } from "../actions/postActions";
 import { getUsers } from "../actions/userActions";
 import Posts from "../components/Posts";
@@ -21,14 +18,7 @@ const SearchResultPage = (props) => {
   const postList = useSelector((state) => state.postList);
   const { post } = postList;
 
-  // get user list
-  const userList = useSelector((state) => state.userList);
-  useEffect(() => {
-    dispatch(getUsers());
-  }, [dispatch]);
-  console.log(userList.userList);
 
-  //console.log(props.match.params.id);
   const searchText = props.match.params.id;
 
   useEffect(() => {
@@ -40,15 +30,20 @@ const SearchResultPage = (props) => {
   const [message, setMessage] = useState("");
   const posts = post ? post.items : [];
 
+
   var searchResultPosts = [];
 
-  for( var i=0;i<posts.length;i++){ 
-    if ( posts[i].title.indexOf(searchText) != -1) {
+  if(searchText){
+    for( var i=0;i<posts.length;i++){ 
       searchResultPosts.push(posts[i]);
     }
+  }else{
+    for( var i=0;i<posts.length;i++){ 
+      if ( posts[i].title.indexOf(searchText) != -1) {
+        searchResultPosts.push(posts[i]);
+      }
+    }
   }
-
-
 
   return (
     <Container className="App fluid min-vh-100 min-vw-100 d-flex flex-column p-0">
@@ -63,10 +58,9 @@ const SearchResultPage = (props) => {
                   Search Results
                 </Alert>    
                 </div>
-                <Alert>hahaha</Alert>
                 {searchResultPosts.map((p) => (
                   <Posts post={p} />
-                ))}
+                ))}                
         </Col>
       </Row>
     </Container>
