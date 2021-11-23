1054bc9e8cd653b29814fbcfa404cbbaa402a348
Student <bondarau@ualberta.ca>
2021-11-23 00:47:20 -0500

    add static path to settings

--- socialdistribution_root/socialdistribution/settings.py
+++ socialdistribution_root/socialdistribution/settings.py
@@ -145,6 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
