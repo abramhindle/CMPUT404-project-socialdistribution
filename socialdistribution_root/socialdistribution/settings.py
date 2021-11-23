f48e53d515117d5e1c15aeddd115948698a2f9ea
Student <bondarau@ualberta.ca>
2021-11-23 01:11:19 -0500

    fix req in settings

--- socialdistribution_root/socialdistribution/settings.py
+++ socialdistribution_root/socialdistribution/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import django_on_heroku
+import django_heroku
 import os
 from pathlib import Path
 
@@ -166,4 +166,4 @@ REST_FRAMEWORK = {
     )
 }
 
-django_on_heroku.settings(locals(), staticfiles=False)
+django_heroku.settings(locals(), staticfiles=False)
