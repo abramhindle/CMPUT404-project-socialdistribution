995934b70cd5c0304057344f1ab2512aef98c015
Jihoon Og <jihoonog@gmail.com>
2021-11-22 17:26:55 -0700

    hot fix

--- backend/serializers.py
+++ backend/serializers.py
@@ -1,4 +1,3 @@
-from typing_extensions import Required
 from rest_framework import serializers
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
