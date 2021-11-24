a792ed3f12895ee309073a12507da2bd56431d30
Matthew Mullen <mtmullen@ualberta.ca>
2021-11-24 09:21:11 -0700

    some changes so models can store and have huge content

--- apps/inbox/models.py
+++ apps/inbox/models.py
@@ -14,4 +14,4 @@ class InboxItem(models.Model):
     author_id = models.ForeignKey(Author, on_delete=models.CASCADE) # guy who wrote the original post
     item_id = models.CharField(max_length=200) # This could be the id of an item that isn't in our databse so can't foreign key
     item_type = models.CharField(max_length=10, choices=ItemTypeEnum.choices)
-    item = models.CharField(max_length=5000) # This is the content of the item
+    item = models.TextField() # This is the content of the item
