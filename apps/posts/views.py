24732f45087f2e6581a8d941a900469a43990aa8
Matthew Mullen <mtmullen@ualberta.ca>
2021-11-25 07:03:28 -0700

    editpost changes

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -11,12 +11,13 @@ from socialdistribution.utils import Utils
 import json
 
 def index(request: HttpRequest):
-    if request.user.is_anonymous:
-        return render(request,'core/index.html')
+    if request.user.is_anonymous or not (request.user.is_authenticated):
+        return render(request,'posts/index.html')
     currentAuthor=Author.objects.filter(userId=request.user).first()
     posts = Post.objects.filter(author=currentAuthor)
     template = loader.get_template('posts/index.html')
-    context = {'posts': posts,}
+    host = request.scheme + "://" + request.get_host()
+    context = {'posts': posts,'host':host}
     return render(request, 'posts/index.html', context)
 
 def makepost(request: HttpRequest):
@@ -26,13 +27,10 @@ def makepost(request: HttpRequest):
     context = {'author' : currentAuthor, 'host' : host}
     return render(request,'posts/makepost.html',context)
 
-def editpost(request: HttpRequest):
+def editpost(request: HttpRequest, post_id: str):
     template= loader.get_template('posts/editpost.html')
     currentAuthor=Author.objects.filter(userId=request.user).first()
-    currentAuthorPosts = Post.objects.filter(author=currentAuthor)
-    currentAuthorPostList = []
-    for post in currentAuthorPosts:
-        currentAuthorPostList.append(post)
+    post = Post.objects.get(pk=post_id)
     host = request.scheme + "://" + request.get_host()
-    context = {'author' : currentAuthor, 'posts': currentAuthorPosts, 'host':host}
+    context = {'author' : currentAuthor, 'post': post, 'host':host}
     return render(request,'posts/editpost.html',context)
