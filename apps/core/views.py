7103a59065e89f9de92b6dd8492c44b46843720f
Patrisha-de-Boon <ttdeboon@gmail.com>
2021-11-26 00:11:18 -0500

    Add sign up

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,19 +1,39 @@
 from django.http.request import HttpRequest
 from django.shortcuts import render
+from django.urls.base import reverse
 from django.views import generic
-from .models import Author, Follow
+from .models import User, Author, Follow
 from apps.posts.models import Post
 from socialdistribution.utils import Utils
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
 
 # Create your views here.
 class IndexView(generic.TemplateView):
     template_name = 'core/index.html'
 
+class RegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("username", )
+
+class SignUpView(generic.CreateView):
+    form_class = RegisterForm
+    template_name = 'registration/signup.html'
+
+    def get_success_url(self):
+        return reverse('login')
+
 def authors(request: HttpRequest):
     if request.user.is_anonymous:
         return render(request,'core/index.html')
     currentAuthor=Author.objects.filter(userId=request.user).first()
-    authors = Author.objects.all()
+    authors = None
+    if (request.user.is_staff):
+        authors = Author.objects.all()
+    else:
+        authors = Author.objects.filter(isApproved=True)
     # Todo: Add authors from other hosts
     host = request.scheme + "://" + request.get_host()
     context = {
