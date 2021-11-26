fcb4fe1ba366e706ca817ffd5852223d061930c7
Student <bondarau@ualberta.ca>
2021-11-25 21:51:13 -0500

    merge Matt's frontend changes

--- apis/posts/views.py
+++ apis/posts/views.py
@@ -36,6 +36,7 @@ class post(GenericAPIView):
         try:
             post = get_object_or_404(self.get_queryset(), pk=self.kwargs["post_id"])
         except: 
+            print("didnt get post")
             raise Http404()
 
         # Check Author permission to edit post
@@ -47,6 +48,7 @@ class post(GenericAPIView):
         try:
             author = get_object_or_404(Author.objects.all(), pk=author_id)
         except: 
+            print("didnt find author")
             raise Http404()
 
         return author
@@ -71,8 +73,8 @@ class post(GenericAPIView):
         if serializer.is_valid():
             serializer.save()
             formatted_data = Utils.formatResponse(query_type="POST on post", data=serializer.data)
-
             return Response(formatted_data)
+            
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     # PUT create a post with that post_id
@@ -80,7 +82,7 @@ class post(GenericAPIView):
         # validate given author_id
         author = self.get_author(author_id)
         host = self.get_host(request)
-
+        print("we got author and host")
         serializer = self.get_serializer(data=request.data, context={'host': host})
         if serializer.is_valid():
             post = Post.objects.create(
@@ -88,8 +90,7 @@ class post(GenericAPIView):
                     author=author,
                     host=host,
                     **serializer.validated_data
-                )
-
+                    )
             # serialize saved post for response
             serializer = self.get_serializer(post, context={'host': host})
             formatted_data = Utils.formatResponse(query_type="PUT on post", data=serializer.data)
@@ -143,8 +144,8 @@ class posts(GenericAPIView):
         # validate given author_id
         author = self.get_author(author_id)
         host = self.get_host(request)
-
-        data = JSONParser().parse(request)
+        #data = JSONParser().parse(request)
+        data = request.data
         serializer = self.get_serializer(data=data)
         if serializer.is_valid():
             post = Post.objects.create(
@@ -158,6 +159,7 @@ class posts(GenericAPIView):
             formatted_data = Utils.formatResponse(query_type="POST on posts", data=serializer.data)
 
             return Response(formatted_data, status=status.HTTP_201_CREATED)
+        print(serializer.errors)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = 'posts'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('makepost',views.makepost,name='makepost'),
+    path('editpost/<str:post_id>',views.editpost, name='editpost'),
     ]
