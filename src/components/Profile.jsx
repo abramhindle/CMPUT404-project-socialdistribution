e3646fcc98c1f0bf14703630b03cde437691ce4a
Ben Wang <xiaoyu.w.99@gmail.com>
2021-11-29 22:13:58 -0700

    update follow button, clean up profile component

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,43 +1,48 @@
-import React from "react";
-import githubMark from "../resources/githubMark/githubMark120px.png"
+import React from 'react';
+import githubMark from '../resources/githubMark/githubMark120px.png';
+import { Button } from '@material-ui/core';
 
 const Profile = ({ author, buttonText, onClick }) => {
+  const renderProfilePicture = (
+    <img src={author.profileImage || '/static/assets/anonProfile.png'} width='200' height='200' alt='profilepic' />
+  );
 
+  const renderId = <p>{author.authorID ? author.authorID : author.id.split('/').at(-1)}</p>;
 
-  console.log(author)
+  const renderHost = author.host ? (
+    <div className='profileHorizontalHeader'>
+      <img src='/static/assets/host.svg' height='27.5em' alt='host mark' />
+      <p>&nbsp;{author.host}</p>
+    </div>
+  ) : (
+    <></>
+  );
+
+  const renderGithub = author.github && (
+    <a href={author.github}>
+      <img src={githubMark} height='25em' alt='github mark' />
+      <div>{author.github}</div>
+    </a>
+  );
   return (
     <>
       {' '}
       {author && (
         <div>
           <div className='profileHeader'>
-            <img
-              src={author.profileImage || '/static/assets/anonProfile.png'}
-              width='200'
-              height='200'
-              alt='profilepic'
-            />
+            {renderProfilePicture}
             <div className='profileHeaderInner'>
               <h1>{author.displayName}</h1>
-              <p>{author.authorID ? author.authorID : author.id.split('/').at(-1)}</p>
-              {author.host ? (
-                <div className='profileHorizontalHeader'>
-                  <img src='/static/assets/host.svg' height='27.5em' alt='host mark' />
-                  <p>&nbsp;{author.host}</p>
-                </div>
-              ) : (
-                <></>
-              )}
-              {author.github && (
-                <a href={author.github}>
-                  <img src={githubMark} height='25em' alt='github mark' />
-                  <div>{author.github}</div>
-                </a>
-              )}
-
-              <div className='profileButton' onClick={onClick}>
+              {renderId}
+              {renderHost}
+              {renderGithub}
+              <Button
+                variant='outlined'
+                className='profileButton'
+                onClick={onClick}
+              >
                 {buttonText}
-              </div>
+              </Button>
             </div>
           </div>
         </div>
