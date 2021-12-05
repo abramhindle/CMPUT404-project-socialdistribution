29866653db6993d771e30617bd939547208d8c52
Student <bondarau@ualberta.ca>
2021-12-05 01:09:48 -0500

    update 404 handler

--- socialdistribution/views.py
+++ socialdistribution/views.py
@@ -4,8 +4,4 @@ from django.template.loader import get_template
 
 def error_404(request, exception):
     context = {}
-    try:
-        body = render(request,'404.html')
-    except Exception as err:
-        print(err)
     return render(request, '404.html', context)
