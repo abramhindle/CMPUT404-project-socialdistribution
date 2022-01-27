515a383ef977cc3ee31aef9585abe6fcb087e4bf
adriancleung <adrian.leung@ualberta.ca>
2022-01-27 16:16:19 -0700

    fix: bind API to heroku defined port

--- src/server/src/app.ts
+++ src/server/src/app.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import path from 'path';
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, '../build')));
 
@@ -10,6 +11,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../build/index.html'));
 });
 
-app.listen(process.env.API_PORT, () => {
-  console.log('Your app is listening on port ' + process.env.API_PORT);
+app.listen(PORT, () => {
+  console.log('Your app is listening on port ' + PORT);
 });
