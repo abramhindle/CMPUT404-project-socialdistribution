be2f3691227daf4c5621467ca1066b57c995dbb1
adriancleung <adrian.leung@ualberta.ca>
2022-02-02 22:39:50 +0000

    feat: testing CI and CD

--- src/server/src/app.ts
+++ src/server/src/app.ts
@@ -5,11 +5,13 @@ import path from 'path';
 const app = express();
 const PORT = process.env.PORT || process.env.API_PORT || 3001;
 
-app.use(express.static(path.join(__dirname, '../build')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build/index.html'));
-});
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build/index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   console.log('Your app is listening on port ' + PORT);
