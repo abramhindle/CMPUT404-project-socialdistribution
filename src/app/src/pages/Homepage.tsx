a059a437ea04a20a374f9ddfc7b8c7fde83b5633
Sutanshu <sutanshu@ualberta.ca>
2022-02-10 17:07:21 +0000

    feat(homepage): navLayout and dev env ease

--- src/app/src/pages/Homepage.tsx
+++ src/app/src/pages/Homepage.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
+import NavBar from '../components/NavBar';
 
+const NavContainer = styled.div`
+  width: 100%;
+  position: absolute;
+  text-align: center;
+  height: 5%;
+  border-bottom: 1px solid;
+`;
+
+const HomepageContainer = styled.div`
+  width: 100%;
+  flex: display;
+  align: center;
+`;
 export default function Homepage() {
-    return (
-        <div>
-            Hello homepage!
-        </div>
-    )
+  return (
+    <HomepageContainer>
+      <NavContainer>
+        <NavBar />
+      </NavContainer>
+      <div>Hello homepage!</div>
+    </HomepageContainer>
+  );
 }
