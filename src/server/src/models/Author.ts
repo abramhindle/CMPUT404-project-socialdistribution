539b106f847d5d9cd0cd2481592e5278e4653678
Kyle McLean <kyle@xlm.li>
2022-02-12 22:24:10 +0000

    Add email and passwordHash as fields on Author model

--- src/server/src/models/Author.ts
+++ src/server/src/models/Author.ts
@@ -4,6 +4,8 @@ import { v4 as uuidv4 } from 'uuid';
 
 class Author extends Model {
   declare id: typeof uuidv4;
+  declare email: string;
+  declare passwordHash: string;
   declare displayName: string;
   declare github: string;
   declare profileImage: string;
@@ -16,6 +18,15 @@ Author.init(
       defaultValue: DataTypes.UUIDV4,
       allowNull: false,
       primaryKey: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    passwordHash: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     displayName: {
       type: DataTypes.STRING,
