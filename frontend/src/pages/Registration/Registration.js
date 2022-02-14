6aadb7ebee6f74a02e741125b2eb9bd76331f32a
JoshuaBillson <jmbillson@outlook.com>
2022-02-14 15:32:08 -0700

    Registration Now Required Password Confirmation

--- frontend/src/pages/Registration/Registration.js
+++ frontend/src/pages/Registration/Registration.js
@@ -14,23 +14,41 @@ import { set } from 'lodash/fp';
 
 export default function RegistrationForm() {
 
+  /* State Hooks For Showing Alert Messages */
   const [openAlert, setOpenAlert] = React.useState({isOpen: false, message: "", severity: "error"})
-
   const showSuccess = _ => setOpenAlert({isOpen: true, message: "You Have Been Successfully Registered!", severity: "success"})
   const showError = msg => setOpenAlert({isOpen: true, message: msg, severity: "error"})
   const handleCloseAlert = () => setOpenAlert(set('isOpen', false, openAlert));
 
+  /* State Hooks For Validating The Password */
+  const [password1, setPassword1] = React.useState("");
+  const [password2, setPassword2] = React.useState("");
+  const [isEqual, setIsEqual] = React.useState(true);
 
+  /* The function validates the password whenever a new character is entered into the text fields */
+  const onPasswordChange = (event) => {
+    if (event.target.name === "password") {
+      setPassword1(event.target.value);
+      setIsEqual(event.target.value === password2);
+    } else {
+      setPassword2(event.target.value);
+      setIsEqual(event.target.value === password1);
+    }
+  }
+
+  /* Callback for handling the form submission process */
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    if (data.get("password") && data.get("displayName")) {
+    if (data.get("password") && data.get("displayName") && isEqual) {
       axios.post("/api/authors/register/", data)
         .then( showSuccess )
         .catch( err => showError(err.response.data.error) );
-    } else {
+    } else if (! data.get("password") || ! data.get("displayName")) {
       showError("Username And Password Required!")
-    }
+    } else if (! isEqual) {
+      showError("Entered Passwords Do Not Match!")
+    }  
   };
 
   return (
@@ -44,12 +62,8 @@ export default function RegistrationForm() {
         <Alert severity={openAlert.severity}>{openAlert.message}</Alert>
       </Snackbar>
       <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', maxWidth: "400px" }} >
-        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign Up
-        </Typography>
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}> <LockOutlinedIcon /> </Avatar>
+        <Typography component="h1" variant="h5"> Sign Up </Typography>
         <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
           <Grid container spacing={2}>
             <Grid item xs={12}>
@@ -66,11 +80,24 @@ export default function RegistrationForm() {
               <TextField
                 required
                 fullWidth
+                onChange={onPasswordChange}
                 name="password"
                 label="Password"
                 type="password"
                 id="password"
-                autoComplete="new-password"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                error={! isEqual}
+                required
+                fullWidth
+                onChange={onPasswordChange}
+                name="password_confirm"
+                label="Confirm Password"
+                type="password"
+                id="password_confirm"
+                helperText={isEqual ? "" : "Passwords Do Not Match!"}
               />
             </Grid>
             <Grid item xs={12}>
@@ -85,9 +112,7 @@ export default function RegistrationForm() {
           <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }} > Sign Up </Button>
           <Grid container justifyContent="center">
             <Grid item>
-              <Link href="/login" variant="body2">
-                Already have an account? Sign in
-              </Link>
+              <Link href="/login" variant="body2"> Already have an account? Sign in </Link>
             </Grid>
           </Grid>
         </Box>
