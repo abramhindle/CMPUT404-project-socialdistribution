3ee9ba0cbf9cbb9a1046f788555e98838fb68e2a
Vyome Agarwal <vyome@ualberta.ca>
2022-02-21 00:19:36 +0000

    layout done for mainpage

--- src/app/src/components/Post.tsx
+++ src/app/src/components/Post.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Button, { ButtonProps } from "@mui/material/Button";
 import { styled as Styled } from "@mui/material/styles";
 import styled from "styled-components";
-import logo from '../logo.svg';
+import logo from "../logo.svg";
 
 interface postItem {
   Name: String;
@@ -12,35 +12,40 @@ interface postItem {
   ProfilePicturePath?: String;
 }
 
+// This is for the whole Post, which includes the profile picure, content, etc
 const PostContainer = styled.div`
-  width: 500px;
+  width: 100%;
   height: 300px;
   display: flex;
 `;
 
+// This is for the details of post: everything except the profile picture
 const PostDetailsContainer = styled.div`
-height: 100%;
-width: 90%;
-display: flex;
-border: 1px solid black;
-flex-direction: column;
-position: relative;
+  height: 100%;
+  width: 90%;
+  display: flex;
+  border: 1px solid black;
+  flex-direction: column;
+  position: relative;
 `;
 
+// This is for the Profile Picture only
 const PostProfilePictureContainer = styled.div`
-height: 100%;
-margin: 20px;
+  height: 100%;
 `;
 
+// This is for the Author name, edit button and delete button, which be at the top
 const TopRowContainer = styled.div`
   display: flex;
   flex-direction: row;
 `;
 
+// This is for the author name
 const NameContainer = styled.div`
   padding: 1%;
 `;
 
+// This is for the Edit and Delete buttons
 const EditDeleteButtonContainer = styled.div`
   padding: 1%;
   display: flex;
@@ -49,11 +54,13 @@ const EditDeleteButtonContainer = styled.div`
   width: 100%;
 `;
 
+// This is for the post content, which can be text and images
 const ContentContainer = styled.div`
   padding: 1%;
   margin-top: 20px;
 `;
 
+// This is for the likes and comments
 const LikesCommentsContainer = styled.div`
   padding: 1%;
   display: flex;
@@ -64,6 +71,7 @@ const LikesCommentsContainer = styled.div`
   left: 0;
 `;
 
+// This is for the likes
 const LikesContainer = styled.div`
   padding: 1%;
   display: flex;
@@ -72,6 +80,7 @@ const LikesContainer = styled.div`
   width: 90px;
 `;
 
+// This is for the comments
 const CommentsContainer = styled.div`
   padding: 1%;
   display: flex;
@@ -84,8 +93,8 @@ const EditButton = Styled(Button)<ButtonProps>(({ theme }) => ({
   color: theme.palette.getContrastText("#e6c9a8"),
   backgroundColor: "white",
   border: "2px solid black",
-  height: "90%",
-  padding: "3%",
+  height: "3%",
+  padding: "1%",
   marginRight: "10px",
   "&:hover": {
     backgroundColor: "#F9F7F5",
@@ -96,8 +105,8 @@ const DeleteButton = Styled(Button)<ButtonProps>(({ theme }) => ({
   color: theme.palette.getContrastText("#e6c9a8"),
   backgroundColor: "white",
   border: "2px solid black",
-  height: "90%",
-  padding: "3%",
+  height: "3%",
+  padding: "1%",
   "&:hover": {
     backgroundColor: "#F9F7F5",
   },
--- src/app/src/pages/Mainpage.tsx
+++ src/app/src/pages/Mainpage.tsx
@@ -1,6 +1,8 @@
 import NavBar from "../components/NavBar";
 import Post from "../components/Post";
 import styled from "styled-components";
+import Button, { ButtonProps } from "@mui/material/Button";
+import { styled as Styled } from "@mui/material/styles";
 
 const items2 = [
   {
@@ -11,28 +13,67 @@ const items2 = [
   },
 ];
 
-const HomeContainer = styled.div`
+// This is for all the stuff in the Main Page
+const MainPageContainer = styled.div`
   display: flex;
   flex-direction: column;
   height: ${window.innerHeight}px;
+  width: ${window.innerWidth}px;
 `;
+
+// This is for the NavBar
 const NavBarContainer = styled.div`
   margin-bottom: 50px;
 `;
 
+// This is for the posts and New Post Button and Github Activity
+const MainPageContentContainer = styled.div`
+  margin: 20px;
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+`;
+
+// This is the NewPost Button
+const NewPostButton = Styled(Button)<ButtonProps>(({ theme }) => ({
+  color: theme.palette.getContrastText("#e6c9a8"),
+  backgroundColor: "white",
+  border: "2px solid black",
+  height: "10%",
+  width: "90%",
+  padding: "1%",
+  "&:hover": {
+    backgroundColor: "#F9F7F5",
+  },
+}));
+
+// This holds the New Post button and Github Stream
+const NewPostGithubActivityContainer = styled.div`
+  width: 10%;
+  display: flex;
+  justify-content: column;
+  margin-right: 50px;
+`;
+
 export default function Mainpage() {
   return (
-    <HomeContainer>
+    <MainPageContainer>
       <NavBarContainer>
         <NavBar items={items2} />
       </NavBarContainer>
 
-      <Post
-        Name={"Seth"}
-        ContentText={"Welcome to my Awesome Webiste :)"}
-        Likes={10}
-        Comments={6}
-      />
-    </HomeContainer>
+      <MainPageContentContainer>
+        <Post
+          Name={"Seth"}
+          ContentText={"Welcome to my Awesome Webiste :)"}
+          Likes={10}
+          Comments={6}
+        />
+
+        <NewPostGithubActivityContainer>
+          <NewPostButton>New Post</NewPostButton>
+        </NewPostGithubActivityContainer>
+      </MainPageContentContainer>
+    </MainPageContainer>
   );
 }
