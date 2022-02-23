4591e6ba4d22f1399c053ef22a5332a48dd49054
Sutanshu <sutanshu@ualberta.ca>
2022-02-22 17:50:10 -0700

    feat(validate):email

--- src/app/src/pages/SignUp.tsx
+++ src/app/src/pages/SignUp.tsx
@@ -8,6 +8,13 @@ interface User {
   username: string;
   password: string;
 }
+const validateEmail = (email: string) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    );
+};
 
 const ColorButton = Styled(Button)<ButtonProps>(({ theme }) => ({
   color: theme.palette.getContrastText('#e6c9a8'),
@@ -36,6 +43,9 @@ export default function SignUp() {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [validEmail, setValidEmail] = useState(true);
+  const [validPassword, setValidPassword] = useState(true);
+  const [validUsername, setValidUsername] = useState(true);
 
   const handleClick = () => {
     const user: User = {
@@ -43,8 +53,14 @@ export default function SignUp() {
       email: email,
       password: password,
     };
-    console.log(username, password);
-    localStorage.setItem('user', JSON.stringify(user));
+
+    validateEmail(email) ? setValidEmail(true) : setValidEmail(false);
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    validEmail && validPassword && validUsername
+      ? localStorage.setItem('user', JSON.stringify(user))
+      : null;
+
     //Discuss this^
   };
   const onChangeUsername = (event: any) => {
@@ -58,28 +74,60 @@ export default function SignUp() {
   };
   return (
     <>
-      <TextFieldCustom
-        id="standard-basic"
-        color="primary"
-        label="Username"
-        variant="standard"
-        onChange={onChangeUsername}
-      />
-      <TextFieldCustom
-        id="standard-basic"
-        color="primary"
-        label="Email"
-        variant="standard"
-        onChange={onChangeEmail}
-      />
-      <TextFieldCustom
-        id="standard-basic"
-        label="Password"
-        variant="standard"
-        type="password"
-        autoComplete="current-password"
-        onChange={onChangePassword}
-      />
+      {validUsername ? (
+        <TextFieldCustom
+          id="standard-basic"
+          color="primary"
+          label="Username"
+          variant="standard"
+          onChange={onChangeUsername}
+        />
+      ) : (
+        <TextFieldCustom
+          id="standard-error"
+          color="primary"
+          label="Username"
+          variant="standard"
+          onChange={onChangeUsername}
+        />
+      )}
+      {validEmail ? (
+        <TextFieldCustom
+          id="standard-basic"
+          defaultValue={email}
+          color="primary"
+          label="Email"
+          variant="standard"
+          onChange={onChangeEmail}
+        />
+      ) : (
+        <TextFieldCustom
+          error
+          id="outlined-error"
+          defaultValue={email}
+          label="Email"
+          onChange={onChangeEmail}
+        />
+      )}
+      {validPassword ? (
+        <TextFieldCustom
+          id="standard-basic"
+          label="Password"
+          variant="standard"
+          type="password"
+          autoComplete="current-password"
+          onChange={onChangePassword}
+        />
+      ) : (
+        <TextFieldCustom
+          id="standard-error"
+          label="Password"
+          variant="standard"
+          type="password"
+          autoComplete="current-password"
+          onChange={onChangePassword}
+        />
+      )}
       <ColorButton variant="contained" onClick={handleClick}>
         Sign up
       </ColorButton>
