3291bb06a8a0cdf46e82d099e6112e348833bbdd
Miller MA <this.is.millerma@gmail.com>
2022-02-25 11:07:07 -0700

    local version control

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -26,6 +26,7 @@ class Post(models.Model):
     description = models.CharField(max_length=500, blank=True)
     contentType = models.CharField(max_length=350, choices=ContentType.choices)
     content = models.TextField()
+    #target file for the image
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     categories = ArrayField(models.CharField(max_length=100))
     published = models.DateTimeField(default=now, editable=False)
