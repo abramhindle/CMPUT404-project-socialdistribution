b5dd159e9cfe0e90fb22241fadb0182f223dbb92
Armianto Sumitro <armiantos@gmail.com>
2022-03-01 22:41:12 -0700

    Define comment model

--- posts/models.py
+++ posts/models.py
@@ -9,13 +9,15 @@ class Category(models.Model):
     category = models.CharField(max_length=STR_MAX_LENGTH)
 
 
+class ContentType(models.TextChoices):
+    MARKDOWN = 'text/markdown', _('Commonmark')
+    PLAIN = 'text/plain', _('Plaintext')
+    BASE64 = 'application/base64', _('Base64Encoded')
+    PNG = 'image/png;base64', _('PNG')
+    JPG = 'image/jpeg;base64', _('JPEG')
+
+
 class Post(models.Model):
-    class ContentType(models.TextChoices):
-        MARKDOWN = 'text/markdown', _('Commonmark')
-        PLAIN = 'text/plain', _('Plaintext')
-        BASE64 = 'application/base64', _('Base64Encoded')
-        PNG = 'image/png;base64', _('PNG')
-        JPG = 'image/jpeg;base64', _('JPEG')
 
     class Visibility(models.TextChoices):
         PUBLIC = "PUBLIC"
@@ -30,3 +32,11 @@ class Post(models.Model):
     date_published = models.DateTimeField(auto_now_add=True)
     unlisted = models.BooleanField()
     categories = models.ManyToManyField(Category, blank=True)
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    comment = models.TextField()
+    content_type = models.CharField(max_length=18, default=ContentType.PLAIN, choices=ContentType.choices)
+    date_published = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
