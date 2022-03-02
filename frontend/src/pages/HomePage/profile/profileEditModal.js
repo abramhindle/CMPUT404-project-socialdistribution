7e36aedd1d32453aebb51a00aba457e1cfb06490
Philip Wang <philipwang313@gmail.com>
2022-03-02 16:00:56 -0700

    Modal fix

--- frontend/src/pages/HomePage/profile/profileEditModal.js
+++ frontend/src/pages/HomePage/profile/profileEditModal.js
@@ -41,43 +41,86 @@ const SmallAvatar = styled(Avatar)(({ theme }) => ({
     border: `2px solid ${theme.palette.background.paper}`,
 }));
 
+
 export default function ProfileEditModal(props) {
+    const displayName = useSelector(state => state.profile.displayName);
+    const userID = useSelector(state => state.profile.userID);
     const profileImage = useSelector(state => state.profile.profileImage);
     const github = useSelector(state => state.profile.github);
     const dispatch = useDispatch();
     const handleSubmit = (event) => {
         event.preventDefault();
         const data = new FormData(event.currentTarget);
+        console.log('data: ' + data.get('github'));
+
 
     };
+
+    const [image, _setImage] = React.useState(null);
+    const inputFileRef = React.createRef(null);
+
+    const cleanup = () => {
+        URL.revokeObjectURL(image);
+        inputFileRef.current.value = null;
+    };
+
+    const setImage = (newImage) => {
+        if (image) {
+            cleanup();
+        }
+        _setImage(newImage);
+    };
+
+    const handleOnChange = (event) => {
+        const newImage = event.target?.files?.[0];
+
+        if (newImage) {
+            setImage(URL.createObjectURL(newImage));
+        }
+    };
+
+    const cancelChanges = () => {
+        cleanup();
+        _setImage(profileImage)
+        props.onClose();
+    }
 
     return (
         <Modal
             open={props.isOpen}
-            onClose={props.onClose}
+            onClose={cancelChanges}
             aria-labelledby="modal-profile-edit"
             aria-describedby="modal-profile-editing"
         >
-            <Box sx={style.box}>
+            <Box sx={style.box} component="form" noValidate onSubmit={handleSubmit}>
                 <Typography sx={{ fontWeight: 'bold', marginBottom: '2rem' }} id="modal-modal-title" variant="h4" component="h2">
                     Edit Profile
                 </Typography>
 
                 <Grid container spacing={0} justifyContent="center" direction='row' alignItems='center'>
-                    <Grid item xs={3} onClick={() => console.log(11)}><Badge
-                        overlap="circular"
-                        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-                        badgeContent={
-                            // <SmallAvatar alt="d" src={AddIcon} />
-                            <AddCircleSharpIcon sx={style.addIcon} />
-                        }
-                    >
-                        <Avatar alt="Travis Howard" src={profileImage} sx={style.avatar} />
-                    </Badge></Grid>
+                    <Grid item xs={3} component="label">
+                        <input
+                            ref={inputFileRef}
+                            accept='image/*'
+                            hidden
+                            type='file'
+                            id='profile-image-upload'
+                            onChange={handleOnChange}
+                        />
+                        <Badge
+                            overlap="circular"
+                            anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                            badgeContent={
+                                // <SmallAvatar alt="d" src={AddIcon} />
+                                <AddCircleSharpIcon sx={style.addIcon} />
+                            }
+                        >
+                            <Avatar alt={displayName} src={image ? image : profileImage} sx={style.avatar} />
+                        </Badge></Grid>
 
-                    <Grid item xs={9} component="form" noValidate onSubmit={handleSubmit}>
+                    <Grid item xs={9}>
                         <TextField
-                            id="outlined"
+                            id='github'
                             name="github"
                             label="Github Link"
                             defaultValue={github}
@@ -88,8 +131,8 @@ export default function ProfileEditModal(props) {
 
 
                 <Stack spacing={1} sx={style.buttonContainer}>
-                    <Button variant='contained'>Save</Button>
-                    <Button variant='outlined'>Cancel</Button>
+                    <Button variant='contained' type='submit'>Save</Button>
+                    <Button variant='outlined' onClick={cancelChanges}>Cancel</Button>
                 </Stack>
 
             </Box >
