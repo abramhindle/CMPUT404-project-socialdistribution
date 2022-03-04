2bc8e98c9fe3a04f861ea223250daafd073ceda0
Nasif Hossain <34114501+nasif92@users.noreply.github.com>
2022-03-03 22:45:03 -0700

    Edit Profile (#72)
    * initial work on the view
    
    * edit profile basics done
    
    * removed unnecessary imports and files

--- auth_provider/views.py
+++ auth_provider/views.py
@@ -1,8 +1,8 @@
 from django.urls import reverse_lazy
 from django.contrib.auth import get_user_model
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.detail import DetailView
-from .forms import SignUpForm
+from .forms import SignUpForm, EditProfileForm
 
 
 class SignUpView(CreateView):
@@ -12,8 +12,17 @@ class SignUpView(CreateView):
 
 
 class ProfileView(DetailView):
-    model = get_user_model
+    model = get_user_model()
     template_name = 'profile/user_profile.html'
+
+    def get_object(self):
+        return self.request.user
+
+
+class EditProfileView(UpdateView):
+    form_class = EditProfileForm
+    model = get_user_model()
+    template_name = 'profile/edit_profile.html'
 
     def get_object(self):
         return self.request.user
