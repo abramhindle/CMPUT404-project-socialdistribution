b2cf0ad16d8d7fe1bb8d7954907ebce4d94a9444
Nasif Hossain <34114501+nasif92@users.noreply.github.com>
2022-03-03 20:17:46 -0700

    Add user profile page (#70)
    * did signup UI, some stuff to finish on login still
    
    * finished the forms
    
    * just creating this branch for now for frontend work on friend requests check
    
    * just some initial cosmetic changes
    
    * just merged the stuff from master
    
    * just made everything more modular by moving most of the stuff to styles.css
    
    * just putting it here now
    
    * pulling
    
    * just made some things better
    
    * Revert h1 to title in head
    
    * Improve app bar
    
    * Highlight links
    
    * Format html files
    
    * profile page
    
    * Make the social distribution text in header bold
    
    * Set header links to black
    
    * Make header links turn blue on hover
    
    * Clean up
    
    * Move user profile page to auth_provider
    
    * Require login to view stream
    
    * Fix unused/redundant css
    
    Co-authored-by: Armianto Sumitro <armiantos@gmail.com>

--- auth_provider/urls.py
+++ auth_provider/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView
 
-from .views import SignUpView
+from .views import SignUpView, ProfileView
 
 app_name = 'auth_provider'
 urlpatterns = [
-    path('signup', SignUpView.as_view(), name='signup'),
-    path('login', LoginView.as_view(template_name='auth/login.html'), name='login'),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(template_name='auth/login.html'), name='login'),
+    path('profile/', ProfileView.as_view(), name='user_profile'),
 ]
--- auth_provider/views.py
+++ auth_provider/views.py
@@ -1,5 +1,7 @@
 from django.urls import reverse_lazy
+from django.contrib.auth import get_user_model
 from django.views.generic.edit import CreateView
+from django.views.generic.detail import DetailView
 from .forms import SignUpForm
 
 
@@ -7,3 +9,11 @@ class SignUpView(CreateView):
     template_name = 'auth/signup.html'
     form_class = SignUpForm
     success_url = reverse_lazy('auth_provider:login')
+
+
+class ProfileView(DetailView):
+    model = get_user_model
+    template_name = 'profile/user_profile.html'
+
+    def get_object(self):
+        return self.request.user
