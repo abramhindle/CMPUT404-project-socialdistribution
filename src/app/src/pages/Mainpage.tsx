1a8de901db26cb128f7a2b8645b86c3216e72767
Sutanshu <sutanshu@ualberta.ca>
2022-03-06 16:54:10 -0700

    feat(create): posts

--- src/app/src/pages/Mainpage.tsx
+++ src/app/src/pages/Mainpage.tsx
@@ -1,18 +1,24 @@
-import NavBar from "../components/NavBar";
-import UserPost from "../components/UserPost";
-import styled from "styled-components";
+import NavBar from '../components/NavBar';
+import UserPost from '../components/UserPost';
+import styled from 'styled-components';
 // import Button, { ButtonProps } from "@mui/material/Button";
 // import { styled as Styled } from "@mui/material/styles";
-import Author from "../api/models/Author";
-import Post from "../api/models/Post";
-import api from "../api/api";
+import Author from '../api/models/Author';
+import Post from '../api/models/Post';
+import api from '../api/api';
+import Add from '../components/Add';
 import { useState, useEffect } from 'react';
+import Backdrop from '@mui/material/Backdrop';
+import { CloseRounded } from '@mui/icons-material';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
 
 const items2 = [
   {
-    Text: 'Hello',
+    Text: 'Logout',
     handleClick: () => {
-      console.log(1);
+      localStorage.removeItem('token');
+      window?.location?.reload();
     },
   },
 ];
@@ -64,34 +70,79 @@ interface Props {
 
 export default function Mainpage({ currentUser }: Props) {
   // For now, mainpage just shows your own posts
-  const [posts,setPosts]= useState<Post[]|undefined>(undefined)
-  
+  const [posts, setPosts] = useState<Post[] | undefined>(undefined);
+  const [open, setOpen] = useState(false);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleToggle = () => {
+    setOpen(!open);
+  };
+
   useEffect(() => {
     api.authors
-    .withId(""+currentUser?.id)
-    .posts
-    .list(1,5)
-    .then((data)=>setPosts(data))
-    .catch((error) => {console.log(error)})
-    }, [currentUser?.id,posts])
+      .withId('' + currentUser?.id)
+      .posts.list(1, 5)
+      .then((data) => {
+        setPosts(data);
+      })
+      .catch((error) => {
+        console.log('hmm?', error);
+      });
+  }, [currentUser?.id, posts]);
 
   return (
     <MainPageContainer>
       <NavBarContainer>
         <NavBar items={items2} />
       </NavBarContainer>
-
-      <MainPageContentContainer>
-        {posts?.map((post) => (
-          <UserPost
-              Name={""+currentUser?.displayName}
+      <Fab
+        color="primary"
+        aria-label="check"
+        sx={{ color: 'black', background: '#46ECA6', '&:hover': { background: '#18E78F' } }}
+      >
+        <AddIcon onClick={handleToggle} />
+      </Fab>
+      {open ? (
+        <Backdrop
+          sx={{
+            color: '#fff',
+            zIndex: (theme) => theme.zIndex.drawer + 1,
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+          open={open}
+        >
+          <CloseRounded
+            onClick={handleClose}
+            sx={{
+              '&:hover': {
+                cursor: 'pointer',
+              },
+              marginBottom: '10px',
+              borderRadius: '100%',
+              border: '1px solid white',
+            }}
+          />
+          <Add currentUser={currentUser} />
+        </Backdrop>
+      ) : (
+        <MainPageContentContainer>
+          {posts?.map((post) => (
+            <UserPost
+              Name={'' + currentUser?.displayName}
               ContentText={post.content}
               Likes={10}
               Comments={6}
               key={post.id}
             />
           ))}
-      </MainPageContentContainer>
+        </MainPageContentContainer>
+      )}
     </MainPageContainer>
   );
 }
