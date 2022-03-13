e000ba495b3bdc6a72074af0dd3cebe946a13064
JoshuaBillson <jmbillson@outlook.com>
2022-03-13 01:13:51 -0700

    Static Files

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -21,7 +21,7 @@ const persistedReducer = persistReducer(persistConfig, reducers);
 
 const store = configureStore({
     reducer: persistedReducer,
-    devTools: process.env.NODE_ENV !== 'production',
+    devTools: false,
     middleware: [thunk],
 })
 
