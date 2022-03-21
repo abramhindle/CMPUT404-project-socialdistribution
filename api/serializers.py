3243b391f4dd0baed381f073ef8fb66b24ee71de
armiantos <43416725+armiantos@users.noreply.github.com>
2022-03-21 14:49:01 -0600

    Add GET post endpoint (#98)
    * Install drf-nested-routers
    
    * Create post serializer
    
    * Add tests for posts
    
    * Link serializer to viewset
    
    * Register posts endpoint
    
    * Add missing fields
    
    * Add post tests
    
    * Add test to validate it doesn't contain posts by other authors
    
    * Add missing url field
    
    * Fix format errors

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,8 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework_nested.serializers import NestedHyperlinkedModelSerializer
+
+from posts.models import Post
 
 
 class AuthorSerializer(serializers.HyperlinkedModelSerializer):
@@ -13,4 +16,23 @@ class AuthorSerializer(serializers.HyperlinkedModelSerializer):
         representation['displayName'] = instance.get_full_name()
         representation['github'] = instance.github_url
         representation['profileImage'] = instance.profile_image_url
+        return representation
+
+
+class PostSerializer(NestedHyperlinkedModelSerializer):
+    parent_lookup_kwargs = {
+        'author_pk': 'author__pk',
+    }
+    author = AuthorSerializer(many=False, read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'description', 'content', 'author', 'visibility', 'unlisted', 'url']
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['type'] = 'post'
+        representation['contentType'] = instance.content_type
+        representation['published'] = instance.date_published
+        representation['categories'] = [category.category for category in instance.categories.all()]
         return representation
--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,15 @@
 from django.urls import include, path
 from rest_framework import routers
-from api.views import AuthorViewSet
+from api.views import AuthorViewSet, PostViewSet
+from rest_framework_nested import routers
 
 router = routers.DefaultRouter()
 router.register(r'authors', AuthorViewSet)
 
+author_router = routers.NestedDefaultRouter(router, r'authors', lookup='author')
+author_router.register(r'posts', PostViewSet, basename='post')
+
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(author_router.urls)),
 ]
