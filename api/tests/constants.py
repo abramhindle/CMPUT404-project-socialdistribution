69f1708f71af81e56e1c640f6f470b83a35119f9
Reilly Keele <54957139+reillykeele@users.noreply.github.com>
2022-03-21 15:51:16 -0600

    Add Image REST Endpoint (#93)
    * Implement skeleton /authors/{author_id}/posts/
    
    * Add images endpoint
    
    * Update requirements.txt
    
    * change to httpresponse
    
    * Send src uri type thing
    
    * Update requirements.txt
    
    * autopep8
    
    * Add Image API Tests
    
    * Update tests.py
    
    * Update constants.py
    
    * Update tests.py
    
    * Fix Test Run
    
    * Update views.py
    
    * Consolidate api/tests
    
    * Update test_api.py
    
    * Update requirements.txt
    
    * Update views.py
    
    * Remove unused code
    
    * Update serializers.py

---
+++ api/tests/constants.py
@@ -0,0 +1,45 @@
+from django.core.files.uploadedfile import SimpleUploadedFile
+
+from posts.models import Post, ContentType
+
+
+def get_test_image_jpeg():
+    jpeg = SimpleUploadedFile('img.jpeg', '', content_type='image/jpeg')
+    return jpeg
+
+# by dsalaj on Stack Overflow at https://stackoverflow.com/a/42502775
+
+
+def get_test_image_png():
+    valid_png_hex = ['\x89', 'P', 'N', 'G', '\r', '\n', '\x1a', '\n', '\x00',
+                     '\x00', '\x00', '\r', 'I', 'H', 'D', 'R', '\x00',
+                     '\x00', '\x00', '\x01', '\x00', '\x00', '\x00', '\x01',
+                     '\x08', '\x02', '\x00', '\x00', '\x00', '\x90',
+                     'w', 'S', '\xde', '\x00', '\x00', '\x00', '\x06', 'b', 'K',
+                     'G', 'D', '\x00', '\x00', '\x00', '\x00',
+                     '\x00', '\x00', '\xf9', 'C', '\xbb', '\x7f', '\x00', '\x00',
+                     '\x00', '\t', 'p', 'H', 'Y', 's', '\x00',
+                     '\x00', '\x0e', '\xc3', '\x00', '\x00', '\x0e', '\xc3',
+                     '\x01', '\xc7', 'o', '\xa8', 'd', '\x00', '\x00',
+                     '\x00', '\x07', 't', 'I', 'M', 'E', '\x07', '\xe0', '\x05',
+                     '\r', '\x08', '%', '/', '\xad', '+', 'Z',
+                     '\x89', '\x00', '\x00', '\x00', '\x0c', 'I', 'D', 'A', 'T',
+                     '\x08', '\xd7', 'c', '\xf8', '\xff', '\xff',
+                     '?', '\x00', '\x05', '\xfe', '\x02', '\xfe', '\xdc', '\xcc',
+                     'Y', '\xe7', '\x00', '\x00', '\x00', '\x00',
+                     'I', 'E', 'N', 'D', '\xae', 'B', '`', '\x82']
+    valid_png_bin = bytes("".join(valid_png_hex), "utf-8")
+    png = SimpleUploadedFile("tiny.png", valid_png_bin)
+    return png
+
+
+POST_IMG_DATA = {
+    'title': 'Test Image',
+    'description': 'This post is an image :P',
+    'content_type': ContentType.PNG,
+    'content': 'No',
+    'img_content': get_test_image_png(),
+    'categories': 'test',
+    'visibility': Post.Visibility.PUBLIC,
+    'unlisted': False,
+}
--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -1,9 +1,12 @@
 import json
 from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 
-from posts.models import Post
+from posts.models import Post, ContentType
+
 from posts.tests.constants import POST_DATA
+from .constants import POST_IMG_DATA
 
 TEST_USERNAME = 'bob'
 TEST_PASSWORD = 'password'
@@ -106,3 +109,47 @@ class PostTests(TestCase):
         self.assertIn('published', post)
         self.assertIn('url', post)
         self.assertIn('categories', post)
+
+
+class ImageTests(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+        self.author = get_user_model().objects.create_user(username='bob', password='password')
+
+        self.post = Post.objects.create(
+            title=POST_DATA['title'],
+            description=POST_DATA['description'],
+            content_type=POST_DATA['content_type'],
+            content=POST_DATA['content'],
+            author_id=self.author.id,
+            unlisted=POST_DATA['unlisted'])
+        self.post.full_clean()
+        self.post.save()
+
+        self.img_post = Post.objects.create(
+            title=POST_IMG_DATA['title'],
+            description=POST_IMG_DATA['description'],
+            content_type=POST_IMG_DATA['content_type'],
+            content=POST_IMG_DATA['content'],
+            img_content=POST_IMG_DATA['img_content'],
+            author_id=self.author.id,
+            unlisted=POST_IMG_DATA['unlisted'])
+        self.img_post.full_clean()
+        self.img_post.save()
+
+        return
+
+    def test_image(self):
+        self.client.login(username='bob', password='password')
+        res2 = self.client.get(f'/api/v1/authors/{self.author.id}/posts/{self.img_post.id}/image/')
+        self.assertEqual(res2.status_code, 200)
+        self.assertEqual(res2.headers['Content-Type'], ContentType.PNG)
+
+    def test_not_image_404(self):
+        self.client.login(username='bob', password='password')
+        res = self.client.get(f'/api/v1/authors/{self.author.id}/posts/{self.post.id}/image/')
+        self.assertEqual(res.status_code, 404)
+
+    def test_image_require_login(self):
+        res = self.client.get(f'/api/v1/authors/{self.author.id}/posts/{self.img_post.id}/image/')
+        self.assertEqual(res.status_code, 403)
