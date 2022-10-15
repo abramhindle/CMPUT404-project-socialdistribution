325f9f211da57ea1612a0040539fbf5edf29cbca
Mark McGoey <mmcgoey@cs.ualberta.ca>
2022-10-14 23:23:13 -0600

    worked on tests and migrations

--- backend/webserver/models.py
+++ backend/webserver/models.py
@@ -1,18 +1,18 @@
 from unittest.util import _MAX_LENGTH
 from django.db import models
 
-class Authors(models.Model):
+class Author(models.Model):
     display_name = models.CharField(max_length=200)
-    pimage = models.ImageField()
+    profile_image = models.CharField(max_length=250)
     github_handle = models.CharField(max_length=200)
 
-class FollowRequests(models.Model):
-    sender_id =  models.ForeignKey(Authors,related_name='sender',on_delete=models.CASCADE)
-    receiver_id =  models.ForeignKey(Authors,related_name='receiver',on_delete=models.CASCADE) 
+class FollowRequest(models.Model):
+    sender =  models.ForeignKey(Author,related_name='sender',on_delete=models.CASCADE)
+    receiver =  models.ForeignKey(Author,related_name='receiver',on_delete=models.CASCADE) 
 
 
-class Follows(models.Model):
-    follower = models.ForeignKey(Authors,related_name='follower',on_delete=models.CASCADE)
-    followee = models.ForeignKey(Authors,related_name='followee',on_delete=models.CASCADE)
+class Follow(models.Model):
+    follower = models.ForeignKey(Author,related_name='follower',on_delete=models.CASCADE)
+    followee = models.ForeignKey(Author,related_name='followee',on_delete=models.CASCADE)
 
 # Create your models here.
