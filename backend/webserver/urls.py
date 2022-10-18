28de8f1d0ae7381d0b01cbbe587c243654b66da1
Zarif Mahfuz <mahfuz.zarif@gmail.com>
2022-10-17 22:28:51 -0600

    Implement author registration and login routes

--- backend/webserver/urls.py
+++ backend/webserver/urls.py
@@ -3,11 +3,17 @@ from . import views
 from .views import(
     AuthorDetailView,
     AuthorsView,
+    AuthorRegistrationView,
+    LoginView,
+    LogoutView
 )
 
 # first argument, endpoint, second argument is the view that calling the url will send the request to
 
 urlpatterns = [
     path('authors/', AuthorsView.as_view()),
-    path('authors/<int:pk>/', AuthorDetailView.as_view(), name='author-detail')
+    path('authors/<int:pk>/', AuthorDetailView.as_view(), name='author-detail'),
+    path('register/', AuthorRegistrationView.as_view()),
+    path('login/', LoginView.as_view()),
+    path('logout/', LogoutView.as_view()),
 ]
