048bcfb85df3864983af5376eb2103c455450942
Zarif Mahfuz <mahfuz.zarif@gmail.com>
2022-10-20 16:47:09 -0600

    Update webserver namespace

--- backend/social_distribution/urls.py
+++ backend/social_distribution/urls.py
@@ -20,6 +20,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index.html')),
-    path('', include('webserver.urls')),
+    path('api/', include('webserver.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
