046676420f085b9382673945cd065d5ac33cf3ad
Timothee Legros <timothee@ualberta.ca>
2022-10-22 17:30:07 -0600

    create-post styled

--- frontend/src/pages/createPost/CreatePost.template.ts
+++ frontend/src/pages/createPost/CreatePost.template.ts
@@ -3,5 +3,42 @@ import { CreatePost } from './CreatePost';
 
 
 export const CreatePostPageTemplate = html<CreatePost>`
-    <div>Create Post!</div>
+    <div class="create-post-container">
+        <div class="create-post-banner">
+            <h1 class="create-post-text">Create A Post</h1>
+        </div>
+        <div class="create-post-container1">
+            <input
+                    type="text"
+                    required=""
+                    autofocus=""
+                    placeholder="Title"
+                    class="create-post-textinput input"
+            />
+            <textarea
+                    placeholder="Content"
+                    class="create-post-textarea textarea"
+            ></textarea>
+            <button class="create-post-button button">
+            <span class="create-post-text1">
+              <span class="create-post-text2">Upload Image</span>
+              <br/>
+            </span>
+            </button>
+            <div class="create-post-container2">
+                <span class="create-post-text4">Visibility:</span>
+                <select class="create-post-select">
+                    <option value="Public">Public</option>
+                    <option value="Private">Private</option>
+                    <option value="Friends Only">Friends Only</option>
+                </select>
+            </div>
+            <button class="create-post-button1 button">
+            <span class="create-post-text5">
+              <span class="create-post-text6">Create</span>
+              <br/>
+            </span>
+            </button>
+        </div>
+    </div>
 `;
--- frontend/src/pages/createPost/index.ts
+++ frontend/src/pages/createPost/index.ts
@@ -2,7 +2,7 @@ import {provideFASTDesignSystem} from "@microsoft/fast-components";
 import {ComponentEntry, defineComponent} from "../AppRegistry";
 
 import {CreatePost} from "./CreatePost";
-import {CreatePostPageStyles as styles} from "./CreatePost.styles";
+import {CreatePostPageStyles as styles} from "./styles/CreatePost.styles";
 import {CreatePostPageTemplate as template} from "./CreatePost.template";
 
 export const createPostPage = {
