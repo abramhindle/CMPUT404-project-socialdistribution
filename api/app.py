2e33bc08a740ce753660bfe3b6c3926272e3a77a
Mattwmaster58 <mattwmaster58@gmail.com>
2023-02-08 22:56:15 -0700

    ft: production serving + developement reverse proxy

--- api/app.py
+++ api/app.py
@@ -3,12 +3,12 @@ from flask import Flask
 from api.admin import admin_bp
 from api.user import user_bp
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../build", static_url_path="/")
 app.register_blueprint(user_bp)
 app.register_blueprint(admin_bp)
 
 
-@app.route("/")
+@app.route("/hello_world")
 def hello_world():
     return {"message": "Hello world"}
 
