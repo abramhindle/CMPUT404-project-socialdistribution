72a23a266e7c262e2897be20d5ec3398389ec6ce
Mattwmaster58 <mattwmaster58@gmail.com>
2023-02-08 23:37:54 -0700

    ft: frontend serving with flask

--- api/app.py
+++ api/app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 from api.admin import admin_bp
 from api.user import user_bp
 
-app = Flask(__name__, static_folder="../build", static_url_path="/")
-app.register_blueprint(user_bp)
-app.register_blueprint(admin_bp)
+app = Flask(__name__, static_folder="../frontend/build", static_url_path="/")
+app.register_blueprint(user_bp, url_prefix="/authors")
+app.register_blueprint(admin_bp, url_prefix="/admin")
+
+
+@app.route("/")
+def home_redirect():
+    return redirect("/index.html", code=302)
 
 
 @app.route("/hello_world")
