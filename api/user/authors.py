54c78eef98554d5e2c53b7a05b30e18dd4f12843
Mattwmaster58 <mattwmaster58@gmail.com>
2023-02-08 22:18:59 -0700

    ft: use blueprint url prefixes

--- api/user/authors.py
+++ api/user/authors.py
@@ -1,15 +1,17 @@
-from api.utils import get_pagination_params
 from flask import Blueprint
 
+from api.utils import get_pagination_params
+
+# note: this blueprint is usually mounted under /authors URL prefix
 authors_bp = Blueprint("authors", __name__)
 
 
-@authors_bp.route("/authors", methods=['GET'])
+@authors_bp.route("/", methods=["GET"])
 def get_authors():
     pagination = get_pagination_params()
     return {"hello": 1}
 
 
-@authors_bp.route('/authors/<string:author_id>', methods=['GET'])
+@authors_bp.route("/<string:author_id>", methods=["GET"])
 def get_single_author(author_id: str):
     pass
