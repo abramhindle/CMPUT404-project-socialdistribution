cc74a833d7bd3356420da50db4fa1b8dc2f40774
Mattwmaster58 <mattwmaster58@gmail.com>
2023-02-09 00:31:10 -0700

    fix: try path relative to something else

--- api/app.py
+++ api/app.py
@@ -6,7 +6,7 @@ from api.user import user_bp
 # note: Heroku will run things from the working directory as the root of this repo. Therefore, this path MUST
 # be relative to the root of the repo, NOT to this file. You will likely need to specify the working directory
 # as the root of this repo # when you run this file in your IDE
-app = Flask(__name__, static_folder="./frontend/build", static_url_path="/")
+app = Flask(__name__, static_folder="../frontend/build", static_url_path="/")
 app.register_blueprint(user_bp, url_prefix="/authors")
 app.register_blueprint(admin_bp, url_prefix="/admin")
 
