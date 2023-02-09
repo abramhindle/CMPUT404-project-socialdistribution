b45784ebd398596c011626568eac33728c3c24d4
Mattwmaster58 <mattwmaster58@gmail.com>
2023-02-08 22:06:24 -0700

    fix: don't use relative imports when not applicable

--- api/app.py
+++ api/app.py
@@ -1,15 +1,17 @@
 from flask import Flask
-from .user import user_bp
-from .admin import admin_bp
+
+from api.admin import admin_bp
+from api.user import user_bp
 
 app = Flask(__name__)
 app.register_blueprint(user_bp)
 app.register_blueprint(admin_bp)
+
 
 @app.route("/")
 def hello_world():
     return {"message": "Hello world"}
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
