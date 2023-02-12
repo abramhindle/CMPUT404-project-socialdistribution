215cd82e85d0968716d2294c1ab4b26502b04282
Junesung Lee <junesoung412@gmail.com>
2023-02-12 01:35:10 -0700

    Using djongo to use mongodb as database

--- socialdistribution/socialdistribution/settings.py
+++ socialdistribution/socialdistribution/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,8 @@ WSGI_APPLICATION = 'socialdistribution.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'socialdistribution',
     }
 }
 
