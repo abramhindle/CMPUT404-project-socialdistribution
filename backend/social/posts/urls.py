a5130fb8b428c978bb4bcad0ef347feaf3c6cf0a
Leen-Alzebdeh <root@DESKTOP-6SG4VQD>
2023-02-13 16:45:16 -0700

    Add post views

---
+++ backend/social/posts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'posts'
+urlpatterns = [
+  path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('post/', views.post, name='post'),
+]
