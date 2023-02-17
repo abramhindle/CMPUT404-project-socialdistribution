19d5ed78f92ae3bc516b7d41be569b9f46ab4f1a
Hari Bheesetti <77315050+hbheesetti@users.noreply.github.com>
2023-02-17 01:29:53 -0700

    new sign in

--- Frontend/socially_distributed/src/App.js
+++ Frontend/socially_distributed/src/App.js
@@ -1,12 +1,13 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import SIGN_IN from "./Components/sign_in";
 
+
 function App() {
 	return (
 		<div className="App">
 			<BrowserRouter>
 				<Routes>
-					<Route path="/signin" element={ <SIGN_IN/> } />
+					<Route path="/login" element={ <SIGN_IN/> } />
 				</Routes>
 			</BrowserRouter>
 		</div>
--- Frontend/socially_distributed/src/Components/sign_in.js
+++ Frontend/socially_distributed/src/Components/sign_in.js
@@ -1,40 +1,34 @@
 import React, { useState } from "react";
+import { Button, Container, Input, Panel, InputGroup } from 'rsuite';
+import EyeIcon from '@rsuite/icons/legacy/Eye';
+import EyeSlashIcon from '@rsuite/icons/legacy/EyeSlash';
+import image from '../assets/socially_distributed.jpg'
+
 
 function SIGN_IN(){
-    const [username, set_username] =  useState("")
-    const [password, set_password] = useState("")
+    // const [username, set_username] =  useState("")
+    // const [password, set_password] = useState("")
+    const [visible, setVisible] = React.useState(false);
+
+    const handleChange = () => {
+      setVisible(!visible);
+    };
+
+    const url = 'http://localhost:3000' + '/socially_distributed'
 
     return (
-        <form>
-            <h3>Sign In</h3>
-            <div className="mb-3">
-                <label>Username</label>
-                <input
-                    type="username"
-                    className="form-control"
-                    placeholder="Enter Username"
-                    value = {username}
-                    onChange={e => set_username(e.target.value)}
-                />
-            </div>
-
-            <div className="mb-3">
-                <label>Password</label>
-                <input
-                    type="password"
-                    className="form-control"
-                    placeholder="Enter password"
-                    value = {password}
-                    onChange={e => set_password(e.target.value)}
-                />
-            </div>
-
-            <div className="d-grid">
-                <button type="submit" className="btn btn-primary" onClick={console.log(username, password)}>
-                    Submit
-                </button>
-            </div>
-      </form>
+        <div>
+            <Panel header="Login" shaded style={{width: "50%", margin:"auto", textAlign:"center" }}>
+                <Input placeholder="username" style={{width: 300, margin: 10}}/>
+                <InputGroup inside style={{width: 300, margin:10}}>
+                    <Input placeholder="password" type={visible ? 'text' : 'password'} />
+                    <InputGroup.Button onClick={handleChange}>
+                        {visible ? <EyeIcon /> : <EyeSlashIcon />}
+                    </InputGroup.Button>
+                </InputGroup>
+                <Button apperance="Primary" block style={{width: 300, margin: 10}}>Login</Button>
+            </Panel>
+        </div>
     )
 }
 
