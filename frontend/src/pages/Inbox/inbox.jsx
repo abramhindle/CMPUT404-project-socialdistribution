615e6fe1469da697bef498bcc311260779b257e9
Courtenay Laing-Kobe <laingkob@ualberta.ca>
2023-02-23 14:52:07 -0700

    Initial Inbox Sidebar

---
+++ frontend/src/pages/Inbox/inbox.jsx
@@ -0,0 +1,17 @@
+import Sidebar from '../../components/Sidebar/sidebar';
+import './inbox.css';
+
+function Inbox() {
+  return (
+    <div>
+      <Sidebar/>
+      <div>
+        <p className='Center'>
+            This is now the inbox page
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Inbox;
