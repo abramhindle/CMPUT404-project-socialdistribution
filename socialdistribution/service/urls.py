b4f9249b717fb4a7c152406e87b1356a4f4fa3d8
Aidan Horemans <aidanjhoremans@gmail.com>
2023-02-23 22:26:10 -0700

    uncomment

--- socialdistribution/service/urls.py
+++ socialdistribution/service/urls.py
@@ -6,8 +6,8 @@ from service.views.comment import CommentView
 
 urlpatterns = [
     #for every different model, create a new model file and view file in the /model and /view directories then link it up here
-    # path('service/authors/<str:author>/followers', FollowersAPIView.as_view(), name='service-followers'),
-    # path('service/authors/<str:author>/followers/<str:another_author>', FollowerAPIView.as_view(), name='service-follower'),
+    path('service/authors/<str:author>/followers', FollowersAPIView.as_view(), name='service-followers'),
+    path('service/authors/<str:author>/followers/<str:another_author>', FollowerAPIView.as_view(), name='service-follower'),
     path('', MultipleAuthors.as_view()), 
     path('<uuid:id>/', SingleAuthor.as_view()),
     path('<uuid:author_id>/posts/', PostCreation.as_view(), name='post_creation'),
