5dfcfb38b5ae073c38456f94d59cb723cb91251b
Junesung Lee <junesoung412@gmail.com>
2023-02-27 02:57:54 -0700

    revised liked views

--- socialdistribution/service/tests/liked.py
+++ socialdistribution/service/tests/liked.py
@@ -4,7 +4,8 @@ from django.contrib.auth.models import User
 from service.models.author import Author
 from service.models.post import Post
 from service.models.likes import Likes
-from service.serializers.likes import LikesSerializer
+from service.models.liked import Liked
+from service.serializers.liked import LikedSerializer
 import json
 
 
@@ -21,6 +22,10 @@ class LikedTests(TestCase):
         
         self.likes1 = Likes.objects.create(context="Test", summary="Testing Now", author=self.author1, object=str(self.post1._id))
         self.likes2 = Likes.objects.create(context="Test222", summary="Testing Now22", author=self.author1, object=str(self.post2._id))
+        self.likes = [self.likes1, self.likes2]
+
+        self.liked1 = Liked.objects.create()
+        self.liked1.items.set(self.likes)
 
         self.client = APIClient()
     
@@ -32,13 +37,11 @@ class LikedTests(TestCase):
         self.post2.delete()
         self.likes1.delete()
         self.likes2.delete()
+        self.liked1.delete()
 
 
     def test_liked(self):
         path = f"/service/authors/{self.author1._id}/liked/"
         response = self.client.generic(method="GET", path=path)
 
-        print(json.dumps(response.data))
-
-        self.assertEqual(json.dumps(LikesSerializer(self.likes1).data), json.dumps(response.data["liked"]["items"][0]))
-        self.assertEqual(json.dumps(LikesSerializer(self.likes2).data), json.dumps(response.data["liked"]["items"][1]))
+        self.assertJSONEqual(json.dumps(LikedSerializer(self.liked1).data), json.loads(response.data["liked"]))
--- socialdistribution/service/views/liked.py
+++ socialdistribution/service/views/liked.py
@@ -1,9 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from service.models.likes import Likes
 from service.models.liked import Liked
 from service.models.author import Author
 from service.serializers.liked import LikedSerializer
+import json
 
 
 class LikedView(APIView):
@@ -11,8 +11,6 @@ class LikedView(APIView):
 
     def get(self, request, author):
         author_query = Author.objects.get(_id = author)
-        liked = Liked.objects.create()
-
-        liked.items.set(Likes.objects.filter(author = str(author_query._id)))
+        liked = Liked.objects.filter(items__author = str(author_query._id))[0] # would be better to add author_id field into Liked model, so it takes less resources to querying
         
-        return Response({"liked": self.serializer_class(liked).data})
+        return Response({"liked": json.dumps(self.serializer_class(liked).data)})
