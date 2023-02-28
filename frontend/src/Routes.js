c34b4ca8622fe22b588457fb551405dcfebf221b
Courtenay Laing-Kobe <laingkob@ualberta.ca>
2023-02-27 23:01:41 -0700

    Removed unnecessary Css for Routes

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,5 +1,3 @@
-import "./App.css";
-
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { PrivateRoute, SignInRoute } from "./utils/CustomRoute";
 import SignIn from "./Signin";
