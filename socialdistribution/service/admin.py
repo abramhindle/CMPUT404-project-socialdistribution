4408b3578cd9d64ee6ad3f68da5a6410c15fb05c
Courtenay Laing-Kobe <laingkob@ualberta.ca>
2023-02-28 13:20:24 -0700

    missed small change

--- socialdistribution/service/admin.py
+++ socialdistribution/service/admin.py
@@ -4,12 +4,8 @@ from service.models import author, post, comment, follow
 # Register your models here.
 
 admin.site.register(author.Author)
-<<<<<<< HEAD
-admin.site.register(post.Post) #we don't need this here, but leaving as comment just in case
-=======
 admin.site.register(follow.Followers)
 #admin.site.register(post.Post) #we don't need this here, but leaving as comment just in case
->>>>>>> 46b21d399abf2a1c3a888dedcd5081359186bde5
 #admin.site.register(post.Category)
 admin.site.register(comment.Comment)
 
