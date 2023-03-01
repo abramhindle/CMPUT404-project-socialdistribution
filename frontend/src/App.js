8865cb01ed2fea24355c87c14150e676203c7c9d
Victor Nguyen <46537385+NVectors@users.noreply.github.com>
2023-02-28 20:43:35 -0700

    Initial components required

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router, Route, Routes,
+}
+  from 'react-router-dom';
+import Login from './components/auth/Login';
+import Signup from './components/auth/Signup';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/login" element={(<Login />)} />
+          <Route path="/signup" element={(<Signup />)} />
+        </Routes>
+      </Router>
+
     </div>
   );
 }
