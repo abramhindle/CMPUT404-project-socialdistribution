c91e1c04e5025d1541bc3aea78ff629e1f7fe5b6
Peter <sqiu@ualberta.ca>
2023-03-02 11:44:35 -0700

    database can store images

--- backend/social/social/urls.py
+++ backend/social/social/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/', include('posts.urls')),
 
 ]
+
+# adds the path to the media directory, we can put images in media/images/[filename] 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
