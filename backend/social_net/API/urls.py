a37cca14ef10085c1e69142ca57fd5c8586c1052
cenab <40250311+cenab@users.noreply.github.com>
2023-03-03 08:20:45 -0700

    liked endpoint

--- backend/social_net/API/urls.py
+++ backend/social_net/API/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
   path('authors/<slug:author_id>/inbox/', views.InboxView, name='InboxView'),
   path('authors/<slug:author_id>/posts/<slug:post_id>/likes/', views.PostLikeView, name='PostLikeView'),
   path('authors/<slug:author_id>/posts/<slug:post_id>/comments/<slug:comment_id>/likes/', views.CommentLikeView, name='CommentLikeView'),
+  path('authors/<slug:author_id>/liked/', views.LikedView, name='LikedView'),
 ]
--- backend/social_net/API/views.py
+++ backend/social_net/API/views.py
@@ -276,3 +276,20 @@ def CommentLikeView(request, author_id, post_id, comment_id):
         comment_likes_paginated.append(serialized_object.data)
     output = {"object": object, "likes": comment_likes_paginated}
     return JsonResponse(output, status = 200)
+
+@api_view(['GET'])
+def LikedView(request, author_id):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    liked_paginated = []
+    liked_object = LikeModel.objects.filter(author=author_id)
+    for liked in liked_object:
+        serialized_object = LikeSerializer(liked).data
+        if "comments" not in serialized_object["object"]:
+            post_id = liked["object"].split("/")[6]
+            post_object = PostsSerializer(PostsModel.objects.get(id=post_id)).data["visibility"]
+            if post_object == "PUBLIC":
+                liked_paginated.append(serialized_object.data)
+    output = {"author_id": author_id, "likes": liked_paginated}
+    return JsonResponse(output, status = 200)
