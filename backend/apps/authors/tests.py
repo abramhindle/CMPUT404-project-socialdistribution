21984dd34d5e0004b5fb25a3331715a38a33b09f
curtiskennedy <52674197+curtiskennedy@users.noreply.github.com>
2023-03-03 12:39:07 -0700

    Update docs for author tests

--- backend/apps/authors/tests.py
+++ backend/apps/authors/tests.py
@@ -10,23 +10,23 @@ class Author_TestCase(TestCase):
         return super().setUp()
     
     def test_get_nonexisting_author(self):
-        """trying to get a non-existant author should return a 404 code"""
+        """GET /authors/-1/ (trying to get a non-existant author should return a 404 code)"""
         response = c.get('/authors/-1/')
         self.assertEqual(response.status_code, 404)
 
     def test_get_all_authors(self):
-        """get all authors should return a 200 code"""
+        """GET /authors/ (get all authors should return a 200 code)"""
         response = c.get('/authors/')
         self.assertEqual(response.status_code, 200)
 
     def test_post_invalid_author(self):
-        """trying to create an invalid author should return a 400 code"""
+        """POST /authors/ (trying to create an invalid author should return a 400 code)"""
         # this is invalid because the github link must be <= 100 chars
         response = c.post('/authors/', {"github": "01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789"})
         self.assertEqual(response.status_code, 400)
 
     def test_create_author(self):
-        """creating a valid author should return a 201 code"""
+        """POST /authors/ (creating a valid author should return a 201 code)"""
         response = c.post('/authors/', {"displayName": "bob"})
         self.assertEqual(response.status_code, 201)
 
