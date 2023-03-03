2242d89cb80e27456e40ac11a1d418da90b42cd1
yuchieh8968 <yuchieh@ualberta.ca>
2023-03-03 15:11:38 -0700

    created basic page for inbox

--- backend/apps/inbox/models.py
+++ backend/apps/inbox/models.py
@@ -9,13 +9,17 @@ class Inbox(models.Model):
     url = models.URLField(max_length=200, blank=True)
     
 
-    # returns lists of comments
     def commentlist_template():
         return {"comments": "[]"}
     
     def likeslist_template():
         return {"likes":"[]"}
+    
+    def followrequestlist_template():
+        return {"Follow Requests": "[]"}
 
 
     comment_list = models.JSONField(blank=True, default=commentlist_template)
     like_list = models.JSONField(blank=True, default=likeslist_template)
+    follow_request_list = models.JSONField(blank=True, default=followrequestlist_template)
+
