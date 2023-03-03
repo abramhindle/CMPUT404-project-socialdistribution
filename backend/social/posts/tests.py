3e0d200c7671a1f7dd822873fe9d2c6179df9a5d
Peter <sqiu@ualberta.ca>
2023-03-03 15:45:07 -0700

    asdf

--- backend/social/posts/tests.py
+++ backend/social/posts/tests.py
@@ -1,12 +1,30 @@
-from django.test import TestCase
-from rest_framework.test import APIRequestFactory
-
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
+from .models import Post
+from PIL import Image
+test_image = Image.open(r"media/test_img/test_img.png")
 # Create your tests here.
 
 # test post creation
 
-
 # test image post + proper base64 serialization
+class ImagePostTestCase(APITestCase):
 
-factory = APIRequestFactory()
-request = factory.post
+    def test_post_image(self):
+        response = self.client.post(reverse('posts'),{
+            'author':'jimbobpaulkevin',
+            'categories':'',
+            'title':'',
+            'description':'',
+            'contentType':'image/png;base64',
+            'content':'',
+            'image':test_image,
+        })
+
+        print(response)
+
+    '''
+    def test_image_is_serialized(self):
+        base64_image = Post.objects.get(author='jimbobpaulkevin')
+    '''
--- backend/social/posts/urls.py
+++ backend/social/posts/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
   path('authors/<str:pk_a>/posts/<str:pk>/', views.DetailView.as_view(), name='detail'),
   path('authors/<str:pk_a>/posts/<str:pk>/comments/', views.get_comments, name='get_comments'),
   path('authors/<str:pk_a>/posts/<str:pk>/likes/', views.get_comments, name='get_likes'),
-  path('authors/<str:pk_a>/posts/<str:pk>/image/', ImageView.as_view()),
+  path('authors/<str:pk_a>/posts/<str:pk>/image/', ImageView.as_view(), name = "image"),
   path('authors/<str:pk_a>/inbox/', views.LikeView.as_view(), name = "likes"),
 
 ]
