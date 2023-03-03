74b6d02cb1ca8847fd729e661cb33408b9479755
hbheesetti <77315050+hbheesetti@users.noreply.github.com>
2023-03-03 16:46:21 -0700

    afaj

--- backend/social/posts/views.py
+++ backend/social/posts/views.py
@@ -43,8 +43,8 @@ class post_list(APIView, PageNumberPagination):
         except Author.DoesNotExist:
             return Response("Author not found", status=status.HTTP_404_NOT_FOUND)
 
-        # should do this a different way but for now, it should serialize as image
-        if 'image' in request.data:
+        # should do this a different fway but for now, it should serialize as image
+        if 'image' in request.data['contentType']:
             serializer = ImageSerializer(data=request.data, context={'author_id': pk_a})
         else:
             serializer = PostSerializer(data=request.data, context={'author_id': pk_a})
