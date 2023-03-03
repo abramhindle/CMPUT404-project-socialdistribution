520850a2ff880a1166530bd173b09d8a2e7a68c4
cenab <40250311+cenab@users.noreply.github.com>
2023-03-02 22:11:01 -0700

    post endpoint

--- backend/social_net/API/views.py
+++ backend/social_net/API/views.py
@@ -1,8 +1,9 @@
 from rest_framework.decorators import api_view
 from django.http import JsonResponse
-from .models import AuthorModel
-from .serializers import AuthorSerializer
+from .models import AuthorModel, PostsModel
+from .serializers import PostsSerializer, AuthorSerializer
 import json
+import uuid
 
 @api_view(['GET', 'POST'])
 def AuthorView(request, uid):
@@ -14,7 +15,6 @@ def AuthorView(request, uid):
         serialized_object = AuthorSerializer(author_object)
         output = serialized_object.data
         return JsonResponse(output, status = 200)
-
     elif request.method == 'POST':
         author_object = AuthorModel.objects.get(id=uid)
         serialized_object = AuthorSerializer(author_object)
@@ -122,6 +122,45 @@ def FollowView(request, uid, uid2):
 
         return JsonResponse(follow_output, status = 200)
     
+@api_view(['GET', 'POST'])
+def PostsView(request, uid):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    #checks friendship
+    posts_paginated = {}
+    if request.method == 'GET':
+        post_object = PostsModel.objects.filter(author=uid).order_by('-published')[0:4]
+        for post in post_object:
+            serialized_object = PostsSerializer(post)
+            posts_paginated.append(serialized_object.data)
+        return JsonResponse(posts_paginated, status = 200)
+    elif request.method == 'POST':
+        PostsModel.objects.create(author=uid, id=str(uuid.uuid4()))
+        return JsonResponse({"status":"success"}, status = 200)
+
+@api_view(['GET', 'POST', 'DELETE', 'PUT'])
+def PostsRetriveView(request, uid, post_id):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    if request.method == 'GET':
+        post_object = PostsModel.objects.get(id=post_id)
+        serialized_object = AuthorSerializer(post_object)
+        return JsonResponse(serialized_object.data, status = 200)
+    elif request.method == 'POST':
+        post_object = PostsModel.objects.get(id=post_id)
+        serialized_object = PostsSerializer(post_object)
+        parameters = json.loads(request.body)
+        serialized_object.update(post_object, parameters)
+        return JsonResponse({"status":"success"}, status = 200)
+    elif request.method == 'DELETE':
+        PostsModel.objects.filter(author=uid, id=post_id).delete()
+        return JsonResponse({"status":"success"}, status = 200)
+    elif request.method == 'PUT':
+        PostsModel.objects.create(author=uid, id=post_id)
+        return JsonResponse({"status":"success"}, status = 200)
+
 
 
 
