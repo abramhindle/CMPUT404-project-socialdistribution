05c3becc14d6e326e64c40c2a8f328f9dc01e45b
dadaqi <qg@ualberta.ca>
2023-03-06 18:25:55 -0700

    Revert "do it later"
    This reverts commit c94909275b75921570dde53d96e8d66708251870.

---
+++ likelikedver2/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
