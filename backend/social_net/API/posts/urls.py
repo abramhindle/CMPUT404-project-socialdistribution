f865023fe8999644e85f9f3c295cdf48d2f1c538
dadaqi <77340349+dadaqi@users.noreply.github.com>
2023-03-06 18:21:22 -0700

    Revert "add likes url"
    This reverts commit 64c73177954f8c505fef48df627a01c77ed5e78e.

--- backend/social_net/API/posts/urls.py
+++ backend/social_net/API/posts/urls.py
@@ -3,11 +3,6 @@ from . import views
 
 
 urlpatterns = [
-   path('authors/<slug:author_id>/posts/<slug:post_id>/', views.PostsRetriveView, name='PostsRetriveView'),
-    path('authors/<slug:author_id>/posts/', views.PostsView, name='PostsView'),
-    path('authors/<slug:author_id>/inbox/', views.inbox, name='inbox'),
-    path('authors/<slug:author_id>/posts/<slug:post_id>/likes/', views.post_likes, name='post_likes'),
-    path('authors/<slug:author_id>/posts/<slug:post_id>/comments/<slug:comment_id>/likes/', views.comment_likes, name='comment_likes'),
-    path('authors/<slug:author_id>/liked/', views.liked, name='liked'),
-    path('authors/<slug:author_id>/inbox/', views.inbox, name='inbox'),
+  path('authors/<slug:author_id>/posts/<slug:post_id>/', views.PostsRetriveView, name='PostsRetriveView'),
+  path('authors/<slug:author_id>/posts/', views.PostsView, name='PostsView'),
 ]
