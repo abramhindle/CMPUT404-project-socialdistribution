9c2705718302d71c5ecb11259621efa32842547d
libbusUA <libbus@ualberta.ca>
2023-03-10 12:09:00 -0700

    Working on Frontend Likes Post request

--- Frontend/socially_distributed/src/Components/Post/Like.js
+++ Frontend/socially_distributed/src/Components/Post/Like.js
@@ -1,9 +1,57 @@
 import React, { useState, useEffect } from "react";
-import { Avatar, Panel } from "rsuite";
+import { Input, InputGroup, Panel } from "rsuite";
+import { Scrollbars } from "react-custom-scrollbars-2";
+import axios from "axios";
+
 // Component Imports
 
 function LIKE({ likeobj }) {
 	const [like, set_like] = useState(likeobj);
+	const [postObj, setPostObj] = useState(postobj);
+	const [new_like, set_new_like] = useState("");
+
+	// const getLike = (url) => {
+	// 	axios({method: "get", url: url})
+	// 		.then((res) => {
+	// 			set_like(res.data)
+	// 		})
+	// 		.catch((err) => console.log(err));
+	// };
+	// const getComments = (url) => {
+	// 	axios({ method: "get", url: url })
+	// 		.then((res) => {
+	// 			setCommentObj(res.data);
+	// 		})
+	// 		.catch((err) => console.log(err));
+	// };
+
+	const handleSubmitClick = () => {
+		
+
+
+	}
+	// const handleSubmitClick = () => {
+	// 	const author = JSON.parse(localStorage.getItem("user"));
+	// 	const len = 36;
+	// 	const author_id = author.id.slice(
+	// 		author.id.length - len,
+	// 		author.id.length
+	// 	);
+	// 	const post_id = postObj.id.slice(
+	// 		postObj.id.length - len,
+	// 		postObj.id.length
+	// 	);
+	// 	const params = { comment: new_comment };
+	// 	const url = `posts/authors/${author_id}/posts/${post_id}/comments/`;
+	// 	axios({ method: "post", url: url, data: params })
+	// 		.then((res) => {
+	// 			if (res.status === 200) {
+	// 				getComments(url);
+	// 			}
+	// 		})
+	// 		.catch((err) => console.log(err));
+	// };
+
 
 	return (
 		<Panel bordered style={{ marginBottom: "5px" }}>
