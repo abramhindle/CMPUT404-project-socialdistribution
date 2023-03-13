5afa8ea2e116f3634c1834b97471f4464703d1ac
libbusUA <libbus@ualberta.ca>
2023-03-13 14:30:50 -0600

    Fixed frontend likes with all params

--- Frontend/socially_distributed/src/Components/Post/Like.js
+++ Frontend/socially_distributed/src/Components/Post/Like.js
@@ -9,12 +9,13 @@ function LIKE({ likeobj }) {
 	const [like, set_like] = useState(likeobj);
 	const [postObj, setPostObj] = useState(postobj);
 	const [new_like, set_new_like] = useState("");
-
+	const postObjUrl = postObj.url;
 	
 	//Confirm the name of the button 
 	const handleSubmitClick = () => {
 		const author = JSON.parse(localStorage.getItem("user"));
-		const len = 36
+		const len = 36;
+		const author_name = author.displayName;
 		const author_id = author.id.slice(
 			author.id.length - len,
 			author.id.length
@@ -23,7 +24,8 @@ function LIKE({ likeobj }) {
 			postObj.id.length - len,
 			postObj.id.length
 		);
-		const params = {author: author_id,  };
+		const message = author_name + " Liked your post."
+		const params = {author: author_id, object: postObjUrl, summary: message};
 		const url = `posts/authors/${author_id}/inbox`;
 
 	//Confirm what to add into the params and send inbox
