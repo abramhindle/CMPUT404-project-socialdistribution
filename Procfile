f84c7346d9b8523aa0465def463b8af0f354754f
Aidan Horemans <aidanjhoremans@gmail.com>
2023-03-15 17:04:30 -0600

    fix proc

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+release: python manage.py migrate
 web: cd socialdistribution && gunicorn socialdistribution.wsgi
--- socialdistribution/socialdistribution/settings.py
+++ socialdistribution/socialdistribution/settings.py
@@ -95,8 +95,8 @@ WSGI_APPLICATION = 'socialdistribution.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE' : 'django.db.backends.sqlite3',
-        'NAME' : BASE_DIR / 'db.sqlite3'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3'
     }
 }
 
