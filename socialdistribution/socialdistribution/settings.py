4a03bdf7a168dcd3f473eff5c95de102f924d79b
Aidan Horemans <aidanjhoremans@gmail.com>
2023-03-15 21:34:33 -0600

    add npm

--- socialdistribution/socialdistribution/settings.py
+++ socialdistribution/socialdistribution/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-dybliy2*u14hioctad94q0woplw9gz0oeqt+22xcyw=qr7bkg3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
