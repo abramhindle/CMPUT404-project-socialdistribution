cc7b7e64b3720ae6f5f72fc8d4c63dc8489142ca
Leen-Alzebdeh <alzebdeh@ualberta.ca>
2023-03-17 12:11:37 -0600

    changed name of type of follow request

--- backend/social/author/models.py
+++ backend/social/author/models.py
@@ -75,7 +75,7 @@ class Inbox(models.Model):
         ]
 
 class FollowRequest(models.Model):
-    Type =models.CharField(max_length=255, blank=True)
+    type =models.CharField(max_length=255, blank=True)
     actor = models.ForeignKey(Author, related_name='actor', on_delete=models.CASCADE)
     object = models.ForeignKey(Author, related_name='object', on_delete=models.CASCADE)
     Summary =models.CharField(max_length=255, blank=True)
