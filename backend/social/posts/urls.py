2721b4f8c0cb62ea33858397ebb1e448665d843c
Leen-Alzebdeh <alzebdeh@ualberta.ca>
2023-03-18 22:42:38 -0600

    add specific comment url (eror)

--- backend/social/posts/urls.py
+++ backend/social/posts/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
   path('authors/<str:pk_a>/posts/', views.post_list.as_view(), name = "posts"),
   path('authors/<str:pk_a>/posts/<str:pk>/', views.post_detail.as_view(), name='detail'),
   path('authors/<str:pk_a>/posts/<str:pk>/comments/', views.CommentView.as_view(), name='comments'),
+  path('authors/<str:pk_a>/posts/<str:pk>/comments/<str:pk_m>', views.CommentDetailView.as_view(), name='comment_detail'),
   path('authors/<str:pk_a>/posts/<str:pk>/likes/', views.get_likes, name='get_likes'),
   path('authors/<str:pk_a>/posts/<str:pk>/image/', ImageView.as_view()),
   #path('authors/<str:pk_a>/inbox/', views.LikeView.as_view(), name = "likes"),
