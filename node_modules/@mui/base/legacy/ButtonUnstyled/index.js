67c3ed9ecf93008e355cd971f0e8d58141ec84dd
Junesung Lee <junesoung412@gmail.com>
2023-03-18 22:40:54 -0600

    clean up

--- node_modules/@mui/base/legacy/ButtonUnstyled/index.js
+++
@@ -1,3 +0,0 @@
-export { default } from './ButtonUnstyled';
-export { default as buttonUnstyledClasses, getButtonUnstyledUtilityClass } from './buttonUnstyledClasses';
-export * from './ButtonUnstyled.types';
