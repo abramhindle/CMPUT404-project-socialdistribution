15444c9d64d5d055be790e66ab387bc5110936fd
Sean-Meyers <smmeyers@ualberta.ca>
2023-03-20 16:14:50 -0600

    utility script for if we ever have to merge into this branch. currently removes all unecessary files and directories, but will eventually also move everything in the backend directory to the root.

---
+++ cleanbackend.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -rf ./frontend
+rm -rf ./likelikedver2
+rm -rf ./testcase
+rm -rf ./docker-compose.yml
+rm -rf ./heroku.yml
+rm -rf ./project.org
+rm -rf ./backend/social_net/entrypoint.sh
+rm -rf ./backend/social_net/Dockerfile
+rm -rf ./backend/social_net/db.sqlite3
+
