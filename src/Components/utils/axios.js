97fe64d376455606f7cd3f3f71931b340ec186da
hbheesetti <77315050+hbheesetti@users.noreply.github.com>
2023-03-23 23:55:42 -0600

    deploy

--- src/Components/utils/axios.js
+++ src/Components/utils/axios.js
@@ -3,12 +3,14 @@ import { getCsrfToken } from "./auth";
 
 var username = localStorage.getItem("username");
 var password = localStorage.getItem("password");
-
+var base64encodedData = username + ":" + password;
+var base64String = btoa(base64encodedData);
 getCsrfToken();
 var token = localStorage.getItem("token");
 export let reqInstance = axios.create({
 	headers: {
 		"X-CSRFToken": token,
+		Authorization: "Basic " + base64String,
 	},
 	auth: {
 		username: username,
