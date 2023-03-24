bea2c3713d45be93793e00cff3228ed6e3be22a4
libbusUA <libbus@ualberta.ca>
2023-03-24 13:53:35 -0600

    Fixed issue

--- author/views.py
+++ author/views.py
@@ -250,8 +250,8 @@ class AuthorView(APIView):
                 author_json, status_code = getNodeAuthor_Yoshi(pk_a)
                 if status_code == 200:
                     # author_dict = json.loads(author_json)
-                    author = Author(id = author_json['authorId'], displayName= author_json['displayname'], url=author_json['url'], profileImage=author_json['profileImage'], github=author_json['github'], host=author_json['host'])
-                    # return Response(author)
+                    # author = Author(id = author_json['authorId'], displayName= author_json['displayname'], url=author_json['url'], profileImage=author_json['profileImage'], github=author_json['github'], host=author_json['host'])
+                    return Response(author_json)
 
                 else:
                     author_json, status_code = getNodeAuthor_social_distro(pk_a)
