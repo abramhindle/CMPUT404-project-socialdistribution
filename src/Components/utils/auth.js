e94097205265aed84c895b1e8cb925f99389fa01
hbheesetti <77315050+hbheesetti@users.noreply.github.com>
2023-03-24 11:51:55 -0600

    deploy

--- src/Components/utils/auth.js
+++ src/Components/utils/auth.js
@@ -44,7 +44,7 @@ export async function getCurrentUser(author_id) {
 
 export async function getCsrfToken() {
 	let _csrfToken = null;
-	const API_HOST = "https://sociallydistributed.herokuapp.com";
+	const API_HOST = "http://127.0.0.1:8000";
 	if (_csrfToken === null) {
 		const response = await fetch(`${API_HOST}/csrf/`, {
 			credentials: "include",
@@ -70,7 +70,9 @@ export function getAuthorId(a_id) {
 
 export const getProfileImageUrl = () => {
 	const user = JSON.parse(localStorage.getItem("user"));
-	return user.profileImage;
+	if (localStorage.getItem("loggedIn")) {
+		return user.profileImage;
+	}
 };
 
 export const setCreds = (obj) => {
