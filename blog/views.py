0d33a3aa1b3132e1c1b1289b4b3df92a27780fb4
Junesung Lee <junesoung412@gmail.com>
2023-03-23 21:09:46 -0600

    sign up endpoint

--- blog/views.py
+++ blog/views.py
@@ -2,15 +2,28 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import serializers
 from service.models.author import Author
+from django.contrib.auth.models import User
 from django.contrib import auth
+from django.db import IntegrityError
 from rest_framework.authtoken.models import Token
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
 import json
 
 class SignInSerializerForm(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField()
+
+
+class SignUpSerializerForm(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+    displayName = serializers.CharField()
+    profileImage = serializers.URLField()
+    github = serializers.URLField()
+
 
 @method_decorator(csrf_exempt, name='dispatch')
 class SignInView(APIView):
@@ -26,7 +39,7 @@ class SignInView(APIView):
         password =  data["password"]
 
         user = auth.authenticate(username=username, password=password)
-        if user is not None:
+        if user:
             try:
                 author = Author.objects.get(user=user)
                 auth.login(request, user)
@@ -46,3 +59,42 @@ class SignOutView(APIView):
             return Response({ 'success': 'Loggout Out' }, status=200)
         except:
             return Response({ 'error': 'Something went wrong when logging out' }, status=401)
+        
+@method_decorator(csrf_exempt, name='dispatch')
+class SignUpView(APIView):
+    permission_classes = ()
+    serializer_class = SignUpSerializerForm
+    
+    def post(self, request, format=None):   
+        try:
+            data = json.loads(self.request.data)
+        except:
+            data = self.request.data
+
+        username = data["username"]
+        password =  data["password"]
+        displayName = data["displayName"]
+        profileImage = data["profileImage"]
+        github = data["github"]
+
+        if not valid_url(github):
+            return Response({"error": "github field should be a url"}, status=400)
+        if not valid_url(profileImage):
+            return Response({"error": "profileImage field should be a url"}, status=400)
+
+        try:
+            user = User.objects.create_user(username=username, password=password, is_active=False)
+            Author.objects.create(displayName=displayName, user=user, profileImage=profileImage, github=github)
+            return Response({"success": "Sign Up Requested!"}, status=202)
+        except IntegrityError:
+            return Response({"error": "Username already in use"}, status=409)
+        except:
+            return Response({"error": "Error Occured"}, status=400)
+        
+def valid_url(to_validate):
+    validator = URLValidator()
+    try:
+        validator(to_validate)
+        return True
+    except ValidationError:
+        return False
