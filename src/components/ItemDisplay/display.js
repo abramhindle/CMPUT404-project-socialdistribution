60c2e5db90b81b88d0b8b7f7c9b4c97b4289f265
Courtenay Laing-Kobe <laingkob@ualberta.ca>
2023-03-28 20:48:51 -0600

    Missed conflict fix

--- src/components/ItemDisplay/display.js
+++ src/components/ItemDisplay/display.js
@@ -3,13 +3,10 @@ import PlainPost from "./post-text";
 import Like from "./like";
 import FollowRequest from "./follow-request";
 
-//TODO : follow request objects
-
 export default function DisplayItem(props) {
   const data = props.data;
   const liked = props.liked;
 
-<<<<<<< Updated upstream
   if (data.type === "comment") {
     //display comment
     return <Comment data={data} liked={liked} updateList={props.updateList} />;
@@ -28,25 +25,3 @@ export default function DisplayItem(props) {
     return;
   }
 }
-=======
-    if (data.type === "comment"){
-        //display comment
-        return (<Comment data={data} liked={liked} updateList={props.updateList}/>);
-    }
-    else if (data.type === "post"){
-        //display post
-        return (<PlainPost post={data} liked={liked} updateList={props.updateList}/>);
-    }
-    else if (data.type === "like"){
-        //display like object
-        return (<Like data={data}/>);
-    }
-    else if (data.type === "follow"){
-        //display follow request object
-        return (<FollowRequest data={props.data}/>);
-    }
-    else{
-        return;
-    }
-}
->>>>>>> Stashed changes
