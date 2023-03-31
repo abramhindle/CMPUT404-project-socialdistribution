2656384882b0368f39972bd4868a692e732ecb50
Sean-Meyers <smmeyers@ualberta.ca>
2023-03-30 19:46:42 -0600

    hopefully fixed an inbox serialization issue

--- api/serializers.py
+++ api/serializers.py
@@ -207,7 +207,7 @@ class FollowSerializer(serializers.ModelSerializer):
 
 
 class InboxSerializer(serializers.ModelSerializer):
-    author = AuthorSerializer()
+    author = AuthorSerializer(read_only=True)
 
     # def create(self, validated_data):
     #     author_data = validated_data.pop('author')
